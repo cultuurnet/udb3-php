1aa9fbbbed1c87f4b060e5a76fbe756be63e2aa0
Kristof Coomans <kristof@coomans.eu>
2014-11-06 11:44:47 +0100

    Ensure that queued jobs transfer their context to the command bus when they are performed

--- src/CommandHandling/QueueJob.php
+++ src/CommandHandling/QueueJob.php
@@ -11,11 +11,16 @@ class QueueJob {
     public function perform()
     {
         $command = unserialize($this->args['command']);
-
         global $app;
 
         /** @var ResqueCommandBus $commandBus */
         $commandBus = $app['event_command_bus'];
+
+        if ($commandBus instanceof ContextAwareInterface) {
+            $context = unserialize($this->args['context']);
+            $commandBus->setContext($context);
+        }
+
         $commandBus->deferredDispatch($command);
     }
 } 

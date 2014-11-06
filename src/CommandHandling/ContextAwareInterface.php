7a92823bd5a9a53ce50d5815bdf037fa4a642f5c
Kristof Coomans <kristof@coomans.eu>
2014-11-06 11:37:02 +0100

    Add interface for components that will need to be contextual information aware, like commands, command busses, meta data enrichers.

---
+++ src/CommandHandling/ContextAwareInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\CommandHandling;
+
+
+use Broadway\Domain\Metadata;
+
+interface ContextAwareInterface
+{
+
+    public function setContext(Metadata $context = null);
+} 

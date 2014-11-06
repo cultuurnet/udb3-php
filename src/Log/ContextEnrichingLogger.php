66af3d8db56610a54088b3a4d61b99d02660d376
Kristof Coomans <kristof@coomans.eu>
2014-11-06 14:17:40 +0100

    Add Logger decorator for enriching the log message context

---
+++ src/Log/ContextEnrichingLogger.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Log;
+
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+
+/**
+ * Logger decorator to enrich log message context.
+ */
+class ContextEnrichingLogger implements LoggerInterface
+{
+    use LoggerTrait;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $decoratee;
+
+    /**
+     * @var array
+     */
+    protected $context;
+
+    public function __construct(LoggerInterface $decoratee, $context = array())
+    {
+        $this->decoratee = $decoratee;
+        $this->context = $context;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $enrichedContext = $this->context + $context;
+
+        $this->decoratee->log($level, $message, $enrichedContext);
+    }
+} 

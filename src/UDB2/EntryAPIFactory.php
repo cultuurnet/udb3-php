8af487fdd4124b19aa9406baefa0c61eacfb08f1
Kristof Coomans <kristof@coomans.eu>
2014-11-08 15:29:29 +0100

    iii-70: Add a factory for the entry API

---
+++ src/UDB2/EntryAPIFactory.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\UDB2;
+
+use CultuurNet\Auth\TokenCredentials;
+
+final class EntryAPIFactory
+{
+    /**
+     * @var Consumer
+     */
+    private $consumer;
+
+    /**
+     * @param Consumer $consumer
+     */
+    public function __construct(
+        Consumer $consumer
+    )
+    {
+        $this->consumer = $consumer;
+    }
+
+    /**
+     * @param TokenCredentials $tokenCredentials
+     * @return \CultureFeed_EntryApi
+     */
+    public function withTokenCredentials(TokenCredentials $tokenCredentials)
+    {
+        $consumerCredentials = $this->consumer->getConsumerCredentials();
+
+        $oauthClient = new \CultureFeed_DefaultOAuthClient(
+            $consumerCredentials->getKey(),
+            $consumerCredentials->getSecret(),
+            $tokenCredentials->getToken(),
+            $tokenCredentials->getSecret()
+        );
+        $oauthClient->setEndpoint($this->consumer->getTargetUrl());
+        return new \CultureFeed_EntryApi($oauthClient);
+    }
+}

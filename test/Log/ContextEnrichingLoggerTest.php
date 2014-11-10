6a4c0cc0fa62bc481f6a29a08af44876ab586816
Kristof Coomans <kristof@coomans.eu>
2014-11-10 16:41:02 +0100

    Add test for ContextEnrichingLogger

---
+++ test/Log/ContextEnrichingLoggerTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Log;
+
+
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+
+class ContextEnrichingLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_passes_additional_context_to_the_decorated_logger()
+    {
+        /** @var LoggerInterface|\PHPUnit_Framework_MockObject_MockObject $decoratedLogger */
+        $decoratedLogger = $this->getMock('\\Psr\Log\\LoggerInterface');
+        $additionalContext = array(
+            'job_id' => 1,
+        );
+        $logger = new ContextEnrichingLogger(
+            $decoratedLogger,
+            $additionalContext
+        );
+
+        $decoratedLogger->expects($this->once())
+            ->method('log')
+            ->with(
+                Logger::DEBUG,
+                'test',
+                [
+                    'foo' => 'bar',
+                    'job_id' => 1
+                ]
+            );
+        $logger->log(
+            Logger::DEBUG,
+            'test',
+            [
+                'foo' => 'bar'
+            ]
+        );
+
+
+    }
+} 

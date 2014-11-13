da7422ecbe99b676f17490ba7db1015b4b09dbee
Kristof Coomans <kristof@coomans.eu>
2014-11-13 20:50:30 +0100

    iii-78: Work in progress on the EventLD read model

---
+++ src/Event/EventLDProjector.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Event;
+
+use Broadway\Domain\DomainMessageInterface;
+use Broadway\ReadModel\Projector;
+use CultuurNet\UDB3\Event\ReadModel\JsonDocument;
+use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
+use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use Broadway\Repository\RepositoryInterface as SourceRepository;
+
+class EventLDProjector extends Projector
+{
+    /**
+     * @var DocumentRepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * @var IriGeneratorInterface
+     */
+    protected $iriGenerator;
+
+    /**
+     * @param DocumentRepositoryInterface $repository
+     * @param IriGeneratorInterface $iriGenerator
+     */
+    public function __construct(
+        DocumentRepositoryInterface $repository,
+        IriGeneratorInterface $iriGenerator
+    ) {
+        $this->repository = $repository;
+        $this->iriGenerator = $iriGenerator;
+    }
+
+    public function handle(DomainMessageInterface $domainMessage)
+    {
+        return parent::handle(
+            $domainMessage
+        );
+    }
+
+
+    /**
+     * @param EventImportedFromUDB2 $eventImportedFromUDB2
+     */
+    protected function applyEventImportedFromUDB(
+        EventImportedFromUDB2 $eventImportedFromUDB2
+    ) {
+        $eventLd = new \stdClass();
+
+
+        // @todo set @context
+        // @todo load event from cdbxml and set properties on eventLd object
+        //$eventLdModel->name =
+        //$eventLdModel->calendarSummary =
+        //$eventLdModel->concept[] =
+
+        $eventLdModel = new JsonDocument(
+            $eventImportedFromUDB2->getEventId()
+        );
+
+        $this->repository->save($eventLdModel->withBody($eventLd));
+    }
+
+    /**
+     * @param EventCreated $eventCreated
+     */
+    protected function applyEventCreated(EventCreated $eventCreated)
+    {
+        // @todo This just creates an empty event. Should we do anything here?
+    }
+
+    /**
+     * @param EventWasTagged $eventTagged
+     */
+    protected function applyEventWasTagged(EventWasTagged $eventTagged)
+    {
+        $document = $this->loadDocumentFromRepository($eventTagged);
+
+        $eventLd = $document->getBody();
+        $eventLd->concept[] = $eventTagged->getKeyword();
+
+        $this->repository->save($document->withBody($eventLd));
+    }
+
+    /**
+     * @param EventEvent $event
+     * @return JsonDocument
+     */
+    protected function loadDocumentFromRepository(EventEvent $event) {
+        $document = $this->repository->get($event->getEventId());
+
+        if (!$document) {
+            $document = new JsonDocument($event->getEventId());
+            $eventLd = $document->getBody();
+            $eventLd->{'@id'} = $this->iriGenerator->iri($event->getEventId());
+            $document = $document->withBody($eventLd);
+        }
+
+        return $document;
+    }
+} 
---
+++ src/Event/ReadModel/DoctrineCacheRepository.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Event\ReadModel;
+
+use Doctrine\Common\Cache\Cache;
+
+class DoctrineCacheRepository implements DocumentRepositoryInterface
+{
+    protected $cache;
+
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function get($id)
+    {
+        $value = $this->cache->fetch($id);
+        return new JsonDocument($id, $value);
+    }
+
+    public function save(JsonDocument $document)
+    {
+        $this->cache->save($document->getId(), $document->getRawBody(), 0);
+    }
+}

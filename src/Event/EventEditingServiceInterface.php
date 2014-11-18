10abe4838f612b36b60e5cb082a248b682e2f66d
Kristof Coomans <kristof@coomans.eu>
2014-11-18 12:08:13 +0100

    iii-89: Define EventEditingServiceInterface

---
+++ src/Event/EventEditingServiceInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Event;
+
+
+use CultuurNet\UDB3\EventNotFoundException;
+use CultuurNet\UDB3\Language;
+
+interface EventEditingServiceInterface
+{
+    /**
+     * @param string $eventId
+     * @param Language $language
+     * @param string $title
+     * @return string command id
+     * @throws EventNotFoundException
+     */
+    public function translateTitle($eventId, Language $language, $title);
+
+    /**
+     * @param string $eventId
+     * @param Language $language
+     * @param string $description
+     * @return string command id
+     * @throws EventNotFoundException
+     */
+    public function translateDescription($eventId, Language $language, $description);
+
+} 

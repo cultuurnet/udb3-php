48529f97f7c4305a9e5b897f3e9ea51a5e525976
Kristof Coomans <kristof@coomans.eu>
2014-11-19 15:44:59 +0100

    iii-97: Add keyword removal method to improved entry API (unfortunately doesn't work yet, gives 401 Unauthorized)

--- src/UDB2/EntryAPI.php
+++ src/UDB2/EntryAPI.php
@@ -7,7 +7,9 @@ namespace CultuurNet\UDB3\UDB2;
 
 
 use CultuurNet\Auth\Guzzle\OAuthProtectedService;
+use CultuurNet\UDB3\Keyword;
 use CultuurNet\UDB3\Language;
+use Guzzle\Http\Message\EntityEnclosingRequest;
 
 class EntryAPI extends OAuthProtectedService
 {
@@ -15,9 +17,18 @@ class EntryAPI extends OAuthProtectedService
 
     const TRANSLATION_CREATED = 'TranslationCreated';
 
+    const KEYWORD_WITHDRAWN = 'KeywordWithdrawn';
+
+    const KEYWORD_PRIVATE = 'PrivateKeyword';
+
     protected function eventTranslationPath($eventId)
     {
         return "event/{$eventId}/translations";
+    }
+
+    protected function eventKeywordsPath($eventId)
+    {
+        return "event/{$eventId}/keywords";
     }
 
     /**
@@ -73,7 +84,38 @@ class EntryAPI extends OAuthProtectedService
         return $rsp;
     }
 
-    public function guardTranslationResponseIsSuccessful(Rsp $rsp)
+    /**
+     * @param string $eventId
+     * @param Keyword $keyword
+     * @return Rsp
+     * @throws UnexpectedKeywordDeleteErrorException
+     */
+    public function deleteKeyword($eventId, Keyword $keyword)
+    {
+        /** @var EntityEnclosingRequest $request */
+        $request = $this->getClient()->delete(
+            $this->eventKeywordsPath($eventId)
+        );
+
+        $request->getPostFields()->add('keyword', (string)$keyword);
+
+        $response = $request->send();
+
+        $rsp = Rsp::fromResponseBody($response->getBody(true));
+
+        $this->guardDeleteKeywordResponseIsSuccessful($rsp);
+
+        return $rsp;
+    }
+
+    private function guardDeleteKeywordResponseIsSuccessful(Rsp $rsp)
+    {
+        if ($rsp->getCode() !== self::KEYWORD_WITHDRAWN) {
+            throw new UnexpectedKeywordDeleteErrorException($rsp);
+        }
+    }
+
+    private function guardTranslationResponseIsSuccessful(Rsp $rsp)
     {
         $validCodes = [
             self::TRANSLATION_CREATED,

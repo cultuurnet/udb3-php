b308dad6af7a25b94357575c5c4c7e10c25c73aa
Bram Cordie <bramcordie@gmail.com>
2014-12-09 14:36:52 +0100

    Ignore organiser info if organiser or contact info is not available

--- src/Event/EventLDProjector.php
+++ src/Event/EventLDProjector.php
@@ -109,22 +109,26 @@ class EventLDProjector extends Projector
         $eventLd->location = $location;
 
         // Organiser.
-        $organiser = array();
+
         $organiser_cdb = $udb2Event->getOrganiser();
         $contact_info_cdb = $udb2Event->getContactInfo();
-        $organiser['name'] = $organiser_cdb->getLabel();
-        $organiser['email'] = array();
-        $mails = $contact_info_cdb->getMails();
-        foreach ($mails as $mail) {
-          $organiser['email'][] = $mail->getMailAddress();
+
+        if($organiser_cdb && $contact_info_cdb) {
+          $organiser = array();
+          $organiser['name'] = $organiser_cdb->getLabel();
+          $organiser['email'] = array();
+          $mails = $contact_info_cdb->getMails();
+          foreach ($mails as $mail) {
+            $organiser['email'][] = $mail->getMailAddress();
+          }
+          $organiser['phone'] = array();
+          /** @var \CultureFeed_Cdb_Data_Phone[] $phones */
+          $phones = $contact_info_cdb->getPhones();
+          foreach ($phones as $phone) {
+            $organiser['phone'][] = $phone->getNumber();
+          }
+          $eventLd->organiser = $organiser;
         }
-        $organiser['phone'] = array();
-        /** @var \CultureFeed_Cdb_Data_Phone[] $phones */
-        $phones = $contact_info_cdb->getPhones();
-        foreach ($phones as $phone) {
-          $organiser['phone'][] = $phone->getNumber();
-        }
-        $eventLd->organiser = $organiser;
 
         // booking info
         $bookingInfo = array();

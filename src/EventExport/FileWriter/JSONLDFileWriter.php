8ae13d94fe145381364c09a1507e36549bddf4b8
Kristof Coomans <kristof@coomans.eu>
2015-02-11 11:55:04 +0100

    III-160: Fix some coding standard violations and remove a remaining var_dump()

--- src/EventExport/FileWriter/JSONLDFileWriter.php
+++ src/EventExport/FileWriter/JSONLDFileWriter.php
@@ -15,21 +15,28 @@ class JSONLDFileWriter implements FileWriterInterface
      */
     protected $includedProperties;
 
-    public function __construct($filePath, $include = null) {
+    public function __construct($filePath, $include = null)
+    {
         $this->f = fopen($filePath, 'w');
         if (false === $this->f) {
-            throw new \RuntimeException('Unable to open file for writing: ' . $filePath);
+            throw new \RuntimeException(
+                'Unable to open file for writing: ' . $filePath
+            );
         }
         fwrite($this->f, '[');
 
         $this->first = true;
 
-        if($include) {
+        if ($include) {
             $include[] = '@id';
             // The address property is nested inside location.
             // The whole location property gets included instead of pulling it
             // out and placing it directly on the object.
-            if(in_array('address', $include) && !in_array('location', $include)) {
+            if (in_array('address', $include) && !in_array(
+                    'location',
+                    $include
+                )
+            ) {
                 array_push($include, 'location');
             }
             $this->includedProperties = $include;
@@ -44,16 +51,14 @@ class JSONLDFileWriter implements FileWriterInterface
     {
         if ($this->first) {
             $this->first = false;
-        }
-        else {
+        } else {
             fwrite($this->f, ',');
         }
 
-        if($this->includedProperties) {
+        if ($this->includedProperties) {
             $eventObject = json_decode($event);
-            foreach($eventObject as $propertyName => $value) {
-                var_dump($propertyName);
-                if(!in_array($propertyName, $this->includedProperties)) {
+            foreach ($eventObject as $propertyName => $value) {
+                if (!in_array($propertyName, $this->includedProperties)) {
                     unset($eventObject->{$propertyName});
                 }
             }
@@ -63,7 +68,8 @@ class JSONLDFileWriter implements FileWriterInterface
         fwrite($this->f, $event);
     }
 
-    public function close() {
+    public function close()
+    {
         if (is_resource($this->f)) {
             fwrite($this->f, ']');
 

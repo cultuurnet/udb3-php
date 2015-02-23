a3f74162ef18c7fec601a53322e6967fa6b2dcaa
Bram Cordie <bramcordie@gmail.com>
2015-02-23 14:20:28 +0100

    III-168: Move formatting of event for export to separate class with test

--- src/EventExport/FileWriter/JSONLDFileWriter.php
+++ src/EventExport/FileWriter/JSONLDFileWriter.php
@@ -10,14 +10,9 @@ class JSONLDFileWriter implements FileWriterInterface
     protected $f;
 
     /**
-     * @var string[]
+     * @var JSONLDEventFormatter
      */
-    protected $includedProperties;
-
-    /**
-     * @var string[]
-     */
-    protected $includedTerms;
+    protected $eventFormatter;
 
     public function __construct($filePath, $include = null)
     {
@@ -29,45 +24,9 @@ class JSONLDFileWriter implements FileWriterInterface
         }
         fwrite($this->f, '[');
 
+        $this->eventFormatter = new JSONLDEventFormatter($inlcude);
+
         $this->first = true;
-
-        if ($include) {
-            $include[] = '@id';
-            // The address property is nested inside location.
-            // The whole location property gets included instead of pulling it
-            // out and placing it directly on the object.
-            if (in_array('address', $include) &&
-                !in_array('location', $include)
-            ) {
-                array_push($include, 'location');
-            }
-
-            $terms = $this->filterTermsFromProperties($include);
-            if (count($terms) > 0) {
-                $this->includedTerms = $terms;
-                $include[] = 'terms';
-            }
-
-            $this->includedProperties = $include;
-        }
-
-    }
-
-    private function filterTermsFromProperties($properties)
-    {
-        $termPrefix = 'terms.';
-
-        $prefixedTerms = array_filter(
-            $properties,
-            function($property) use ($termPrefix) {
-                return strpos($property, $termPrefix) === 0;
-            }
-        );
-        $terms = array_map(function($term) use ($termPrefix) {
-            return str_replace($termPrefix, "", $term);
-        }, $prefixedTerms);
-
-        return $terms;
     }
 
     /**
@@ -80,38 +39,10 @@ class JSONLDFileWriter implements FileWriterInterface
         } else {
             fwrite($this->f, ',');
         }
-        
-        $includedProperties = $this->includedProperties;
-        $includedTerms = $this->includedTerms;
 
-        if ($includedProperties) {
-            $eventObject = json_decode($event);
+        $formattedEvent = $this->eventFormatter->formatEvent($event);
 
-            // filter out terms
-            if (property_exists($eventObject, 'terms') && $includedTerms) {
-                $filteredTerms = array_filter(
-                    $eventObject->terms,
-                    function ($term) use ($includedTerms) {
-                        return in_array($term->domain, $includedTerms);
-                    }
-                );
-
-                $eventObject->terms = array_values($filteredTerms);
-            }
-            
-            // filter out base propoerties
-            foreach ($eventObject as $propertyName => $value) {
-                if (!in_array($propertyName, $includedProperties)) {
-                    unset($eventObject->{$propertyName});
-                }
-            }
-
-            
-
-            $event = json_encode($eventObject);
-        }
-
-        fwrite($this->f, $event);
+        fwrite($this->f, $formattedEvent);
     }
 
     public function close()

07bd75a070369f82f431b945bdefd340eeffbcf7
Kristof Coomans <kristof@coomans.eu>
2015-02-25 21:57:23 +0100

    III-196: Implement LoggerAwareInterface and re-use the same channel

--- src/UDB2/AMQP/EventBusForwardingConsumer.php
+++ src/UDB2/AMQP/EventBusForwardingConsumer.php
@@ -11,16 +11,22 @@ use Broadway\Domain\DomainMessage;
 use Broadway\Domain\Metadata;
 use Broadway\EventHandling\EventBusInterface;
 use CultuurNet\Deserializer\DeserializerLocatorInterface;
+use CultuurNet\Deserializer\DeserializerNotFoundException;
+use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Rhumsaa\Uuid\Uuid;
 use ValueObjects\String\String;
 
 /**
  * Forwards messages coming in via AMQP to an event bus.
  */
-class EventBusForwardingConsumer
+class EventBusForwardingConsumer implements LoggerAwareInterface
 {
+    use LoggerAwareTrait;
+
     /**
      * @var AMQPStreamConnection
      */
@@ -47,6 +53,11 @@ class EventBusForwardingConsumer
     private $deserializerLocator;
 
     /**
+     * @var AMQPChannel
+     */
+    private $channel;
+
+    /**
      * @param AMQPStreamConnection $connection
      * @param EventBusInterface $eventBus
      */
@@ -56,10 +67,14 @@ class EventBusForwardingConsumer
         DeserializerLocatorInterface $deserializerLocator
     ) {
         $this->connection = $connection;
+        $this->channel = $connection->channel();
+
+        $this->eventBus = $eventBus;
+
         $this->deserializerLocator = $deserializerLocator;
 
         $this->queueName = 'udb3.q.udb2';
-        $this->consumerTag = 'udb3-' . $_SERVER['SERVER_NAME'];
+        $this->consumerTag = 'udb3';
 
         $this->declareQueue();
         $this->registerConsumeCallback();
@@ -67,12 +82,36 @@ class EventBusForwardingConsumer
 
     public function consume(AMQPMessage $message)
     {
+        if ($this->logger) {
+            $this->logger->info('received message with content-type ' . $message->get('content_type'));
+        }
+
         $contentType = new String($message->get('content_type'));
 
-        $deserializer = $this->deserializerLocator->getDeserializerForContentType($contentType);
-        $event = $deserializer->deserialize(
-            new String($message->body)
-        );
+        try {
+            $deserializer = $this->deserializerLocator->getDeserializerForContentType(
+                $contentType
+            );
+            $event = $deserializer->deserialize(
+                new String($message->body)
+            );
+        }
+        catch (\Exception $e) {
+            if ($this->logger) {
+                $this->logger->error($e->getMessage());
+            }
+
+            $message->delivery_info['channel']->basic_reject(
+                $message->delivery_info['delivery_tag'],
+                false
+            );
+
+            if ($this->logger) {
+                $this->logger->info('message rejected');
+            }
+
+            return;
+        }
 
         $events = [
             new DomainMessage(
@@ -85,41 +124,52 @@ class EventBusForwardingConsumer
         ];
 
         $stream = new DomainEventStream($events);
+
+        if ($this->logger) {
+            $this->logger->info('passing on message to event bus');
+        }
+
         $this->eventBus->publish(
             $stream
         );
+
+        $message->delivery_info['channel']->basic_ack(
+            $message->delivery_info['delivery_tag']
+        );
+
+        if ($this->logger) {
+            $this->logger->info('message acknowledged');
+        }
     }
 
     protected function declareQueue()
     {
-        $passive = false;
-        $durable = true;
-        $exclusive = false;
-        $autoDelete = false;
+        $this->channel->queue_declare(
+            $this->queueName,
+            $passive = false,
+            $durable = true,
+            $exclusive = false,
+            $autoDelete = false
+        );
 
-        $this->connection
-            ->channel()
-            ->queue_declare(
-                $this->queueName,
-                $passive,
-                $durable,
-                $exclusive,
-                $autoDelete
-            );
+        $this->channel->queue_bind(
+            $this->queueName,
+            'udb2',
+            $routingKey = '#'
+        );
     }
 
     protected function registerConsumeCallback()
     {
-        $this->connection
-            ->channel()
-            ->basic_consume(
-                $this->queueName,
-                $consumerTag = $this->consumerTag,
-                $noLocal = false,
-                $noAck = false,
-                $exclusive = false,
-                $noWait = false,
-                [$this, 'consume']
-            );
+        $handler = $this;
+        $this->channel->basic_consume(
+            $this->queueName,
+            $consumerTag = $this->consumerTag,
+            $noLocal = false,
+            $noAck = false,
+            $exclusive = false,
+            $noWait = false,
+            [$this, 'consume']
+        );
     }
 }

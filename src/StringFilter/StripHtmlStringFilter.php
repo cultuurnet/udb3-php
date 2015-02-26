3baf170471803d6d5cc960938a8cf9117488f147
Bert Ramakers <bertramakers@me.com>
2015-02-26 11:45:20 +0100

    III-173: Paragraphs are now appended with two newlines instead of one, and the maximum number of consecutive newlines is restricted to two.

--- src/StringFilter/StripHtmlStringFilter.php
+++ src/StringFilter/StripHtmlStringFilter.php
@@ -5,21 +5,34 @@ namespace CultuurNet\UDB3\StringFilter;
 class StripHtmlStringFilter implements StringFilterInterface
 {
     /**
+     * Flag to indicate that a tag is self-closing.
+     */
+    const TAG_SELF_CLOSING = 1;
+
+    /**
+     * Flag to indicate that 2 newlines should be appended after a tag.
+     */
+    const TAG_APPEND_DOUBLE_NEWLINE = 2;
+
+    /**
      * {@inheritdoc}
      */
     public function filter($string)
     {
         // Add newlines after break tags.
-        $string = $this->addNewLinesAfterClosingTags($string, 'br', true);
+        $string = $this->addNewlinesAfterClosingTags($string, 'br', self::TAG_SELF_CLOSING);
 
         // Add newlines after closing paragraph tags.
-        $string = $this->addNewLinesAfterClosingTags($string, 'p');
+        $string = $this->addNewlinesAfterClosingTags($string, 'p', self::TAG_APPEND_DOUBLE_NEWLINE);
 
         // Decode all HTML entities, like &amp;, so they are human-readable.
         $string = html_entity_decode($string);
 
         // Strip all HTML tags.
         $string = strip_tags($string);
+
+        // Remove any excessive consecutive newlines.
+        $string = $this->limitConsecutiveNewlines($string, 2);
 
         // Trim any whitespace or newlines from the start and/or end of the string.
         $string = trim($string);
@@ -33,39 +46,70 @@ class StripHtmlStringFilter implements StringFilterInterface
      * @param string $string
      *   String to add newlines to.
      * @param string $tag
-     *   Tag name. For example "br" to add a newline after each "<br />" or "<br>".
-     * @param bool $selfClosing
-     *   Indicates whether the tag is self-closing or not.
+     *   Tag name. For example "br" to add a newline after each "<br />" or "<br>" (if self-closing flag is set), or
+     *   "p" to add a newline after each "</p>" (if not self-closing).
+     * @param int $flags
+     *   Optional flags to set.
      *
      * @return string
      *   Processed string.
      */
-    protected function addNewLinesAfterClosingTags($string, $tag, $selfClosing = false)
+    protected function addNewlinesAfterClosingTags($string, $tag, $flags = 0)
     {
         // Start of the pattern.
         $pattern = '/';
 
-        if ($selfClosing) {
+        if ($flags & self::TAG_SELF_CLOSING) {
             // Find the self-closing tag, including its attributes and optionally a closing slash.
-            $pattern .= '<' . $tag . '\\s*[\\/]?>';
+            // .*? means: Get any characters, 0 or more, but non-greedy so stop when the first / or > is encountered.
+            $pattern .= '(<' . $tag . '.*?[\\/]?>)';
         } else {
             // Find the closing tag.
-            $pattern .= '<\\/' . $tag . '>';
+            $pattern .= '(<\\/' . $tag . '>)';
         }
 
-        // Make sure the character after the tag is NOT a newline, or that there's no character after the tag. Use a
-        // lookahead to make sure the next character is not added to the matched string if it's not a newline or the end
-        // of the string.
-        $pattern .= '(?=[^\\n]|$)';
+        // Capture any newlines after the tag as well.
+        $pattern .= '([\\n]*)';
 
         // End of the pattern. Use i to make it case-insensitive, as HTML tags can be both uppercase and lowercase.
         $pattern .= '/i';
 
-        // Append all pattern matches with a newline character.
-        $string = preg_replace_callback($pattern, function($match) {
-            // Return the whole match (first value) appended by a newline character.
-            return $match[0] . PHP_EOL;
+        // Append all pattern matches with a newline character (or 2 if specified).
+        $newlines = ($flags & self::TAG_APPEND_DOUBLE_NEWLINE) ? PHP_EOL . PHP_EOL : PHP_EOL;
+
+        // Loop over all matching tags from the string.
+        return preg_replace_callback($pattern, function($match) use ($newlines) {
+            // Return the tag appended by the specified amount of newlines. Note that $match[0] is the full captured
+            // match, so it also includes the newlines after the tag. $match[1] is just the tag itself, and $match[2]
+            // are the newlines following it (if any).
+            return $match[1] . $newlines;
         }, $string);
-        return $string;
+    }
+
+    /**
+     * Restricts the number of consecutive newlines in a specific string.
+     *
+     * @param string $string
+     *   String to limit consecutive newlines in.
+     * @param int $limit
+     *   Limit of consecutive newlines. (Defaults to 2.)
+     *
+     * @return string
+     *   Processed string.
+     */
+    protected function limitConsecutiveNewlines($string, $limit = 2)
+    {
+        // Pattern that finds any consecutive newlines that exceed the allowed limit.
+        $exceeded = $limit + 1;
+        $pattern = '/((\\n){' . $exceeded . ',})/';
+
+        // Create a string with the maximum number of allowed newlines.
+        $newlines = '';
+        for ($i = 0; $i < $limit; $i++) {
+            $newlines .= PHP_EOL;
+        }
+
+        // Find each match and replace it with the maximum number of newlines.
+        return preg_replace($pattern, $newlines, $string);
     }
 }
--- test/StringFilter/StripHtmlStringFilterTest.php
+++ test/StringFilter/StripHtmlStringFilterTest.php
@@ -29,13 +29,13 @@ class StripHtmlStringFilterTest extends \PHPUnit_Framework_TestCase
         return array(
             array('<span>Lorem ipsum</span> <strong>dolor</strong>.', 'Lorem ipsum dolor.'),
             array('Lorem &amp; ipsum.', 'Lorem & ipsum.'),
-            array('Lorem & impsum.', 'Lorem & impsum.'),
-            array('<p>Line 1.</p><p>Line 2.</p>', $single_newline),
-            array('<p>Line 1.</p>' . PHP_EOL . '<p>Line 2.</p>', $single_newline),
+            array('Lorem & ipsum.', 'Lorem & ipsum.'),
             array('Line 1.<br />Line 2.<br />', $single_newline),
             array('Line 1.<br />' . PHP_EOL . 'Line 2.', $single_newline),
-            array('<p>Line 1.</p><br /><p>Line 2.</p><br /><br />', $double_newline),
             array('<br />Line 1.<br /><br />Line 2.<br />', $double_newline),
+            array('<p>Line 1.</p><p>Line 2.</p>', $double_newline),
+            array('<p>Line 1.</p>' . PHP_EOL . '<p>Line 2.</p>', $double_newline),
+            array('<p>Line 1.</p><p>Line 2.</p><br /><br />', $double_newline),
         );
     }
 }

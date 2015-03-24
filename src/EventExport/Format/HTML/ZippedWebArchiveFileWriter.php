ad0ebc35eaf46f65c42915f619430e47e0e451d1
Kristof Coomans <kristof@coomans.eu>
2015-03-24 18:14:48 +0100

    III-238: Fix Use FlySystem zip adapter instead of Zippy to generate the zip file

--- src/EventExport/Format/HTML/ZippedWebArchiveFileWriter.php
+++ src/EventExport/Format/HTML/ZippedWebArchiveFileWriter.php
@@ -2,7 +2,8 @@
 
 namespace CultuurNet\UDB3\EventExport\Format\HTML;
 
-use Alchemy\Zippy\Zippy;
+use League\Flysystem\Filesystem;
+use League\Flysystem\ZipArchive\ZipArchiveAdapter;
 
 /**
  * Creates a ZIP archive of a an HTML export and all needed assets.
@@ -18,14 +19,55 @@ class ZippedWebArchiveFileWriter extends WebArchiveFileWriter
     {
         $directory = $this->createWebArchiveDirectory($events);
 
-        $zippy = Zippy::load();
-        $zippy->create(
-            $filePath,
-            [
-                'html' => $this->expandTmpPath($directory)
-            ]
-        );
+        $this->zipDirectory($directory, $filePath);
 
         $this->removeTemporaryArchiveDirectory($directory);
+    }
+
+    protected function zipDirectory($directory, $filePath)
+    {
+        $zipArchive = new ZipArchiveAdapter(
+            $filePath
+        );
+
+        $this->mountManager->mountFilesystem(
+            'zip',
+            new Filesystem(
+                $zipArchive
+            )
+        );
+
+        $webArchiveFiles = $this->mountManager->listContents(
+            'tmp://' . $directory,
+            true
+        );
+
+        foreach ($webArchiveFiles as $file) {
+            if ($file['type'] !== 'file') {
+                continue;
+            }
+
+            $from = $file['filesystem'] . '://' . $file['path'];
+
+            $pathWithoutUpperDirectory = $this->pathWithoutUpperDirectory(
+                $file['path']
+            );
+            $to = 'zip://html/' . $pathWithoutUpperDirectory;
+
+
+            $this->mountManager->copy($from, $to);
+        }
+
+        // Need to close the archive in order to actually save it.
+        $zipArchive->getArchive()->close();
+    }
+
+    protected function pathWithoutUpperDirectory($file)
+    {
+        $pos = strpos($file, '/');
+
+        if (false !== $pos) {
+            return substr($file, $pos);
+        }
     }
 }

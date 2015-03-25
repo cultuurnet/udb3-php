c4ef26f4869f3b830efd9212a1be42cf817e6c14
Bert Ramakers <bertramakers@me.com>
2015-03-25 11:31:25 +0100

    III-238: Removed commented code.

--- src/EventExport/Format/HTML/EventFormatter.php
+++ src/EventExport/Format/HTML/EventFormatter.php
@@ -69,22 +69,6 @@ class EventFormatter
 
         $formattedEvent['dates'] = $event->calendarSummary;
 
-        /*
-        'image' => 'http://media.uitdatabank.be/20140715/p18qn74oth1uvnnpidhj1i6t1f9p1.png',
-        'type' => 'Cursus of workshop',
-        'title' => 'De muziek van de middeleeuwen // Een middeleeuwse muziekgeschiedenis in veertig toppers',
-        'description' => 'Alhoewel de middeleeuwen zo’n duizend jaar duurden, is het grootste deel van de ...',
-        'dates' => 'ma 22/09/14 van 10:00 tot 12:30  ma 2...',
-        'address' => array(
-            'name' => 'CC De Werf',
-            'street' => 'Molenstraat',
-            'number' => '51',
-            'postcode' => '9300',
-            'municipality' => 'Aalst',
-        ),
-        'price' => '119,0'
-        */
-
         return $formattedEvent;
     }
 }

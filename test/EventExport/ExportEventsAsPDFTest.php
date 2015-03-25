4c0bb531f4fc0ba7725c6f460f102bd0b67baa8e
Bram Cordie <bramcordie@gmail.com>
2015-03-25 11:27:35 +0100

    III-217: Test event export command for allowing a publisher, subtitle and footer

--- test/EventExport/ExportEventsAsPDFTest.php
+++ test/EventExport/ExportEventsAsPDFTest.php
@@ -4,14 +4,23 @@ namespace CultuurNet\UDB3\EventExport\Command;
 
 use CultuurNet\UDB3\EventExport\EventExportQuery;
 use CultuurNet\UDB3\EventExport\HTML\Brand;
+use CultuurNet\UDB3\EventExport\HTML\Footer;
+use CultuurNet\UDB3\EventExport\HTML\Publisher;
+use CultuurNet\UDB3\EventExport\HTML\Subtitle;
 use CultuurNet\UDB3\EventExport\HTML\Title;
 use ValueObjects\Web\EmailAddress;
 
 class ExportEventsAsPDFTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var ExportEventsAsPDF
+     */
     private $export;
 
+    /**
+     * @var ExportEventsAsPDF
+     */
     private $clonedExport;
 
     public function setUp()
@@ -32,9 +41,44 @@ class ExportEventsAsPDFTest extends \PHPUnit_Framework_TestCase
         $email = new EmailAddress('john@doe.com');
         $newExport = $this->export->withEmailNotificationTo($email);
 
-        $this->assertNotSame($newExport, $this->export);
         $this->assertEquals($email, $newExport->getAddress());
 
+        $this->assertNotModified($newExport);
+    }
+
+    public function it_allows_to_specify_a_subtitle()
+    {
+        $subtitle = new Subtitle('Some subtitle');
+        $newExport = $this->export->withSubtitle($subtitle);
+
+        $this->assertEquals($subtitle, $newExport->getSubtitle());
+
+        $this->assertNotModified($newExport);
+    }
+
+    public function it_allows_to_specify_a_footer()
+    {
+        $footer = new Footer('footer text');
+        $newExport = $this->export->withFooter($footer);
+
+        $this->assertEquals($footer, $newExport->getFooter());
+
+        $this->assertNotModified($newExport);
+    }
+
+    public function it_allows_to_specify_a_publisher()
+    {
+        $publisher = new Publisher('footer text');
+        $newExport = $this->export->withPublisher($publisher);
+
+        $this->assertEquals($publisher, $newExport->getFooter());
+
+        $this->assertNotModified($newExport);
+    }
+
+
+    private function assertNotModified($newExport) {
+        $this->assertNotSame($newExport, $this->export);
         $this->assertEquals($this->clonedExport, $this->export);
     }
 }

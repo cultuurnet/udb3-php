fe99f6e2552e7588f338935b9b908acd123621ce
Bram Cordie <bramcordie@gmail.com>
2015-03-25 13:53:16 +0100

    III-217: Test if PDF json deserializer expects a query, brand and title property

--- test/EventExport/PDF/ExportEventsAsPDFJSONDeserializerTest.php
+++ test/EventExport/PDF/ExportEventsAsPDFJSONDeserializerTest.php
@@ -3,6 +3,7 @@
 namespace CultuurNet\UDB3\EventExport\Command;
 
 use ValueObjects\String\String;
+use CultuurNet\Deserializer\MissingValueException;
 
 class ExportEventsAsPDFJSONDeserializerTest extends \PHPUnit_Framework_TestCase
 {
@@ -22,19 +23,52 @@ class ExportEventsAsPDFJSONDeserializerTest extends \PHPUnit_Framework_TestCase
      */
     public function it_returns_a_PDF_export_command()
     {
-        $PDFExportData = new String($this->getFileContent('pdf_export_data.json'));
-        $command = $this->deserializer->deserialize($PDFExportData);
+        $exportData = $this->getJSONStringFromFile('export_data.json');
+        $command = $this->deserializer->deserialize($exportData);
 
         $this->assertInstanceOf(ExportEventsAsPDF::class, $command);
 
     }
 
-    private function getFileContent($fileName)
+    /**
+     * @test
+     */
+    public function it_expects_a_query_property()
+    {
+        $exportData = $this->getJSONStringFromFile('export_data_without_query.json');
+
+        $this->setExpectedException(MissingValueException::class, 'query is missing');
+        $this->deserializer->deserialize($exportData);
+    }
+
+    /**
+     * @test
+     */
+    public function it_expects_a_brand_property()
+    {
+        $exportData = $this->getJSONStringFromFile('export_data_without_brand.json');
+
+        $this->setExpectedException(MissingValueException::class, 'brand is missing');
+        $this->deserializer->deserialize($exportData);
+    }
+
+    /**
+     * @test
+     */
+    public function it_expects_a_title_property()
+    {
+        $exportData = $this->getJSONStringFromFile('export_data_without_title.json');
+
+        $this->setExpectedException(MissingValueException::class, 'title is missing');
+        $this->deserializer->deserialize($exportData);
+    }
+
+    private function getJSONStringFromFile($fileName)
     {
         $json = file_get_contents(
             __DIR__ . '/' . $fileName
         );
 
-        return $json;
+        return new String($json);
     }
 }

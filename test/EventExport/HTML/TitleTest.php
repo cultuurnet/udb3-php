ec979a8c05daf9afc813a10d5fdecc981267a939
Bram Cordie <bramcordie@gmail.com>
2015-03-25 15:09:43 +0100

    III-217: Test Title length

---
+++ test/EventExport/HTML/TitleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CultuurNet\UDB3\EventExport\HTML;
+
+class TitleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_be_at_least_one_character_long()
+    {
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Title('');
+    }
+}

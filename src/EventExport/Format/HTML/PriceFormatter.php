c864059b728c08b610d8dc45d985169d376b38bb
Bert Ramakers <bertramakers@me.com>
2015-04-01 16:34:38 +0200

    III-216: Tests and bugfixes for PriceFormatter class.

--- src/EventExport/Format/HTML/PriceFormatter.php
+++ src/EventExport/Format/HTML/PriceFormatter.php
@@ -51,7 +51,7 @@ class PriceFormatter
     }
 
     /**
-     * @param mixed $price
+     * @param float|int $price
      * @return string $price
      */
     public function format($price)
@@ -64,17 +64,23 @@ class PriceFormatter
             $this->thousandsSeparator
         );
 
-        // Return the "free" label if enabled and the price is 0.
-        if ($price === '0,00' && $this->useFreeLabel) {
-            return $this->freeLabel;
+        // Check that the price is zero after rounding the decimals.
+        if ($price === "0{$this->decimalPoint}00") {
+            if ($this->useFreeLabel) {
+                // Return the "free" label if enabled.
+                return $this->freeLabel;
+            } else {
+                // Otherwise simply return zero without decimals, as we only show significant decimals anyway.
+                return '0';
+            }
         }
 
         // Trim any insignificant zeroes after the decimal point.
         $price = trim($price, 0);
 
-        // Trim the comma if there were only zeroes after the decimal point. Don't do this in the same trim as above, as
-        // that would format 50,00 as 5.
-        $price = trim($price, ',');
+        // Trim the decimal point if there were only zeroes after the decimal point. Don't do this in the same trim as
+        // above, as that would format 50,00 as 5.
+        $price = trim($price, $this->decimalPoint);
 
         return $price;
     }

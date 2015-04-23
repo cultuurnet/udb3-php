943fdbbb6b062917c51bcd543e41c3f7b6a20854
Bert Ramakers <bertramakers@me.com>
2015-04-23 09:53:17 +0200

    III-288: EventCalendarProjector can now save the Calendar of imported of updated UDB2 events.

--- src/Event/EventCalendarProjector.php
+++ src/Event/EventCalendarProjector.php
@@ -3,6 +3,11 @@
 namespace CultuurNet\UDB3\Event;
 
 use Broadway\EventHandling\EventListenerInterface;
+use \CultureFeed_Cdb_Item_Event as Event;
+use CultuurNet\UDB3\Cdb\EventItemFactory;
+use CultuurNet\UDB3\Event\Events\EventCdbXMLInterface;
+use CultuurNet\UDB3\Event\Events\EventImportedFromUDB2;
+use CultuurNet\UDB3\Event\Events\EventUpdatedFromUDB2;
 use CultuurNet\UDB3\Event\ReadModel\CacheCalendarRepository;
 use CultuurNet\UDB3\EventHandling\DelegateEventHandlingToSpecificMethodTrait;
 
@@ -21,5 +26,36 @@ class EventCalendarProjector implements EventListenerInterface
     public function __construct(CacheCalendarRepository $repository)
     {
         $this->repository = $repository;
+    }
+
+    /**
+     * @param EventImportedFromUDB2 $eventImportedFromUDB2
+     */
+    public function applyEventImportedFromUDB2(EventImportedFromUDB2 $eventImportedFromUDB2)
+    {
+        $this->saveEventCalendar($eventImportedFromUDB2);
+    }
+
+    /**
+     * @param EventUpdatedFromUDB2 $eventUpdatedFromUDB2
+     */
+    public function applyEventUpdatedFromUDB2(EventUpdatedFromUDB2 $eventUpdatedFromUDB2)
+    {
+        $this->saveEventCalendar($eventUpdatedFromUDB2);
+    }
+
+    /**
+     * @param EventCdbXMLInterface $event
+     */
+    private function saveEventCalendar(EventCdbXMLInterface $eventEvent)
+    {
+        $eventId = $eventEvent->getEventId();
+
+        $event = EventItemFactory::createEventFromCdbXml(
+            $eventEvent->getCdbXmlNamespaceUri(),
+            $eventEvent->getCdbXml()
+        );
+
+        $this->repository->save($eventId, $event->getCalendar());
     }
 }

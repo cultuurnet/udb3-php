4ed670e75abe750b45d4503a9c258d47b833f6ff
Bert Ramakers <bertramakers@me.com>
2015-04-24 14:35:15 +0200

    III-7: Command to subscribe to a saved search.

---
+++ src/SavedSearch/Command/SubscribeToSavedSearch.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace CultuurNet\UDB3\SavedSearch\Command;
+
+use \CultureFeed_SavedSearches_SavedSearch as SavedSearch;
+
+class SubscribeToSavedSearch
+{
+    /**
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $query;
+
+    /**
+     * @var string
+     */
+    protected $frequency;
+
+    /**
+     * @param string $userId
+     * @param string $name
+     * @param string $query
+     *
+     * @throws \InvalidArgumentException
+     *   When an invalid frequency value is given.
+     */
+    public function __construct($userId, $name, $query, $frequency = SavedSearch::NEVER) {
+        $this->userId = $userId;
+        $this->name = $name;
+        $this->query = $query;
+        $this->setFrequency($frequency);
+    }
+
+    /**
+     * @param string $frequency
+     *
+     * @throws \InvalidArgumentException
+     *   When an invalid frequency value is given.
+     */
+    private function setFrequency($frequency)
+    {
+        if (SavedSearch::validateFrequency($frequency)) {
+            $this->frequency = $frequency;
+        } else {
+            throw new \InvalidArgumentException('Invalid value for frequency: ' . $frequency);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFrequency()
+    {
+        return $this->frequency;
+    }
+}

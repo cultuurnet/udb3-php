b677054272dfd0e824c412950a20dd558fb58f6d
Kristof Coomans <kristof@coomans.eu>
2015-04-27 12:18:39 +0200

    III-7: Move initialization of common objects under test to setUp()

--- test/SavedSearches/SavedSearchesCommandHandlerTest.php
+++ test/SavedSearches/SavedSearchesCommandHandlerTest.php
@@ -3,7 +3,7 @@
 namespace CultuurNet\UDB3\SavedSearches;
 
 use Broadway\Domain\Metadata;
-use \CultureFeed_SavedSearches_Default as SavedSearches;
+use \CultureFeed_SavedSearches as SavedSearches;
 use \CultureFeed_SavedSearches_SavedSearch as SavedSearch;
 use CultuurNet\Auth\TokenCredentials;
 use CultuurNet\UDB3\SavedSearches\Command\SubscribeToSavedSearch;
@@ -13,10 +13,50 @@ use Psr\Log\LoggerInterface;
 class SavedSearchesCommandHandlerTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var SavedSearches|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $savedSearchesService;
+
+    /**
+     * @var SavedSearchesCommandHandler
+     */
+    protected $commandHandler;
+
+    public function setUp()
+    {
+        $this->savedSearchesService = $this->getMock(
+            SavedSearches::class
+        );
+
+        $this->commandHandler = $this->getSavedSearchesCommandHandlerWithService(
+            $this->savedSearchesService
+        );
+    }
+
+    /**
      * @test
      */
     public function it_can_handle_subscribe_to_saved_search_commands()
     {
+        $subscribeToSavedSearch = $this->aSubscribeToSavedSearchCommand();
+
+        // We expect the service to be called to subscribe to a saved search.
+        $savedSearch = new SavedSearch(
+            $subscribeToSavedSearch->getUserId(),
+            $subscribeToSavedSearch->getName(),
+            $subscribeToSavedSearch->getQuery(),
+            $subscribeToSavedSearch->getFrequency()
+        );
+
+        $this->savedSearchesService->expects($this->once())
+            ->method('subscribe')
+            ->with($savedSearch);
+
+        // Handle the "subscribe to saved search" command.
+        $this->commandHandler->handle($subscribeToSavedSearch);
+    }
+
+    private function aSubscribeToSavedSearchCommand() {
         $userId = 'some-user-id';
         $name = 'My very first saved search!';
         $query = 'city:"Leuven"';
@@ -25,20 +65,8 @@ class SavedSearchesCommandHandlerTest extends \PHPUnit_Framework_TestCase
         // Subscribe command.
         $subscribeToSavedSearch = new SubscribeToSavedSearch($userId, $name, $query);
 
-        // We expect the service to be called to subscribe to a saved search.
-        $savedSearch = new SavedSearch($userId, $name, $query, $frequency);
-        $savedSearchesService = $this->getSavedSearchesService();
-        $savedSearchesService->expects($this->once())
-            ->method('subscribe')
-            ->with($savedSearch);
-
-        // Create the command handler.
-        $commandHandler = $this->getSavedSearchesCommandHandlerWithService($savedSearchesService);
-
-        // Handle the "subscribe to saved search" command.
-        $commandHandler->handle($subscribeToSavedSearch);
+        return $subscribeToSavedSearch;
     }
-
 
     /**
      * @test
@@ -47,22 +75,13 @@ class SavedSearchesCommandHandlerTest extends \PHPUnit_Framework_TestCase
     {
         $error = 'An unknown error has occurred.';
 
-        $userId = 'some-user-id';
-        $name = 'My very first saved search!';
-        $query = 'city:"Leuven"';
-        $frequency = SavedSearch::NEVER;
-
         // Subscribe command.
-        $subscribeToSavedSearch = new SubscribeToSavedSearch($userId, $name, $query);
+        $subscribeToSavedSearch = $this->aSubscribeToSavedSearchCommand();
 
         // We expect subscribe method of the saved searches service to throw an exception.
-        $savedSearchesService = $this->getSavedSearchesService();
-        $savedSearchesService->expects($this->once())
+        $this->savedSearchesService->expects($this->once())
             ->method('subscribe')
             ->willThrowException(new \CultureFeed_Exception($error, 'UNKNOWN_ERROR'));
-
-        // Create the command handler.
-        $commandHandler = $this->getSavedSearchesCommandHandlerWithService($savedSearchesService);
 
         // We expect the logger's error method to be called when the exception is thrown.
         $logger = $this->getMock(LoggerInterface::class);
@@ -72,35 +91,17 @@ class SavedSearchesCommandHandlerTest extends \PHPUnit_Framework_TestCase
                 'saved_search_was_not_subscribed',
                 [
                     'error' => $error,
-                    'userId' => $userId,
-                    'name' => $name,
-                    'query' => $query,
-                    'frequency' => $frequency,
+                    'userId' => $subscribeToSavedSearch->getUserId(),
+                    'name' => $subscribeToSavedSearch->getName(),
+                    'query' => $subscribeToSavedSearch->getQuery(),
+                    'frequency' => $subscribeToSavedSearch->getFrequency(),
                 ]
             );
-        $commandHandler->setLogger($logger);
+        $this->commandHandler->setLogger($logger);
 
         // Handle the "subscribe to saved search" command.
         // This will result in an exception being thrown, and logged.
-        $commandHandler->handle($subscribeToSavedSearch);
-    }
-
-    /**
-     * @param string $class
-     */
-    private function getMockWithoutConstructorCall($class)
-    {
-        return $this->getMock($class, array(), array(), '', false);
-    }
-
-    /**
-     * @return SavedSearches|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private function getSavedSearchesService()
-    {
-        return $this->getMockWithoutConstructorCall(
-            SavedSearches::class
-        );
+        $this->commandHandler->handle($subscribeToSavedSearch);
     }
 
     /**
@@ -111,8 +112,8 @@ class SavedSearchesCommandHandlerTest extends \PHPUnit_Framework_TestCase
         $tokenCredentials = new TokenCredentials('token', 'secret');
 
         // Saved searches service factory used to instantiate the saved searches service.
-        $savedSearchesServiceFactory = $this->getMockWithoutConstructorCall(
-            SavedSearchesServiceFactory::class
+        $savedSearchesServiceFactory = $this->getMock(
+            SavedSearchesServiceFactoryInterface::class
         );
         $savedSearchesServiceFactory->expects($this->once())
             ->method('withTokenCredentials')

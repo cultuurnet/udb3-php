505c1bea5e28ae8ca44173752d4faeb258c334ef
zuuperman <nils.destoop@gmail.com>
2015-04-27 23:21:23 +0200

    restore bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 
-require_once __DIR__ . '/../../../autoload.php';
-require_once __DIR__ . '/Event/CdbXMLProjectorTestBase.php';
-require_once __DIR__ . '/EventExport/Format/HTML/Uitpas/DistributionKey/DistributionKeyConditionFactory.php';
-require_once __DIR__ . '/EventExport/Format/HTML/Uitpas/DistributionKey/DistributionKeyFactory.php';
-require_once __DIR__ . '/EventExport/Format/HTML/Uitpas/Event/EventFactory.php';
+require_once __DIR__ . '/../vendor/autoload.php';

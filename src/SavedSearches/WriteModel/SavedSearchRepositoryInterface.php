cc208443b84713fad8a6247e555138e0a63d4c5c
Bert Ramakers <bertramakers@me.com>
2015-04-30 11:45:38 +0200

    III-9: SavedSearchRepositoryInterface for writing new saved searches.

---
+++ src/SavedSearches/WriteModel/SavedSearchRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CultuurNet\UDB3\SavedSearches\WriteModel;
+
+use ValueObjects\String\String;
+use CultuurNet\UDB3\SavedSearches\Properties\QueryString;
+
+interface SavedSearchRepositoryInterface
+{
+    /**
+     * @param String $userId
+     * @param String $name
+     * @param QueryString $queryString
+     * @param String $id
+     */
+    public function write(
+        String $userId,
+        String $name,
+        QueryString $queryString
+    );
+}

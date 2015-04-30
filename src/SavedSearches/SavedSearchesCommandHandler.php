c1ab4213e99c6091defb229ea1d154e02f9ba231
Bert Ramakers <bertramakers@me.com>
2015-04-30 15:11:43 +0200

    III-10: Handling of the UnsubscribeFromSavedSearch command by calling the new delete method on the UiTIDSavedSearchesRepository class.

--- src/SavedSearches/SavedSearchesCommandHandler.php
+++ src/SavedSearches/SavedSearchesCommandHandler.php
@@ -6,9 +6,16 @@ use Broadway\CommandHandling\CommandHandler;
 use CultuurNet\UDB3\CommandHandling\ContextAwareInterface;
 use CultuurNet\UDB3\CommandHandling\ContextAwareTrait;
 use CultuurNet\UDB3\SavedSearches\Command\SubscribeToSavedSearch;
+use CultuurNet\UDB3\SavedSearches\Command\UnsubscribeFromSavedSearch;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 
+/**
+ * Class SavedSearchesCommandHandler
+ * @package CultuurNet\UDB3\SavedSearches
+ *
+ * @property UiTIDSavedSearchRepository $repository
+ */
 class SavedSearchesCommandHandler extends CommandHandler implements LoggerAwareInterface, ContextAwareInterface
 {
     use ContextAwareTrait;
@@ -36,6 +43,29 @@ class SavedSearchesCommandHandler extends CommandHandler implements LoggerAwareI
         $name = $subscribeToSavedSearch->getName();
         $query = $subscribeToSavedSearch->getQuery();
 
+        $this->repository->write($userId, $name, $query);
+    }
+
+    /**
+     * @param UnsubscribeFromSavedSearch $unsubscribeFromSavedSearch
+     */
+    public function handleUnsubscribeFromSavedSearch(UnsubscribeFromSavedSearch $unsubscribeFromSavedSearch)
+    {
+        $userId = $unsubscribeFromSavedSearch->getUserId();
+        $searchId = $unsubscribeFromSavedSearch->getSearchId();
+
+        $this->repository->delete($userId, $searchId);
+    }
+
+    /**
+     * Should be called inside the handle methods and not inside the constructor,
+     * because the metadata property is set or overwritten right before a handle
+     * method is called.
+     *
+     * @return UiTIDSavedSearchRepository
+     */
+    private function getUiTIDSavedSearchRepository()
+    {
         $metadata = $this->metadata->serialize();
         $tokenCredentials = $metadata['uitid_token_credentials'];
 
@@ -47,6 +77,20 @@ class SavedSearchesCommandHandler extends CommandHandler implements LoggerAwareI
             $repository->setLogger($this->logger);
         }
 
-        $repository->write($userId, $name, $query);
+        return $repository;
+    }
+
+    /**
+     * @param $property
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        switch ($property) {
+            case 'repository':
+                return $this->getUiTIDSavedSearchRepository();
+                break;
+        }
+        return null;
     }
 }

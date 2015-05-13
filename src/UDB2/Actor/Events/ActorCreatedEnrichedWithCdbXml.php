27dadb55424652e23a56ec36799fb4060eb4b550
Kristof Coomans <kristof@coomans.eu>
2015-05-13 16:30:51 +0200

    III-179: Cast to string internally to solve existing expectations of the cdb xml interface & trait

--- src/UDB2/Actor/Events/ActorCreatedEnrichedWithCdbXml.php
+++ src/UDB2/Actor/Events/ActorCreatedEnrichedWithCdbXml.php
@@ -27,8 +27,8 @@ class ActorCreatedEnrichedWithCdbXml extends ActorCreated implements CdbXmlConta
             $author
         );
 
-        $this->setCdbXml($cdbXml);
-        $this->setCdbXmlNamespaceUri($cdbXmlNamespaceUri);
+        $this->setCdbXml((string)$cdbXml);
+        $this->setCdbXmlNamespaceUri((string)$cdbXmlNamespaceUri);
     }
 
     public static function fromActorCreated(

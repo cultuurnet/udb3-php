0e4e761a84029ae6398dd7c4ec0094c06303d401
Kristof Coomans <kristof@coomans.eu>
2015-05-13 11:51:08 +0200

    III-179: Mock the cdb xml namespace uri property of EventCdbXmlServiceInterface in EventImporterTest

--- test/UDB2/EventImporterTest.php
+++ test/UDB2/EventImporterTest.php
@@ -72,6 +72,10 @@ class EventImporterTest extends \PHPUnit_Framework_TestCase
             EventCdbXmlServiceInterface::class
         );
 
+        $this->eventCdbXmlService->expects($this->any())
+            ->method('getCdbXmlNamespaceUri')
+            ->willReturn('http://www.cultuurdatabank.com/XMLSchema/CdbXSD/3.2/FINAL');
+
         $this->placeService = $this->getMock(
             PlaceService::class,
             [],
@@ -134,7 +138,7 @@ class EventImporterTest extends \PHPUnit_Framework_TestCase
                 new EventUpdatedFromUDB2(
                     $cdbId,
                     $eventXml,
-                    \CultureFeed_Cdb_Default::CDB_SCHEME_URL
+                    $this->eventCdbXmlService->getCdbXmlNamespaceUri()
                 ),
             ]
         );

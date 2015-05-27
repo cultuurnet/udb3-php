a2995fd894e0b5844e81e88dade7d91f6a3a1a9d
Kristof Coomans <kristof@coomans.eu>
2015-05-27 15:13:04 +0200

    III-310: Event sourcing repositories now rely on an aggregate factory

--- src/Event/EventRepository.php
+++ src/Event/EventRepository.php
@@ -5,31 +5,22 @@
 
 namespace CultuurNet\UDB3\Event;
 
-use Assert\Assertion;
-use Broadway\Domain\AggregateRoot;
-use Broadway\Domain\DomainEventStream;
 use Broadway\EventHandling\EventBusInterface;
+use Broadway\EventSourcing\AggregateFactory\AggregateFactoryInterface;
+use Broadway\EventSourcing\AggregateFactory\PublicConstructorAggregateFactory;
+use Broadway\EventSourcing\EventSourcingRepository;
 use Broadway\EventSourcing\EventStreamDecoratorInterface;
 use Broadway\EventStore\EventStoreInterface;
-use Broadway\EventStore\EventStreamNotFoundException;
-use Broadway\Repository\AggregateNotFoundException;
-use Broadway\Repository\RepositoryInterface;
 
 /**
  * Class EventRepository
- *
- * This class used to extend EventSourcingRepository from the Broadway library,
- * however we had to change it to publish the decorated event stream to the
- * event bus, instead of the non-decorated event stream. See the add() method.
- *
- * @link https://github.com/qandidate-labs/broadway/issues/61
  */
-class EventRepository implements RepositoryInterface
+class EventRepository extends EventSourcingRepository
 {
-    private $eventStore;
-    private $eventBus;
-    private $aggregateClass;
-    private $eventStreamDecorators = array();
+    /**
+     * @var AggregateFactoryInterface
+     */
+    private $aggregateFactory;
 
     /**
      * @param EventStoreInterface             $eventStore
@@ -41,58 +32,12 @@ class EventRepository implements RepositoryInterface
         EventBusInterface $eventBus,
         array $eventStreamDecorators = array()
     ) {
-        $this->eventStore            = $eventStore;
-        $this->eventBus              = $eventBus;
-        $this->aggregateClass        = Event::class;
-        $this->eventStreamDecorators = $eventStreamDecorators;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function load($id)
-    {
-        try {
-            $domainEventStream = $this->eventStore->load($id);
-
-            $aggregate = new $this->aggregateClass();
-            $aggregate->initializeState($domainEventStream);
-
-            return $aggregate;
-        } catch (EventStreamNotFoundException $e) {
-            throw AggregateNotFoundException::create($id, $e);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * Overwritten
-     */
-    public function save(AggregateRoot $aggregate)
-    {
-        Assertion::isInstanceOf($aggregate, $this->getType());
-
-        $domainEventStream = $aggregate->getUncommittedEvents();
-        $eventStream       = $this->decorateForWrite($aggregate, $domainEventStream);
-        $this->eventStore->append($aggregate->getAggregateRootId(), $eventStream);
-        $this->eventBus->publish($eventStream);
-    }
-
-    private function decorateForWrite(AggregateRoot $aggregate, DomainEventStream $eventStream)
-    {
-        $aggregateType       = $this->getType();
-        $aggregateIdentifier = $aggregate->getAggregateRootId();
-
-        foreach ($this->eventStreamDecorators as $eventStreamDecorator) {
-            $eventStream = $eventStreamDecorator->decorateForWrite($aggregateType, $aggregateIdentifier, $eventStream);
-        }
-
-        return $eventStream;
-    }
-
-    private function getType()
-    {
-        return $this->aggregateClass;
+        parent::__construct(
+            $eventStore,
+            $eventBus,
+            Event::class,
+            new PublicConstructorAggregateFactory(),
+            $eventStreamDecorators
+        );
     }
 }

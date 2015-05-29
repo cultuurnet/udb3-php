52f76c3db3e2af4e9673f2c0210e89f79ad63cde
Bram Cordie <bramcordie@gmail.com>
2015-05-29 12:29:22 +0200

    III-314: Add sentence friendly option to string truncate filter

--- test/EventExport/Format/HTML/HTMLEventFormatterTest.php
+++ test/EventExport/Format/HTML/HTMLEventFormatterTest.php
@@ -65,7 +65,7 @@ class HTMLEventFormatterTest extends \PHPUnit_Framework_TestCase
         $expectedFormattedFreeEvent = [
             'title' => 'Koran, kaliefen en kruistochten - De fundamenten van de islam',
             'image' => 'http://media.uitdatabank.be/20141211/558bb7cf-5ff8-40b4-872b-5f5b46bb16c2.jpg',
-            'description' => 'De islam is niet meer weg te denken uit onze maatschappij. Aan de hand van boeiende anekdotes doet Urbain Vermeulen de ontstaansgeschiedenis van de godsdienst uit de doeken. Hij verklaart hoe de islam zich verhoudt tot de andere wereldgodsdiensten en legt de oorsprong van de fundamentalistische...',
+            'description' => 'De islam is niet meer weg te denken uit onze maatschappij. Aan de hand van boeiende anekdotes doet Urbain Vermeulen de ontstaansgeschiedenis van de godsdienst uit de doeken...',
             'address' => [
                 'name' => 'Cultuurcentrum De Kruisboog',
                 'street' => 'Sint-Jorisplein 20 ',
@@ -95,7 +95,7 @@ class HTMLEventFormatterTest extends \PHPUnit_Framework_TestCase
             'image' => 'http://media.uitdatabank.be/20141211/558bb7cf-5ff8-40b4-872b-5f5b46bb16c2.jpg',
             'type' => 'Cursus of workshop',
             'title' => 'Lessenreeks MURGA',
-            'description' => "Wij zijn Murga çava, een vrolijke groep van 20 percussionisten,\njong en oud, uit Herent en omgeving. Bij ons is iedereen welkom!\nMuzikale voorkennis is geen vereiste. Behalve percussie staan we\nook open voor blazers, dansers of ander talent. Kom gratis met ons\nkennismaken op maandag 1 september...",
+            'description' => "Wij zijn Murga çava, een vrolijke groep van 20 percussionisten,\njong en oud, uit Herent en omgeving. Bij ons is iedereen welkom!\nMuzikale voorkennis is geen vereiste. Behalve percussie staan we\nook open voor blazers, dansers of ander talent...",
             'address' => [
                 'name' => 'GC De Wildeman',
                 'street' => 'Schoolstraat 15',
@@ -118,7 +118,7 @@ class HTMLEventFormatterTest extends \PHPUnit_Framework_TestCase
         $expectedFormattedEvent = [
             'type' => 'Cursus of workshop',
             'title' => 'Koran, kaliefen en kruistochten - De fundamenten van de islam',
-            'description' => 'De islam is niet meer weg te denken uit onze maatschappij. Aan de hand van boeiende anekdotes doet Urbain Vermeulen de ontstaansgeschiedenis van de godsdienst uit de doeken. Hij verklaart hoe de islam zich verhoudt tot de andere wereldgodsdiensten en legt de oorsprong van de fundamentalistische...',
+            'description' => 'De islam is niet meer weg te denken uit onze maatschappij. Aan de hand van boeiende anekdotes doet Urbain Vermeulen de ontstaansgeschiedenis van de godsdienst uit de doeken...',
             'address' => [
                 'name' => 'Cultuurcentrum De Kruisboog',
                 'street' => 'Sint-Jorisplein 20 ',
@@ -143,7 +143,7 @@ class HTMLEventFormatterTest extends \PHPUnit_Framework_TestCase
             "artiestenmee die garant staan voor authenticiteit en originaliteit.\n" .
             "De eerste gastis niemand minder dan Stoomboot, die in het seizoen\n" .
             "2014 doorbrakmet zijn bejubelde debuutalbum. Verder is ooK fluitist\n" .
-            "Stefan Bracavalopnieuw van de...",
+            "Stefan Bracavalopnieuw van de partij...",
             $eventWithHTMLDescription['description']
         );
     }
@@ -289,7 +289,7 @@ class HTMLEventFormatterTest extends \PHPUnit_Framework_TestCase
             ],
             'type' => 'Cursus of workshop',
             'title' => 'Koran, kaliefen en kruistochten - De fundamenten van de islam',
-            'description' => 'De islam is niet meer weg te denken uit onze maatschappij. Aan de hand van boeiende anekdotes doet Urbain Vermeulen de ontstaansgeschiedenis van de godsdienst uit de doeken. Hij verklaart hoe de islam zich verhoudt tot de andere wereldgodsdiensten en legt de oorsprong van de fundamentalistische...',
+            'description' => 'De islam is niet meer weg te denken uit onze maatschappij. Aan de hand van boeiende anekdotes doet Urbain Vermeulen de ontstaansgeschiedenis van de godsdienst uit de doeken...',
             'address' => [
                 'name' => 'Cultuurcentrum De Kruisboog',
                 'street' => 'Sint-Jorisplein 20 ',

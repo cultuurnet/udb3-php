ac2de73ec0b4a21817ce898f574d1948537bfc97
Kristof Coomans <kristof@coomans.eu>
2015-06-15 09:53:07 +0200

    III-321: Document exceptions expected to be thrown by EventVariationServiceInterface implementations

--- src/Variations/EventVariationServiceInterface.php
+++ src/Variations/EventVariationServiceInterface.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UDB3\Variations;
 
+use Broadway\Repository\AggregateNotFoundException;
 use CultuurNet\UDB3\Variations\Model\EventVariation;
 use CultuurNet\UDB3\Variations\Model\Properties\Description;
 use CultuurNet\UDB3\Variations\Model\Properties\Id;
@@ -29,6 +30,9 @@ interface EventVariationServiceInterface
     /**
      * @param Id $id
      * @param Description $description
+     *
+     * @throws AggregateNotFoundException
+     * @throws AggregateDeletedException
      */
     public function editDescription(Id $id, Description $description);
 
@@ -36,6 +40,9 @@ interface EventVariationServiceInterface
      * Delete a variation
      *
      * @param Id $id
+     *
+     * @throws AggregateNotFoundException
+     * @throws AggregateDeletedException
      */
     public function deleteEventVariation(Id $id);
 }

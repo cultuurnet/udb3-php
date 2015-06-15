630c2772f945032f21e6d8b5f24f2f1e98238728
Kristof Coomans <kristof@coomans.eu>
2015-06-15 21:49:06 +0200

    III-327: Add test for EventVariationEvent

---
+++ test/Variations/Model/Events/EventVariationEventTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Variations\Model\Events;
+
+
+use CultuurNet\UDB3\Variations\Model\Properties\Id;
+
+class EventVariationEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_supports_serialization()
+    {
+        /** @var EventVariationEvent $event */
+        $event = $this->getMockForAbstractClass(
+            EventVariationEvent::class,
+            [
+                new Id('28CD91A1-EB0F-49A9-991D-5DCAFEC0A043')
+            ]
+        );
+
+        $serialized = $event->serialize();
+
+        $this->assertEquals(
+            [
+                'id' => '28CD91A1-EB0F-49A9-991D-5DCAFEC0A043',
+            ],
+            $serialized
+        );
+
+        $this->assertEquals(
+            $event,
+            $event::deserialize($serialized)
+        );
+    }
+}

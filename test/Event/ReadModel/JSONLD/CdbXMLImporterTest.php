00710462cf82c04d7d2b08312d9f766998a36586
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-08-12 11:35:17 +0200

    III-307: Set default timezone and adjust assertion

--- test/Event/ReadModel/JSONLD/CdbXMLImporterTest.php
+++ test/Event/ReadModel/JSONLD/CdbXMLImporterTest.php
@@ -36,6 +36,7 @@ class CdbXMLImporterTest extends \PHPUnit_Framework_TestCase
         $this->organizerManager = $this->getMock(OrganizerServiceInterface::class);
         $this->placeManager = $this->getMock(PlaceServiceInterface::class);
         $this->slugger = $this->getMock(SluggerInterface::class);
+        date_default_timezone_set('Europe/Brussels');
     }
 
     private function createJsonEventFromCdbXml($fileName)
@@ -296,12 +297,12 @@ class CdbXMLImporterTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_has_a_correct_datetime_when_cdbxml_contains_minimum_unix_timestamp()
+    public function it_has_a_correct_datetime_when_cdbxml_contains_negative_unix_timestamp()
     {
         $jsonEvent = $this->createJsonEventFromCdbXml('event_with_minimum_timestamp.cdbxml.xml');
 
         $this->assertObjectHasAttribute('bookingInfo', $jsonEvent);
-        $this->assertEquals('1970-01-01T00:00:00+00:00', $jsonEvent->bookingInfo[0]['availabilityStarts']);
-        $this->assertEquals('1970-01-01T00:00:00+00:00', $jsonEvent->bookingInfo[0]['availabilityEnds']);
+        $this->assertEquals('1968-12-31T23:00:00+00:00', $jsonEvent->bookingInfo[0]['availabilityStarts']);
+        $this->assertEquals('1968-12-31T23:00:00+00:00', $jsonEvent->bookingInfo[0]['availabilityEnds']);
     }
 }

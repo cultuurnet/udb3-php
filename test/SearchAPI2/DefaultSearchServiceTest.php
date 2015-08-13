19a681f06a2d2127a2c5233b323a335d8a93c1e3
Bram Cordie <bramcordie@gmail.com>
2015-08-13 12:28:42 +0200

    III-401: Port specifications to unit tests and remove phpspec from project

---
+++ test/SearchAPI2/DefaultSearchServiceTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace SearchAPI2;
+
+use CultuurNet\Auth\ConsumerCredentials;
+use CultuurNet\Auth\Guzzle\HttpClientFactory;
+use CultuurNet\Auth\TokenCredentials;
+use CultuurNet\Search\Parameter\FilterQuery;
+use CultuurNet\Search\Parameter\Group;
+use CultuurNet\Search\Parameter\Query;
+use CultuurNet\UDB3\SearchAPI2\DefaultSearchService;
+use Guzzle\Http\ClientInterface;
+use Guzzle\Http\Message\RequestInterface;
+use Guzzle\Http\Message\Response;
+use Guzzle\Http\QueryString;
+
+class DefaultSearchServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ConsumerCredentials
+     */
+    protected $consumerCredentials;
+
+    /**
+     * @var DefaultSearchService
+     */
+    protected $searchService;
+
+    protected $baseUrl = 'http://acc.uitid.be/uitid/rest/searchv2';
+
+    /**
+     * @var HttpClientFactory | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $clientFactory;
+
+    public function SetUp()
+    {
+        $this->consumerCredentials = new ConsumerCredentials();
+        $this->searchService = new DefaultSearchService(
+            $this->baseUrl,
+            $this->consumerCredentials
+        );
+        $this->clientFactory = $this->getMock(HttpClientFactory::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_initializable_with_at_least_a_base_url_and_consumer_credentials()
+    {
+        $this->assertInstanceOf(DefaultSearchService::class, $this->searchService);
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_initializable_with_additional_token_credentials()
+    {
+        $tokenCredentials = new TokenCredentials('token', 'secret');
+
+        $this->searchService = new DefaultSearchService(
+            $this->baseUrl,
+            $this->consumerCredentials,
+            $tokenCredentials
+        );
+
+        $this->assertInstanceOf(DefaultSearchService::class, $this->searchService);
+    }
+
+    /**
+     * @test
+     */
+    public function it_searches_with_an_array_of_arbitrary_solr_parameters()
+    {
+        $this->searchService->setHttpClientFactory($this->clientFactory);
+
+        $queryString = $this->getMock(QueryString::class);
+        $queryString
+            ->expects($this->exactly(5))
+            ->method('add')
+            ->withConsecutive(
+                ['q', 'pop'],
+                ['fq', 'type:event'],
+                ['group', 'true'],
+                ['past', 'true'],
+                ['unavailable', 'true']
+            );
+
+        $request = $this->getMock(RequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getQuery')
+            ->willReturn($queryString);
+
+        $client = $this->getMock(ClientInterface::class);
+        $client
+            ->expects($this->once())
+            ->method('get')
+            ->with('search')
+            ->willReturn($request);
+
+        $this->clientFactory
+            ->expects($this->once())
+            ->method('createClient')
+            ->willReturn($client);
+
+        $request
+            ->expects($this->once())
+            ->method('send');
+
+        $this->searchService->search([
+            new Query('pop'),
+            new FilterQuery('type:event'),
+            new Group()
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_the_raw_search_response()
+    {
+        $this->searchService->setHttpClientFactory($this->clientFactory);
+        $expectedResponse = new Response(200);
+
+        $request = $this->getMock(RequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getQuery')
+            ->willReturn(new QueryString());
+
+        $client = $this->getMock(ClientInterface::class);
+        $client
+            ->expects($this->once())
+            ->method('get')
+            ->with('search')
+            ->willReturn($request);
+
+        $this->clientFactory
+            ->expects($this->once())
+            ->method('createClient')
+            ->willReturn($client);
+
+        $request
+            ->expects($this->once())
+            ->method('send')
+            ->willReturn($expectedResponse);
+
+        $response = $this->searchService->search([]);
+        $this->assertEquals($expectedResponse, $response);
+    }
+}
---
+++ test/SearchAPI2/ResultSetPullParserTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SearchAPI2;
+
+use CultuurNet\UDB3\Iri\IriGeneratorInterface;
+use CultuurNet\UDB3\Search\Results;
+use CultuurNet\UDB3\SearchAPI2\ResultSetPullParser;
+use ValueObjects\Number\Integer;
+
+class ResultSetPullParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var IriGeneratorInterface | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $iriGenerator;
+
+    /**
+     * @var ResultSetPullParser
+     */
+    protected $resultSetPullParser;
+
+    public function SetUp()
+    {
+        $this->iriGenerator = $this->getMock(IriGeneratorInterface::class);
+        $this->resultSetPullParser = new ResultSetPullParser(
+            new \XMLReader(),
+            $this->iriGenerator
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_extracts_totalItems_and_member_ids_from_a_cbxml_result_set()
+    {
+        $cdbxml = file_get_contents(
+            __DIR__ . '/ResultSetPullParserSpec.search.xml'
+        );
+
+        $this->iriGenerator
+            ->expects($this->exactly(8))
+            ->method('iri')
+            ->withConsecutive(
+                ['590174eb-5577-4b49-8bc2-4b619a948c56'],
+                ['9b60002a-9671-4b91-a2ad-5ccf8fbf7e5c'],
+                ['d9725327-cbec-4bb8-bc56-9f3f7761b716'],
+                ['70d24706-6e23-406c-9b54-445f5249ae6b'],
+                ['2c86bd2d-686a-41e8-a1fc-6fe99c9d6b45'],
+                ['409cca2b-d5bb-4f53-9312-de22bdbbcbb2'],
+                ['40836aa3-9fcb-4672-8f69-6394fc0873f2'],
+                ['ee08000a-ccfa-4675-93ef-a0dc02ae1be4']
+            )
+            ->willReturnArgument(0);
+
+        $resultSet = $this->resultSetPullParser->getResultSet($cdbxml);
+
+        $expectedResultSet = new Results(
+            [
+                ['@id' => '590174eb-5577-4b49-8bc2-4b619a948c56'],
+                ['@id' => '9b60002a-9671-4b91-a2ad-5ccf8fbf7e5c'],
+                ['@id' => 'd9725327-cbec-4bb8-bc56-9f3f7761b716'],
+                ['@id' => '70d24706-6e23-406c-9b54-445f5249ae6b'],
+                ['@id' => '2c86bd2d-686a-41e8-a1fc-6fe99c9d6b45'],
+                ['@id' => '409cca2b-d5bb-4f53-9312-de22bdbbcbb2'],
+                ['@id' => '40836aa3-9fcb-4672-8f69-6394fc0873f2'],
+                ['@id' => 'ee08000a-ccfa-4675-93ef-a0dc02ae1be4'],
+            ],
+            new Integer(1820)
+        );
+
+        $this->assertEquals($expectedResultSet, $resultSet);
+    }
+}

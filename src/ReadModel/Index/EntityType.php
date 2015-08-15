c62eb3cc3a07f988fc9e25f6d825f640ce20695a
Kristof Coomans <kristof@coomans.eu>
2015-08-15 08:51:55 +0200

    III-359: Add doc annotations for the magic methods

--- src/ReadModel/Index/EntityType.php
+++ src/ReadModel/Index/EntityType.php
@@ -4,6 +4,11 @@ namespace CultuurNet\UDB3\ReadModel\Index;
 
 use ValueObjects\Enum\Enum;
 
+/**
+ * @method EntityType EVENT
+ * @method EntityType ORGANIZER
+ * @method EntityType PLACE
+ */
 class EntityType extends Enum
 {
     const EVENT = 'event';

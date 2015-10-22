7cff323d92d9af7c10eed82224432d1a04c276ab
Nicolas Leroy <nicolas.leroy@gmail.com>
2015-10-22 11:53:28 +0200

    III-448: Add relations projection for EVentUpdatedFromCdbXml, with test

--- test/Event/ReadModel/Relations/ProjectorTest.php
+++ test/Event/ReadModel/Relations/ProjectorTest.php
@@ -12,6 +12,7 @@ use Broadway\Domain\DateTime;
 use Broadway\Domain\DomainMessage;
 use Broadway\Domain\Metadata;
 use CultuurNet\UDB3\Event\Events\EventCreatedFromCdbXml;
+use CultuurNet\UDB3\Event\Events\EventUpdatedFromCdbXml;
 use CultuurNet\UDB3\EventServiceInterface;
 use CultuurNet\UDB3\EventXmlString;
 use ValueObjects\String\String;
@@ -174,6 +175,135 @@ class ProjectorTest extends \PHPUnit_Framework_TestCase
             1,
             new Metadata($metadata),
             $eventCreatedFromCdbXml,
+            DateTime::fromString($importedDate)
+        );
+
+        $this->projector->handle($domainMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_relations_when_updating_event_from_cdbxml_with_place_and_without_organizer()
+    {
+        $xml = file_get_contents(__DIR__ . '/event_with_placeid_and_without_organiserid.xml');
+
+        $eventUpdatedFromCdbXml = new EventUpdatedFromCdbXml(
+            new String('foo'),
+            new EventXmlString($xml),
+            new String(self::CDBXML_NAMESPACE_33)
+        );
+
+        $expectedEventId = 'foo';
+        $expectedPlaceId = 'bcb983d2-ffba-457d-a023-a821aa841fba';
+        $expectedOrganizerId = null;
+
+        $this->repository
+            ->expects($this->once())
+            ->method('storeRelations')
+            ->with(
+                $this->equalTo($expectedEventId),
+                $this->equalTo($expectedPlaceId),
+                $this->equalTo($expectedOrganizerId)
+            );
+
+        $importedDate = '2015-03-01T10:17:19.176169+02:00';
+
+        $metadata = array();
+        $metadata['user_nick'] = 'Jantest';
+        $metadata['consumer']['name'] = 'UiTDatabank';
+
+        $domainMessage = new DomainMessage(
+            $eventUpdatedFromCdbXml->getEventId()->toNative(),
+            1,
+            new Metadata($metadata),
+            $eventUpdatedFromCdbXml,
+            DateTime::fromString($importedDate)
+        );
+
+        $this->projector->handle($domainMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_relations_when_updating_event_from_cdbxml_with_place_and_organizer()
+    {
+        $xml = file_get_contents(__DIR__ . '/event_with_placeid_and_organiserid.xml');
+
+        $eventUpdatedFromCdbXml = new EventUpdatedFromCdbXml(
+            new String('foo'),
+            new EventXmlString($xml),
+            new String(self::CDBXML_NAMESPACE_33)
+        );
+
+        $expectedEventId = 'foo';
+        $expectedPlaceId = 'bcb983d2-ffba-457d-a023-a821aa841fba';
+        $expectedOrganizerId = 'test-de-bijloke';
+
+        $this->repository
+            ->expects($this->once())
+            ->method('storeRelations')
+            ->with(
+                $this->equalTo($expectedEventId),
+                $this->equalTo($expectedPlaceId),
+                $this->equalTo($expectedOrganizerId)
+            );
+
+        $importedDate = '2015-03-01T10:17:19.176169+02:00';
+
+        $metadata = array();
+        $metadata['user_nick'] = 'Jantest';
+        $metadata['consumer']['name'] = 'UiTDatabank';
+
+        $domainMessage = new DomainMessage(
+            $eventUpdatedFromCdbXml->getEventId()->toNative(),
+            1,
+            new Metadata($metadata),
+            $eventUpdatedFromCdbXml,
+            DateTime::fromString($importedDate)
+        );
+
+        $this->projector->handle($domainMessage);
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_relations_when_updating_event_from_cdbxml_without_place_and_without_organizer()
+    {
+        $xml = file_get_contents(__DIR__ . '/event_without_placeid_and_without_organiserid.xml');
+
+        $eventUpdatedFromCdbXml = new EventUpdatedFromCdbXml(
+            new String('foo'),
+            new EventXmlString($xml),
+            new String(self::CDBXML_NAMESPACE_33)
+        );
+
+        $expectedEventId = 'foo';
+        $expectedPlaceId = null;
+        $expectedOrganizerId = null;
+
+        $this->repository
+            ->expects($this->once())
+            ->method('storeRelations')
+            ->with(
+                $this->equalTo($expectedEventId),
+                $this->equalTo($expectedPlaceId),
+                $this->equalTo($expectedOrganizerId)
+            );
+
+        $importedDate = '2015-03-01T10:17:19.176169+02:00';
+
+        $metadata = array();
+        $metadata['user_nick'] = 'Jantest';
+        $metadata['consumer']['name'] = 'UiTDatabank';
+
+        $domainMessage = new DomainMessage(
+            $eventUpdatedFromCdbXml->getEventId()->toNative(),
+            1,
+            new Metadata($metadata),
+            $eventUpdatedFromCdbXml,
             DateTime::fromString($importedDate)
         );
 

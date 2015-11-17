50f9c97fb3b42b7663a1652c406406b4eec0f2e7
Kristof Coomans <kristof@2dotstwice.be>
2015-11-17 21:28:00 +0100

    III-473: Add test for OrganizerUpdated (de)serialization

---
+++ test/Event/Events/OrganizerUpdatedTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Event\Events;
+
+class OrganizerUpdatedTest extends \PHPUnit_Framework_TestCase
+{
+    public function serializationDataProvider()
+    {
+        return [
+            [
+                [
+                    'event_id' => 'event-123',
+                    'organizerId' => 'place-456',
+                ],
+                new OrganizerUpdated(
+                    'event-123',
+                    'place-456'
+                ),
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider serializationDataProvider
+     */
+    public function it_can_be_serialized_to_an_array(
+        array $expectedSerializedValue,
+        OrganizerUpdated $organizerUpdated
+    ) {
+        $this->assertEquals(
+            $expectedSerializedValue,
+            $organizerUpdated->serialize()
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider serializationDataProvider
+     */
+    public function it_can_be_deserialized_from_an_array(
+        array $serializedValue,
+        OrganizerUpdated $expectedOrganizerUpdated
+    ) {
+        $this->assertEquals(
+            $expectedOrganizerUpdated,
+            OrganizerUpdated::deserialize($serializedValue)
+        );
+    }
+}

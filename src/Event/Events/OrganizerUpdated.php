381e7e6e2b9ad6fa81793acbb1f8819ab8ac9097
Kristof Coomans <kristof@2dotstwice.be>
2015-11-17 21:34:42 +0100

    III-473: Remove useless, wrong class description

--- src/Event/Events/OrganizerUpdated.php
+++ src/Event/Events/OrganizerUpdated.php
@@ -9,9 +9,6 @@ namespace CultuurNet\UDB3\Event\Events;
 
 use CultuurNet\UDB3\Event\EventEvent;
 
-/**
- * Description of DescriptionUpdated
- */
 class OrganizerUpdated extends EventEvent
 {
     use \CultuurNet\UDB3\OrganizerUpdatedTrait;

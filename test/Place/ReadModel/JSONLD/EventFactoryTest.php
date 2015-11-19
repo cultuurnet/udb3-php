4f76e39080688417350b2dcb83aa6afa36f2e9bb
Kristof Coomans <kristof@2dotstwice.be>
2015-11-19 15:26:22 +0100

    III-472: Add EventFactory implementation suitable for Place repository

---
+++ test/Place/ReadModel/JSONLD/EventFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Place\ReadModel\JSONLD;
+
+use CultuurNet\UDB3\Place\PlaceProjectedToJSONLD;
+
+class EventFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_factors_an_event_indicating_place_was_projected_to_jsonld()
+    {
+        $eventFactory = new EventFactory();
+
+        $event = $eventFactory->createEvent('123');
+
+        $this->assertEquals(
+            new PlaceProjectedToJSONLD('123'),
+            $event
+        );
+    }
+}

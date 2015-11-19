3c7c1d8d4b698918957de270a2130358f1697fc0
Kristof Coomans <kristof@2dotstwice.be>
2015-11-19 13:30:10 +0100

    III-472: Add test for OrganizerDeleted

---
+++ test/Place/Events/OrganizerDeletedTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\UDB3\Place\Events;
+
+class OrganizerDeletedTest extends \PHPUnit_Framework_TestCase
+{
+    public function serializationDataProvider()
+    {
+        return [
+            [
+                [
+                    'place_id' => 'place-123',
+                    'organizerId' => 'organizer-456',
+                ],
+                new OrganizerDeleted(
+                    'place-123',
+                    'organizer-456'
+                ),
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider serializationDataProvider
+     */
+    public function it_can_be_serialized_to_an_array(
+        array $expectedSerializedValue,
+        OrganizerDeleted $organizerDeleted
+    ) {
+        $this->assertEquals(
+            $expectedSerializedValue,
+            $organizerDeleted->serialize()
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider serializationDataProvider
+     */
+    public function it_can_be_deserialized_from_an_array(
+        array $serializedValue,
+        OrganizerDeleted $expectedOrganizerDeleted
+    ) {
+        $this->assertEquals(
+            $expectedOrganizerDeleted,
+            OrganizerDeleted::deserialize($serializedValue)
+        );
+    }
+}
+

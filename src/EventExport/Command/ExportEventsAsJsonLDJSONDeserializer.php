8f61727afe867b48e3d976aa8c7b434436585051
Bert Ramakers <bertramakers@me.com>
2016-01-22 18:12:30 +0100

    III-709: JSONDeserializers for Export commands.

---
+++ src/EventExport/Command/ExportEventsAsJsonLDJSONDeserializer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CultuurNet\UDB3\EventExport\Command;
+
+use CultuurNet\UDB3\EventExport\EventExportQuery;
+use ValueObjects\Web\EmailAddress;
+
+class ExportEventsAsJsonLDJSONDeserializer extends ExportEventsJSONDeserializer
+{
+    /**
+     * {@inheritdoc}
+     */
+    function createCommand(
+        EventExportQuery $query,
+        EmailAddress $address = null,
+        $selection = null,
+        $include = null
+    ) {
+        return new ExportEventsAsJsonLD(
+            $query,
+            $address,
+            $selection,
+            $include
+        );
+    }
+}
---
+++ src/EventExport/Command/ExportEventsAsOOXMLJSONDeserializer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CultuurNet\UDB3\EventExport\Command;
+
+use CultuurNet\UDB3\EventExport\EventExportQuery;
+use ValueObjects\Web\EmailAddress;
+
+class ExportEventsAsOOXMLJSONDeserializer extends ExportEventsJSONDeserializer
+{
+    /**
+     * {@inheritdoc}
+     */
+    function createCommand(
+        EventExportQuery $query,
+        EmailAddress $address = null,
+        $selection = null,
+        $include = null
+    ) {
+        return new ExportEventsAsOOXML(
+            $query,
+            $address,
+            $selection,
+            $include
+        );
+    }
+}

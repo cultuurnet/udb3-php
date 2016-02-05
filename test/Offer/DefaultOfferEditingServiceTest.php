8fdae9e203d1c3024dd3cf3e83c473a35e9cb2ec
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-02-05 14:49:34 +0100

    III-657: Adapt the DefaultOfferEditingService with test

--- test/Offer/DefaultOfferEditingServiceTest.php
+++ test/Offer/DefaultOfferEditingServiceTest.php
@@ -7,9 +7,13 @@ use Broadway\UuidGenerator\UuidGeneratorInterface;
 use Broadway\Repository\RepositoryInterface;
 use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
 use CultuurNet\UDB3\Label;
+use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Offer\Commands\AbstractAddLabel;
 use CultuurNet\UDB3\Offer\Commands\AbstractDeleteLabel;
+use CultuurNet\UDB3\Offer\Commands\AbstractTranslateDescription;
+use CultuurNet\UDB3\Offer\Commands\AbstractTranslateTitle;
 use CultuurNet\UDB3\Offer\Commands\OfferCommandFactoryInterface;
+use ValueObjects\String\String;
 
 class DefaultOfferEditingServiceTest extends \PHPUnit_Framework_TestCase
 {
@@ -53,6 +57,16 @@ class DefaultOfferEditingServiceTest extends \PHPUnit_Framework_TestCase
      */
     private $expectedCommandId;
 
+    /**
+     * @var AbstractTranslateTitle
+     */
+    private $translateTitleCommand;
+
+    /**
+     * @var AbstractTranslateDescription
+     */
+    private $translateDescriptionCommand;
+
     public function setUp()
     {
         $this->commandBus = $this->getMock(CommandBusInterface::class);
@@ -68,6 +82,16 @@ class DefaultOfferEditingServiceTest extends \PHPUnit_Framework_TestCase
         $this->deleteLabelCommand = $this->getMockForAbstractClass(
             AbstractDeleteLabel::class,
             array('foo', new Label('label1'))
+        );
+
+        $this->translateTitleCommand = $this->getMockForAbstractClass(
+            AbstractTranslateTitle::class,
+            array('foo', new Language('en'), new String('English title'))
+        );
+
+        $this->translateDescriptionCommand = $this->getMockForAbstractClass(
+            AbstractTranslateDescription::class,
+            array('foo', new Language('en'), new String('English description'))
         );
 
         $this->offerEditingService = new DefaultOfferEditingService(
@@ -122,6 +146,56 @@ class DefaultOfferEditingServiceTest extends \PHPUnit_Framework_TestCase
             ->willReturn($this->expectedCommandId);
 
         $commandId = $this->offerEditingService->deleteLabel('foo', new Label('label1'));
+
+        $this->assertEquals($this->expectedCommandId, $commandId);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_translate_a_title()
+    {
+        $this->offerRepository->expects($this->once())
+            ->method('get')
+            ->with('foo');
+
+        $this->commandFactory->expects($this->once())
+            ->method('createTranslateTitleCommand')
+            ->with('foo', new Language('en'), new String('English title'))
+            ->willReturn($this->translateTitleCommand);
+
+        $this->commandBus->expects($this->once())
+            ->method('dispatch')
+            ->willReturn($this->expectedCommandId);
+
+        $commandId = $this->offerEditingService->translateTitle('foo', new Language('en'), new String('English title'));
+
+        $this->assertEquals($this->expectedCommandId, $commandId);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_translate_a_description()
+    {
+        $this->offerRepository->expects($this->once())
+            ->method('get')
+            ->with('foo');
+
+        $this->commandFactory->expects($this->once())
+            ->method('createTranslateDescriptionCommand')
+            ->with('foo', new Language('en'), new String('English description'))
+            ->willReturn($this->translateDescriptionCommand);
+
+        $this->commandBus->expects($this->once())
+            ->method('dispatch')
+            ->willReturn($this->expectedCommandId);
+
+        $commandId = $this->offerEditingService->translateDescription(
+            'foo',
+            new Language('en'),
+            new String('English description')
+        );
 
         $this->assertEquals($this->expectedCommandId, $commandId);
     }

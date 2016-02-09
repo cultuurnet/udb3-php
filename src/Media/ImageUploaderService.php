394ef5db3edf5d6e94431d9e480c13493b9f678b
Bram Cordie <bramcordie@gmail.com>
2016-02-09 17:24:48 +0100

    III-626: Optional file size limitation

--- src/Media/ImageUploaderService.php
+++ src/Media/ImageUploaderService.php
@@ -2,13 +2,16 @@
 
 namespace CultuurNet\UDB3\Media;
 
+use Assert\Assertion;
 use Broadway\CommandHandling\CommandBusInterface;
 use Broadway\UuidGenerator\UuidGeneratorInterface;
+use CultuurNet\Entry\Number;
 use CultuurNet\UDB3\Media\Commands\UploadImage;
 use CultuurNet\UDB3\Media\Properties\MIMEType;
 use League\Flysystem\FilesystemInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use ValueObjects\Identity\UUID;
+use ValueObjects\Number\Natural;
 use ValueObjects\String\String;
 
 class ImageUploaderService implements ImageUploaderInterface
@@ -34,21 +37,33 @@ class ImageUploaderService implements ImageUploaderInterface
     protected $filesystem;
 
     /**
+     * @var Natural|null
+     *  The maximum file size in bytes.
+     *  There is no limit when the file size if null.
+     */
+    protected $maxFileSize;
+
+    /**
      * @param UuidGeneratorInterface $uuidGenerator
      * @param CommandBusInterface $commandBus
      * @param FilesystemInterface $filesystem
      * @param $uploadDirectory
+     *
+     * @param Natural|null $maxFileSize
+     *  The maximum file size in bytes.
      */
     public function __construct(
         UuidGeneratorInterface $uuidGenerator,
         CommandBusInterface $commandBus,
         FilesystemInterface $filesystem,
-        $uploadDirectory
+        $uploadDirectory,
+        Natural $maxFileSize = null
     ) {
         $this->uuidGenerator = $uuidGenerator;
         $this->commandBus = $commandBus;
         $this->filesystem = $filesystem;
         $this->uploadDirectory = $uploadDirectory;
+        $this->maxFileSize = $maxFileSize;
     }
 
     /**
@@ -65,6 +80,8 @@ class ImageUploaderService implements ImageUploaderInterface
         if (!$mimeTypeString) {
             throw new \InvalidArgumentException('The type of the uploaded file can not be guessed.');
         }
+
+        $this->guardFileSizeLimit($file);
 
         $fileTypeParts = explode('/', $mimeTypeString);
         $fileType = array_shift($fileTypeParts);
@@ -90,6 +107,22 @@ class ImageUploaderService implements ImageUploaderInterface
                 new String($destination)
             )
         );
+    }
+
+    private function guardFileSizeLimit(UploadedFile $file)
+    {
+        $filePath = $file->getRealPath();
+        $fileSize = filesize($filePath);
+
+        if ($this->maxFileSize && !$fileSize) {
+            throw new \InvalidArgumentException('There is a maximum size and we could not determine the size of the uploaded image.');
+        }
+
+        if ($this->maxFileSize && $fileSize > $this->maxFileSize->toNative()) {
+            throw new FileSizeExceededException(
+                "The file size of the uploaded image is too big."
+            );
+        }
     }
 
     /**
--- test/Media/ImageUploaderServiceTest.php
+++ test/Media/ImageUploaderServiceTest.php
@@ -4,8 +4,15 @@ namespace CultuurNet\UDB3\Media;
 
 use Broadway\CommandHandling\CommandBusInterface;
 use Broadway\UuidGenerator\UuidGeneratorInterface;
+use League\Flysystem\File;
 use League\Flysystem\FilesystemInterface;
+use org\bovigo\vfs\content\LargeFileContent;
+use org\bovigo\vfs\content\LargeFileContentTestCase;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamContent;
+use org\bovigo\vfs\vfsStreamFile;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use ValueObjects\Number\Natural;
 use ValueObjects\String\String;
 
 class ImageUploaderServiceTest extends \PHPUnit_Framework_TestCase
@@ -158,6 +165,92 @@ class ImageUploaderServiceTest extends \PHPUnit_Framework_TestCase
         $this->uploader->upload($file, $description, $copyrightHolder);
     }
 
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_when_the_file_size_limit_is_exceeded()
+    {
+        $uploader = new ImageUploaderService(
+            $this->uuidGenerator,
+            $this->commandBus,
+            $this->filesystem,
+            $this->directory,
+            new Natural(1000000)
+        );
+
+        $file = $this->getMockImage(1111111);
+
+        $description = new String('file description');
+        $copyrightHolder = new String('Dude Man');
+
+        $this->setExpectedException(
+            FileSizeExceededException::class,
+            'The file size of the uploaded image is too big.'
+        );
+
+        $uploader->upload($file, $description, $copyrightHolder);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_when_the_file_size_is_limited_but_cannot_be_determined()
+    {
+        $uploader = new ImageUploaderService(
+            $this->uuidGenerator,
+            $this->commandBus,
+            $this->filesystem,
+            $this->directory,
+            new Natural(1000000)
+        );
+
+        $file = $this->getMockImage(false);
+
+        $description = new String('file description');
+        $copyrightHolder = new String('Dude Man');
+
+        $this->setExpectedException(
+            \InvalidArgumentException::class,
+            'There is a maximum size and we could not determine the size of the uploaded image.'
+        );
+
+        $uploader->upload($file, $description, $copyrightHolder);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_upload_a_file_that_does_not_exceed_the_maximum_file_size()
+    {
+        $file = $this->getMockImage(1000000);
+
+        $uploader = new ImageUploaderService(
+            $this->uuidGenerator,
+            $this->commandBus,
+            $this->filesystem,
+            $this->directory,
+            new Natural(1000000)
+        );
+
+        $description = new String('file description');
+        $copyrightHolder = new String('Dude Man');
+        $generatedUuid = 'de305d54-75b4-431b-adb2-eb6b9e546014';
+
+        $this->uuidGenerator
+            ->expects($this->once())
+            ->method('generate')
+            ->willReturn($generatedUuid);
+
+        $expectedDestination = $this->directory.'/'.$generatedUuid.'.jpg';
+
+        $this->filesystem
+            ->expects($this->once())
+            ->method('writeStream')
+            ->with($expectedDestination, $this->anything());
+
+        $uploader->upload($file, $description, $copyrightHolder);
+    }
+
     private function getMockFile()
     {
         return $this
@@ -165,5 +258,44 @@ class ImageUploaderServiceTest extends \PHPUnit_Framework_TestCase
             ->enableOriginalConstructor()
             ->setConstructorArgs([tempnam(sys_get_temp_dir(), ''), 'dummy'])
             ->getMock();
+    }
+
+    /**
+     * @param int $imageSize
+     *  Image size in bytes.
+     *
+     * @return UploadedFile|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockImage($imageSize)
+    {
+        // Use a virtual file that and store its
+        $fileDirectory = vfsStream::setup('files');
+        $file = vfsStream::newFile('my-image.jpg')
+            ->withContent(new LargeFileContent($imageSize))
+            ->at($fileDirectory);
+        $filePath = $file->url();
+
+        $image = $this->getMockFile();
+
+        $image
+            ->expects($this->once())
+            ->method('isValid')
+            ->willReturn(true);
+
+        $image
+            ->expects($this->once())
+            ->method('getMimeType')
+            ->willReturn('image/jpg');
+
+        $image
+            ->expects($this->any())
+            ->method('guessExtension')
+            ->willReturn('jpg');
+
+        $image->expects($this->any())
+            ->method('getRealPath')
+            ->willReturn($filePath);
+
+        return $image;
     }
 }

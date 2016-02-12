61d16b2d7fdb8d8ba0696f06d811512b0da6c9aa
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-02-12 15:40:30 +0100

    III-765: Rename offerRepository to readRepository
    In order to make a difference in read and write repostiories, we need to rename some variables.

--- src/Offer/DefaultOfferEditingService.php
+++ src/Offer/DefaultOfferEditingService.php
@@ -24,7 +24,7 @@ class DefaultOfferEditingService implements OfferEditingServiceInterface
     /**
      * @var DocumentRepositoryInterface
      */
-    protected $offerRepository;
+    protected $readRepository;
 
     /**
      * @var OfferCommandFactoryInterface
@@ -40,12 +40,12 @@ class DefaultOfferEditingService implements OfferEditingServiceInterface
     public function __construct(
         CommandBusInterface $commandBus,
         UuidGeneratorInterface $uuidGenerator,
-        DocumentRepositoryInterface $offerRepository,
+        DocumentRepositoryInterface $readRepository,
         OfferCommandFactoryInterface $commandFactory
     ) {
         $this->commandBus = $commandBus;
         $this->uuidGenerator = $uuidGenerator;
-        $this->offerRepository = $offerRepository;
+        $this->readRepository = $readRepository;
         $this->commandFactory = $commandFactory;
     }
 
@@ -88,6 +88,6 @@ class DefaultOfferEditingService implements OfferEditingServiceInterface
      */
     public function guardId($id)
     {
-        $this->offerRepository->get($id);
+        $this->readRepository->get($id);
     }
 }

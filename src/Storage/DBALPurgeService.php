e241eaa8de9b92ab79b2f209270c44900b79bc1f
Luc Wollants <luc.wollants@apptite.be>
2016-02-15 18:30:24 +0100

    III-633 Added interface for service to purge storages (for now only concrete Doctrine DBAL implementation). Also added manager to store write and read purge services.

---
+++ src/Storage/DBALPurgeService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CultuurNet\UDB3\Storage;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * Class DBALPurgeService
+ * @package CultuurNet\UDB3\Storage
+ */
+class DBALPurgeService implements PurgeServiceInterface
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var string
+     */
+    private $tableName;
+
+    /**
+     * DBALPurgeService constructor.
+     * @param Connection $connection
+     * @param string $tableName
+     */
+    public function __construct(
+         $connection,
+         $tableName
+    )
+    {
+        $this->connection = $connection;
+
+        $this->tableName = $tableName;
+    }
+
+    public function purgeAll()
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+
+        $queryBuilder->delete($this->tableName);
+        $queryBuilder->execute();
+    }
+}

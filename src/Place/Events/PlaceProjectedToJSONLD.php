92884a0897dd512ba34e6504698e99ef5607b86e
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-02-18 20:50:01 +0100

    III-666: Move PlaceProjectedToJSONLD

---
+++ src/Place/Events/PlaceProjectedToJSONLD.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CultuurNet\UDB3\Place;
+
+use Broadway\Serializer\SerializableInterface;
+use CultuurNet\UDB3\Offer\Events\AbstractEvent;
+
+class PlaceProjectedToJSONLD extends AbstractEvent implements SerializableInterface
+{
+}

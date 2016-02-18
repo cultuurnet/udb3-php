d29af06e7c906783a3f896e7369cd190dd5837c8
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-02-18 20:49:35 +0100

    III-666: Move DocumentEventFactory & BroadcastingDocumentRepositoryDecorator

--- src/ReadModel/DocumentEventFactory.php
+++ src/ReadModel/DocumentEventFactory.php
@@ -2,13 +2,13 @@
 
 namespace CultuurNet\UDB3\Event\ReadModel;
 
-use CultuurNet\UDB3\Event\EventEvent;
+use CultuurNet\UDB3\Offer\Events\AbstractEvent;
 
 interface DocumentEventFactory
 {
     /**
      * @param $id
-     * @return EventEvent
+     * @return AbstractEvent
      */
     public function createEvent($id);
 }

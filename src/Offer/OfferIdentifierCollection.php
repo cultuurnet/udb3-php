6fbc8271fe5cf2da4b916dabe1949460f8971558
Bert Ramakers <bertramakers@me.com>
2016-02-23 19:23:59 +0100

    III-680: Document return type of toArray().

--- src/Offer/OfferIdentifierCollection.php
+++ src/Offer/OfferIdentifierCollection.php
@@ -4,6 +4,9 @@ namespace CultuurNet\UDB3\Offer;
 
 use TwoDotsTwice\Collection\AbstractCollection;
 
+/**
+ * @method OfferIdentifierInterface[] toArray()
+ */
 class OfferIdentifierCollection extends AbstractCollection
 {
     protected function getValidObjectType()

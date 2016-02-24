fbb2886168f593ba69f15bd27bbc6f0e78b87ed6
Bert Ramakers <bertramakers@me.com>
2016-02-24 16:25:18 +0100

    III-680: Docblock for better type hinting.

--- src/Offer/OfferIdentifierCollection.php
+++ src/Offer/OfferIdentifierCollection.php
@@ -5,6 +5,7 @@ namespace CultuurNet\UDB3\Offer;
 use TwoDotsTwice\Collection\AbstractCollection;
 
 /**
+ * @method OfferIdentifierCollection with($item)
  * @method OfferIdentifierInterface[] toArray()
  */
 class OfferIdentifierCollection extends AbstractCollection

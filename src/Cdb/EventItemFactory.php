d883f7dd7da2c72e62bbe7ae32a1197cbb3d4c32
Luc Wollants <luc.wollants@apptite.be>
2016-03-09 11:52:09 +0100

    III-810 Workaround for the bug that allowed UDB2 to have semicolons inside the keyword tag, these import events are still inside the event store.

--- src/Cdb/EventItemFactory.php
+++ src/Cdb/EventItemFactory.php
@@ -22,8 +22,62 @@ class EventItemFactory
             $namespaceUri
         );
 
-        return \CultureFeed_Cdb_Item_Event::parseFromCdbXml(
+        $event = \CultureFeed_Cdb_Item_Event::parseFromCdbXml(
             $udb2SimpleXml
         );
+
+        if (self::isEventOlderThenSplitKeywordFix($event)) {
+            $event = self::splitKeywordTagOnSemiColon(
+                $event
+            );
+        }
+
+        return $event;
+    }
+
+    /**
+     * UDB2 contained a bug that allowed for a keyword tag to have a semicolon.
+     * @param \CultureFeed_Cdb_Item_Event $event
+     * @return \CultureFeed_Cdb_Item_Event
+     */
+    private static function splitKeywordTagOnSemiColon(
+        \CultureFeed_Cdb_Item_Event $event
+    ) {
+        $event = clone $event;
+
+        /**
+         * @var \CultureFeed_Cdb_Data_Keyword[] $keywords
+         */
+        $keywords = $event->getKeywords(true);
+
+        foreach ($keywords as $keyword) {
+            $individualKeywords = explode(';', $keyword->getValue());
+
+            if (count($individualKeywords) > 1) {
+                $event->deleteKeyword($keyword);
+
+                foreach ($individualKeywords as $individualKeyword) {
+                    $cultureFeed_Cdb_Data_Keyword = new \CultureFeed_Cdb_Data_Keyword(
+                        trim($individualKeyword),
+                        $keyword->isVisible()
+                    );
+                    $event->addKeyword(
+                        $cultureFeed_Cdb_Data_Keyword
+                    );
+                }
+            }
+        }
+
+        return $event;
+    }
+
+    /**
+     * @param \CultureFeed_Cdb_Item_Event $event
+     * @return bool
+     */
+    private static function isEventOlderThenSplitKeywordFix(
+        \CultureFeed_Cdb_Item_Event $event
+    ) {
+        return $event->getLastUpdated() < '2016-03-10T00:00:00';
     }
 }

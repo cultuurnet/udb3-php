f2daac210f0fcfd8c98797cf5686ca2be0ea41d3
Kristof Coomans <kristof@2dotstwice.be>
2016-03-09 21:21:58 +0100

    III-810: Catch & log exceptions in CdbXmlCreatedByToUserIdResolver

--- src/UiTID/CdbXmlCreatedByToUserIdResolver.php
+++ src/UiTID/CdbXmlCreatedByToUserIdResolver.php
@@ -6,6 +6,7 @@
 namespace CultuurNet\UDB3\UiTID;
 
 use CultuurNet\UDB3\Cdb\CreatedByToUserIdResolverInterface;
+use Exception;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
@@ -36,17 +37,43 @@ class CdbXmlCreatedByToUserIdResolver implements LoggerAwareInterface, CreatedBy
      */
     public function resolveCreatedByToUserId(String $createdByIdentifier)
     {
+        $userId = null;
+
+        try {
+            $userId = $this->resolveByEmailOrByNick($createdByIdentifier);
+
+            if (!$userId) {
+                $this->logger->warning(
+                    'Unable to find user with identifier ' . $createdByIdentifier
+                );
+            }
+        }
+        catch (Exception $e) {
+            $this->logger->error(
+                sprintf(
+                    'An unexpected error occurred while resolving user with identifier %s',
+                    $createdByIdentifier
+                ),
+                [
+                    'exception' => $e,
+                ]
+            );
+        }
+
+        return $userId;
+    }
+
+    /**
+     * @param String $createdByIdentifier
+     * @return String
+     */
+    private function resolveByEmailOrByNick(String $createdByIdentifier)
+    {
         try {
             $email = new EmailAddress($createdByIdentifier->toNative());
             $userId = $this->users->byEmail($email);
         } catch (InvalidNativeArgumentException $e) {
             $userId = $this->users->byNick($createdByIdentifier);
-        }
-
-        if (!$userId) {
-            $this->logger->warning(
-                'Unable to find user with identifier ' . $createdByIdentifier
-            );
         }
 
         return $userId;

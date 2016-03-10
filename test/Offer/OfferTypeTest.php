6b50a78f289990786c426de3f060be7adb3c7126
Bert Ramakers <bertramakers@me.com>
2016-03-10 16:51:16 +0100

    III-666: (continued) clarification for test and actual value with incorrect casing.

--- test/Offer/OfferTypeTest.php
+++ test/Offer/OfferTypeTest.php
@@ -9,8 +9,10 @@ class OfferTypeTest extends \PHPUnit_Framework_TestCase
      */
     public function it_can_be_created_from_a_value_with_incorrect_casing()
     {
+        // Enum values are "Place" and "Event", so lower case with an
+        // uppercase first letter.
         $expectedOfferType = OfferType::PLACE();
-        $actualOfferType = OfferType::fromCaseInsensitiveValue('Place');
+        $actualOfferType = OfferType::fromCaseInsensitiveValue('place');
         $this->assertTrue($expectedOfferType->sameValueAs($actualOfferType));
 
         $expectedOfferType = OfferType::EVENT();

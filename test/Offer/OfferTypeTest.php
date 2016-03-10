059194abbe57b44d4927117d287edb9deb014464
Bert Ramakers <bertramakers@me.com>
2016-03-10 16:53:54 +0100

    III-666: More test data for OfferType instantiation.

--- test/Offer/OfferTypeTest.php
+++ test/Offer/OfferTypeTest.php
@@ -6,17 +6,38 @@ class OfferTypeTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
+     * @dataProvider offerTypeDataProvider
+     *
+     * @param string $enumValue
+     * @param OfferType $expectedOfferType
      */
-    public function it_can_be_created_from_a_value_with_incorrect_casing()
-    {
-        // Enum values are "Place" and "Event", so lower case with an
-        // uppercase first letter.
-        $expectedOfferType = OfferType::PLACE();
-        $actualOfferType = OfferType::fromCaseInsensitiveValue('place');
+    public function it_can_be_created_from_a_value_with_incorrect_casing(
+        $enumValue,
+        OfferType $expectedOfferType
+    ) {
+        $actualOfferType = OfferType::fromCaseInsensitiveValue($enumValue);
         $this->assertTrue($expectedOfferType->sameValueAs($actualOfferType));
+    }
 
-        $expectedOfferType = OfferType::EVENT();
-        $actualOfferType = OfferType::fromCaseInsensitiveValue('eVeNt');
-        $this->assertTrue($expectedOfferType->sameValueAs($actualOfferType));
+    public function offerTypeDataProvider()
+    {
+        return [
+            [
+                'place',
+                OfferType::PLACE(),
+            ],
+            [
+                'eVeNt',
+                OfferType::EVENT(),
+            ],
+            [
+                'Place',
+                OfferType::PLACE(),
+            ],
+            [
+                'EVENT',
+                OfferType::EVENT(),
+            ],
+        ];
     }
 }

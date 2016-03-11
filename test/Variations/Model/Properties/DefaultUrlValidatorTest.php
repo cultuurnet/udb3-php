c0994853271e53e834ff77588c65acb45ec17bdd
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-03-11 09:56:32 +0100

    III-666: Finish DefaultUrlValidatorTest

--- test/Variations/Model/Properties/DefaultUrlValidatorTest.php
+++ test/Variations/Model/Properties/DefaultUrlValidatorTest.php
@@ -2,23 +2,31 @@
 
 namespace CultuurNet\UDB3\Variations\Model\Properties;
 
+use CultuurNet\UDB3\EntityNotFoundException;
 use CultuurNet\UDB3\EntityServiceInterface;
 use CultuurNet\UDB3\EventNotFoundException;
 use CultuurNet\UDB3\EventServiceInterface;
+use CultuurNet\UDB3\Offer\IriOfferIdentifier;
 use CultuurNet\UDB3\Offer\IriOfferIdentifierFactoryInterface;
+use CultuurNet\UDB3\Offer\OfferType;
 use CultuurNet\UDB3\Variations\Command\ValidationException;
 
 class DefaultUrlValidatorTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var IriOfferIdentifierFactoryInterface
+     * @var IriOfferIdentifierFactoryInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $iriOfferIdentifierFactory;
 
     /**
      * @var EntityServiceInterface|\PHPUnit_Framework_MockObject_MockObject
      */
-    private $entityService;
+    private $eventService;
+
+    /**
+     * @var EntityServiceInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $placeService;
 
     /**
      * @var DefaultUrlValidator
@@ -28,9 +36,20 @@ class DefaultUrlValidatorTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->iriOfferIdentifierFactory = $this->getMock(IriOfferIdentifierFactoryInterface::class);
-        $this->entityService = $this->getMock(EntityServiceInterface::class);
-        $this->defaultUrlValidator(
+        $this->eventService = $this->getMock(EntityServiceInterface::class);
+        $this->placeService = $this->getMock(EntityServiceInterface::class);
+
+        $this->defaultUrlValidator = new DefaultUrlValidator(
             $this->iriOfferIdentifierFactory
+        );
+
+        $this->defaultUrlValidator = $this->defaultUrlValidator->withEntityService(
+            OfferType::EVENT(),
+            $this->eventService
+        );
+        $this->defaultUrlValidator = $this->defaultUrlValidator->withEntityService(
+            OfferType::PLACE(),
+            $this->placeService
         );
     }
 
@@ -39,18 +58,23 @@ class DefaultUrlValidatorTest extends \PHPUnit_Framework_TestCase
      */
     public function it_tries_to_get_the_event_when_an_event_url_is_given()
     {
-        $defaultUrlValidator = new DefaultUrlValidator(
-            $this->regExpPattern,
-            $this->eventService,
-            $this->placeService
-        );
-
         $this->eventService->expects($this->once())
-            ->method('getEvent')
+            ->method('getEntity')
             ->with('foo-bar');
 
-        $defaultUrlValidator->validateUrl(
-            new Url('https://culudb-silex.dev:8080/event/foo-bar')
+        $identifier = new IriOfferIdentifier(
+            'https://foo.bar/event/foo-bar',
+            'foo-bar',
+            OfferType::EVENT()
+        );
+
+        $this->iriOfferIdentifierFactory->expects($this->once())
+            ->method('fromIri')
+            ->with('https://foo.bar/event/foo-bar')
+            ->willReturn($identifier);
+
+        $this->defaultUrlValidator->validateUrl(
+            new Url('https://foo.bar/event/foo-bar')
         );
     }
 
@@ -59,18 +83,23 @@ class DefaultUrlValidatorTest extends \PHPUnit_Framework_TestCase
      */
     public function it_tries_to_get_the_place_when_a_place_url_is_given()
     {
-        $defaultUrlValidator = new DefaultUrlValidator(
-            $this->regExpPattern,
-            $this->eventService,
-            $this->placeService
-        );
-
         $this->placeService->expects($this->once())
             ->method('getEntity')
-            ->with('foo-bar');
+            ->with('foo-bar-place');
 
-        $defaultUrlValidator->validateUrl(
-            new Url('https://culudb-silex.dev:8080/place/foo-bar')
+        $identifier = new IriOfferIdentifier(
+            'https://foo.bar/place/foo-bar-place',
+            'foo-bar-place',
+            OfferType::PLACE()
+        );
+
+        $this->iriOfferIdentifierFactory->expects($this->once())
+            ->method('fromIri')
+            ->with('https://foo-bar/place/foo-bar-place')
+            ->willReturn($identifier);
+
+        $this->defaultUrlValidator->validateUrl(
+            new Url('https://foo-bar/place/foo-bar-place')
         );
     }
 
@@ -79,24 +108,64 @@ class DefaultUrlValidatorTest extends \PHPUnit_Framework_TestCase
      */
     public function it_throws_an_error_if_the_event_cannot_be_found()
     {
-        $defaultUrlValidator = new DefaultUrlValidator(
-            $this->regExpPattern,
-            $this->eventService,
-            $this->placeService
+        $this->eventService->expects($this->once())
+            ->method('getEntity')
+            ->with('foo-bar')
+            ->willThrowException(new EntityNotFoundException());
+
+        $identifier = new IriOfferIdentifier(
+            'https://foo.bar/event/foo-bar',
+            'foo-bar',
+            OfferType::EVENT()
         );
 
-        $this->eventService->expects($this->once())
-            ->method('getEvent')
-            ->with('foo-bar')
-            ->willThrowException(new EventNotFoundException());
+        $this->iriOfferIdentifierFactory->expects($this->once())
+            ->method('fromIri')
+            ->with('https://foo-bar/event/foo-bar')
+            ->willReturn($identifier);
 
         $this->setExpectedException(
             ValidationException::class,
             'Invalid data.'
         );
 
+        $this->defaultUrlValidator->validateUrl(
+            new Url('https://foo-bar/event/foo-bar')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_error_if_no_entity_service_can_be_found_for_a_certain_offer_type()
+    {
+        $defaultUrlValidator = new DefaultUrlValidator(
+            $this->iriOfferIdentifierFactory
+        );
+
+        $defaultUrlValidator = $defaultUrlValidator->withEntityService(
+            OfferType::EVENT(),
+            $this->eventService
+        );
+
+        $identifier = new IriOfferIdentifier(
+            'https://foo.bar/place/foo-bar-place',
+            'foo-bar-place',
+            OfferType::PLACE()
+        );
+
+        $this->iriOfferIdentifierFactory->expects($this->once())
+            ->method('fromIri')
+            ->with('https://foo-bar/place/foo-bar-place')
+            ->willReturn($identifier);
+
+        $this->setExpectedException(
+            \LogicException::class,
+            'Found no repository for type Place.'
+        );
+
         $defaultUrlValidator->validateUrl(
-            new Url('https://culudb-silex.dev:8080/event/foo-bar')
+            new Url('https://foo-bar/place/foo-bar-place')
         );
     }
 }

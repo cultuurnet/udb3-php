c27e50b6211fbc16d60454f89f4deea2c07b3375
Larry Boltovskoi <larry.bolt@gmail.com>
2016-04-17 22:53:26 +0200

    III-877: Refactor BookingInfoUpdated to use AbstractBookingInfoEvent

---
+++ src/Offer/Events/AbstractBookingInfoEvent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CultuurNet\UDB3\Offer\Events;
+use \CultuurNet\UDB3\BookingInfo;
+
+abstract class AbstractBookingInfoEvent extends AbstractEvent
+{
+    protected $bookingInfo;
+    /**
+     * @param string $id
+     * @param Object $bookingInfo
+     */
+    public function __construct($id, bookingInfo $bookingInfo)
+    {
+        parent::__construct($id);
+        $this->bookingInfo = $bookingInfo;
+    }
+
+    /**
+     * @return mixed The object instance
+     */
+    public static function deserialize(array $data)
+    {
+        return new static($data['item_id'], BookingInfo::deserialize($data['bookingInfo']));
+    }
+}

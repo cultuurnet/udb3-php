a281e4ef65f5a4355a0dfd1ff8b9030b6633cae9
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-04-25 14:03:46 +0200

    III-877: Add AbstractUpdateTypicalAgeRangeTest

---
+++ test/Offer/Commands/AbstractUpdateTypicalAgeRangeTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CultuurNet\UDB3\Offer\Commands;
+
+class AbstractUpdateTypicalAgeRangeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AbstractUpdateTypicalAgeRange|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $updateTypicalAgeRange;
+
+    /**
+     * @var string
+     */
+    protected $itemId;
+
+    /**
+     * @var string
+     */
+    protected $typicalAgeRange;
+
+    public function setUp()
+    {
+        $this->itemId = 'Foo';
+        $this->typicalAgeRange = '3-12';
+
+        $this->updateTypicalAgeRange = $this->getMockForAbstractClass(
+            AbstractUpdateTypicalAgeRange::class,
+            array($this->itemId, $this->typicalAgeRange)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_return_its_properties()
+    {
+        $typicalAgeRange = $this->updateTypicalAgeRange->getTypicalAgeRange();
+        $expectedTypicalAgeRange = '3-12';
+
+        $this->assertEquals($expectedTypicalAgeRange, $typicalAgeRange);
+
+        $itemId = $this->updateTypicalAgeRange->getId();
+        $expectedItemId = 'Foo';
+
+        $this->assertEquals($expectedItemId, $itemId);
+    }
+}

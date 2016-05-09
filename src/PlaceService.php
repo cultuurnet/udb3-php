69a86dc5b6f8984fbd1b0ec8ed7b445a40134b5d
Luc Wollants <luc.wollants@apptite.be>
2016-05-09 12:46:38 +0200

    III-943 Marked PlaceService as obsolete and should use LocalPlaceService instead.

--- src/PlaceService.php
+++ src/PlaceService.php
@@ -4,6 +4,11 @@
  */
 namespace CultuurNet\UDB3;
 
+/**
+ * Class PlaceService
+ * @package CultuurNet\UDB3
+ * @deprecated use LocalPlaceService instead
+ */
 class PlaceService extends LocalEntityService
 {
 }

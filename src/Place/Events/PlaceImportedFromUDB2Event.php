102766a3333b13699cf53b647e3e94a5e64e08c1
Bert Ramakers <bertramakers@me.com>
2016-05-18 18:30:17 +0200

    III-972: PlaceImportedFromUDB2Event is actually not an "ActorImportedFromUDB2".

--- src/Place/Events/PlaceImportedFromUDB2Event.php
+++ src/Place/Events/PlaceImportedFromUDB2Event.php
@@ -1,13 +1,12 @@
 <?php
-/**
- * @file
- */
 
 namespace CultuurNet\UDB3\Place\Events;
 
 use CultuurNet\UDB3\Actor\ActorImportedFromUDB2;
 
+/**
+ * @todo This actually contains event cdbxml, not actor cdbxml.
+ */
 class PlaceImportedFromUDB2Event extends ActorImportedFromUDB2
 {
-
 }

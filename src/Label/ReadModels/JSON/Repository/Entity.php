936117120e68cc8838b0ed4691a77630bbf53e50
Luc Wollants <luc.wollants@apptite.be>
2016-05-30 16:16:09 +0200

    III-945 Added entity class which represents a object stored inside the json read model.

---
+++ src/Label/ReadModels/JSON/Repository/Entity.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace CultuurNet\UDB3\Label\ReadModels\JSON\Repository;
+
+use CultuurNet\UDB3\Label\ValueObjects\Privacy;
+use CultuurNet\UDB3\Label\ValueObjects\Visibility;
+use ValueObjects\Identity\UUID;
+use ValueObjects\Number\Natural;
+use ValueObjects\String\String as StringLiteral;
+
+class Entity
+{
+    /**
+     * @var UUID
+     */
+    private $uuid;
+
+    /**
+     * @var StringLiteral
+     */
+    private $name;
+
+    /**
+     * @var Visibility
+     */
+    private $visibility;
+
+    /**
+     * @var Privacy
+     */
+    private $privacy;
+
+    /**
+     * @var UUID
+     */
+    private $parentUuid;
+
+    /**
+     * @var Natural
+     */
+    private $count;
+
+    /**
+     * LabelEntity constructor.
+     * @param UUID $uuid
+     * @param StringLiteral $name
+     * @param Visibility $visibility
+     * @param Privacy $privacy
+     * @param UUID $parentUuid
+     * @param Natural $count
+     */
+    public function __construct(
+        UUID $uuid,
+        StringLiteral $name,
+        Visibility $visibility,
+        Privacy $privacy,
+        UUID $parentUuid = null,
+        Natural $count = null
+    ) {
+        $this->uuid = $uuid;
+        $this->name = $name;
+        $this->visibility = $visibility;
+        $this->privacy = $privacy;
+        $this->parentUuid = $parentUuid;
+        $this->count = $count;
+    }
+
+    /**
+     * @return UUID
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @return StringLiteral
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Visibility
+     */
+    public function getVisibility()
+    {
+        return $this->visibility;
+    }
+
+    /**
+     * @return Privacy
+     */
+    public function getPrivacy()
+    {
+        return $this->privacy;
+    }
+
+    /**
+     * @return UUID
+     */
+    public function getParentUuid()
+    {
+        return $this->parentUuid;
+    }
+
+    /**
+     * @return Natural
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+}

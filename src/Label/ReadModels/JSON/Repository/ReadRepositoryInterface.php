1340979de509e26eae2b346d5f4c784e5b516e74
Luc Wollants <luc.wollants@apptite.be>
2016-05-30 16:24:45 +0200

    III-945 Added interface for reading entities from the JSON projection repository.

---
+++ src/Label/ReadModels/JSON/Repository/ReadRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CultuurNet\UDB3\Label\ReadModels\JSON\Repository;
+
+use ValueObjects\Identity\UUID;
+use ValueObjects\String\String as StringLiteral;
+
+interface ReadRepositoryInterface
+{
+    /**
+     * @param UUID $uuid
+     * @return Entity|null
+     */
+    public function getByUuid(UUID $uuid);
+
+    /**
+     * @param StringLiteral $name
+     * @return Entity|null
+     */
+    public function getByName(StringLiteral $name);
+
+    // TODO: Add find methods with pagination.
+}

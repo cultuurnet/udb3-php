30eb51a759258e3f96c0f5002c9f5fed60cc1d6a
Luc Wollants <luc.wollants@apptite.be>
2016-05-31 10:41:40 +0200

    III-945 Did some refactoring and added a base class for dbal tests of the json projection.

--- test/Label/ReadModels/JSON/Repository/Doctrine/DBALWriteRepositoryTest.php
+++ test/Label/ReadModels/JSON/Repository/Doctrine/DBALWriteRepositoryTest.php
@@ -2,24 +2,15 @@
 
 namespace CultuurNet\UDB3\Label\ReadModels\JSON\Repository\Doctrine;
 
-use CultuurNet\UDB3\DBALTestConnectionTrait;
 use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\Entity;
 use CultuurNet\UDB3\Label\ValueObjects\Privacy;
 use CultuurNet\UDB3\Label\ValueObjects\Visibility;
 use ValueObjects\Identity\UUID;
 use ValueObjects\Number\Natural;
 use ValueObjects\String\String as StringLiteral;
-use ValueObjects\String\String;
 
-class DBALWriteRepositoryTest extends \PHPUnit_Framework_TestCase
+class DBALWriteRepositoryTest extends BaseDBALRepositoryTest
 {
-    use DBALTestConnectionTrait;
-
-    /**
-     * @var StringLiteral
-     */
-    private $tableName;
-
     /**
      * @var DBALWriteRepository
      */
@@ -27,17 +18,11 @@ class DBALWriteRepositoryTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->tableName = new StringLiteral('test_places_json');
-
-        $schemaConfigurator = new SchemaConfigurator($this->tableName);
-
-        $schemaManager = $this->getConnection()->getSchemaManager();
-
-        $schemaConfigurator->configure($schemaManager);
+        parent::setUp();
 
         $this->dbalWriteRepository = new DBALWriteRepository(
-            $this->connection,
-            $this->tableName
+            $this->getConnection(),
+            $this->getTableName()
         );
     }
 
@@ -220,67 +205,6 @@ class DBALWriteRepositoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(
             new Natural(665),
             $actualEntity->getCount()
-        );
-    }
-
-    /**
-     * @param Entity $entity
-     */
-    private function saveEntity(Entity $entity)
-    {
-        $values = $this->entityToValues($entity);
-
-        $sql = 'INSERT INTO ' . $this->tableName . ' VALUES (?, ?, ?, ?, ?, ?)';
-
-        $this->connection->executeQuery($sql, $values);
-    }
-
-    /**
-     * @param Entity $entity
-     * @return array
-     */
-    private function entityToValues(Entity $entity)
-    {
-        return [
-            $entity->getUuid()->toNative(),
-            $entity->getName()->toNative(),
-            $entity->getVisibility() === Visibility::VISIBLE()
-                ? true : false,
-            $entity->getPrivacy() === Privacy::PRIVACY_PRIVATE()
-                ? true : false,
-            $entity->getParentUuid()->toNative(),
-            $entity->getCount()->toNative()
-        ];
-    }
-
-    /**
-     * @return Entity
-     */
-    private function getEntity()
-    {
-        $sql = 'SELECT * FROM ' . $this->tableName;
-
-        $statement = $this->connection->executeQuery($sql);
-        $row = $statement->fetch(\PDO::FETCH_ASSOC);
-
-        return $this->rowToEntity($row);
-    }
-
-    /**
-     * @param array $row
-     * @return Entity
-     */
-    private function rowToEntity(array $row)
-    {
-        return new Entity(
-            new UUID($row[SchemaConfigurator::UUID_COLUMN]),
-            new String($row[SchemaConfigurator::NAME_COLUMN]),
-            $row[SchemaConfigurator::VISIBLE_COLUMN]
-                ? Visibility::VISIBLE() : Visibility::INVISIBLE(),
-            $row[SchemaConfigurator::PRIVATE_COLUMN]
-                ? Privacy::PRIVACY_PRIVATE() : Privacy::PRIVACY_PUBLIC(),
-            new UUID($row[SchemaConfigurator::PARENT_UUID_COLUMN]),
-            new Natural($row[SchemaConfigurator::COUNT_COLUMN])
         );
     }
 }

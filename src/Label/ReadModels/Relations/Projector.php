aaef887fee322e9326461870b80903de2426d500
Luc Wollants <luc.wollants@apptite.be>
2016-06-03 09:04:45 +0200

    III-945 Added projector for label relations.

---
+++ src/Label/ReadModels/Relations/Projector.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace CultuurNet\UDB3\Label\ReadModels\Relations;
+
+use CultuurNet\UDB3\Label\ReadModels\AbstractProjector;
+use CultuurNet\UDB3\Label\ReadModels\Helper\LabelEventHelper;
+use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\Entity;
+use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\WriteRepositoryInterface;
+use CultuurNet\UDB3\Offer\Events\AbstractLabelAdded;
+use CultuurNet\UDB3\Offer\Events\AbstractLabelDeleted;
+use CultuurNet\UDB3\Offer\Events\AbstractLabelEvent;
+
+class Projector extends AbstractProjector
+{
+    /**
+     * @var LabelEventHelper
+     */
+    private $labelEventHelper;
+
+    /**
+     * @var WriteRepositoryInterface
+     */
+    private $writeRepository;
+
+    /**
+     * Projector constructor.
+     * @param LabelEventHelper $labelEventHelper
+     * @param WriteRepositoryInterface $writeRepository
+     */
+    public function __construct(
+        LabelEventHelper $labelEventHelper,
+        WriteRepositoryInterface $writeRepository
+    ) {
+        $this->labelEventHelper = $labelEventHelper;
+        $this->writeRepository = $writeRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function applyLabelAdded(AbstractLabelAdded $labelAdded)
+    {
+        $entity = $this->createEntity($this->labelEventHelper, $labelAdded);
+
+        $this->writeRepository->save(
+            $entity->getUuid(),
+            $entity->getRelationType(),
+            $entity->getRelationId()
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function applyLabelDeleted(AbstractLabelDeleted $labelDeleted)
+    {
+        $entity = $this->createEntity($this->labelEventHelper, $labelDeleted);
+
+        $this->writeRepository->deleteByUuid($entity->getUuid());
+    }
+
+    /**
+     * @param LabelEventHelper $labelEventHelper
+     * @param AbstractLabelEvent $labelEvent
+     * @return Entity
+     */
+    private function createEntity(
+        LabelEventHelper $labelEventHelper,
+        AbstractLabelEvent $labelEvent
+    ) {
+        $uuid = $labelEventHelper->getUuid($labelEvent);
+        $relationType = $labelEventHelper->getRelationType($labelEvent);
+        $relationId = $labelEventHelper->getRelationId($labelEvent);
+
+        return new Entity(
+            $uuid,
+            $relationType,
+            $relationId
+        );
+    }
+}
---
+++ test/Label/ReadModels/Relations/ProjectorTest.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace CultuurNet\UDB3\Label\ReadModels\Relations;
+
+use Broadway\Domain\DateTime as BroadwayDateTime;
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Event\Events\LabelAdded as LabelAddedToEvent;
+use CultuurNet\UDB3\Event\Events\LabelDeleted as LabelDeletedFromEvent;
+use CultuurNet\UDB3\Label;
+use CultuurNet\UDB3\Label\ReadModels\Helper\LabelEventHelper;
+use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface;
+use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\WriteRepositoryInterface;
+use CultuurNet\UDB3\Label\ValueObjects\RelationType;
+use CultuurNet\UDB3\Offer\Events\AbstractEvent;
+use CultuurNet\UDB3\Offer\Events\AbstractLabelAdded;
+use CultuurNet\UDB3\Offer\Events\AbstractLabelDeleted;
+use CultuurNet\UDB3\Offer\Events\AbstractLabelEvent;
+use CultuurNet\UDB3\Place\Events\LabelAdded as LabelAddedToPlace;
+use CultuurNet\UDB3\Place\Events\LabelDeleted as LabelDeletedFromPlace;
+use ValueObjects\Identity\UUID;
+use ValueObjects\String\String as StringLiteral;
+
+class ProjectorTest extends \PHPUnit_Framework_TestCase
+{
+    const RELATION_ID = 'relationId';
+
+    /**
+     * @var UUID
+     */
+    private $uuid;
+
+    /**
+     * @var WriteRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $writeRepository;
+
+    /**
+     * @var ReadRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $readRepository;
+
+    /**
+     * @var LabelEventHelper|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $labelEventHelper;
+
+    /**
+     * @var Projector
+     */
+    private $projector;
+
+    protected function setUp()
+    {
+        $this->uuid = new UUID();
+
+        $this->writeRepository = $this->getMock(
+            WriteRepositoryInterface::class
+        );
+
+        $this->readRepository = $this->getMock(
+            ReadRepositoryInterface::class
+        );
+
+        $this->labelEventHelper = $this->getMock(
+            LabelEventHelper::class,
+            [],
+            [$this->readRepository]
+        );
+        $this->mockLabelEventHelper();
+
+        $this->projector = new Projector(
+            $this->labelEventHelper,
+            $this->writeRepository
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_label_added_to_event()
+    {
+        $labelAdded = new LabelAddedToEvent(
+            self::RELATION_ID,
+            new Label('labelName')
+        );
+
+        $this->handleAdding($labelAdded);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_label_deleted_from_event()
+    {
+        $labelDeleted = new LabelDeletedFromEvent(
+            self::RELATION_ID,
+            new Label('labelName')
+        );
+
+        $this->handleDeleting($labelDeleted);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_label_added_to_place()
+    {
+        $labelAdded = new LabelAddedToPlace(
+            self::RELATION_ID,
+            new Label('labelName')
+        );
+
+        $this->handleAdding($labelAdded);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_label_deleted_from_place()
+    {
+        $labelDeleted = new LabelDeletedFromPlace(
+            self::RELATION_ID,
+            new Label('labelName')
+        );
+
+        $this->handleDeleting($labelDeleted);
+    }
+
+    /**
+     * @param string $id
+     * @param AbstractEvent|AbstractLabelEvent $payload
+     * @return DomainMessage
+     */
+    private function createDomainMessage($id, $payload)
+    {
+        return new DomainMessage(
+            $id,
+            0,
+            new Metadata(),
+            $payload,
+            BroadwayDateTime::now()
+        );
+    }
+
+    /**
+     * @param AbstractLabelAdded $labelAdded
+     */
+    private function handleAdding(AbstractLabelAdded $labelAdded)
+    {
+        $domainMessage = $this->createDomainMessage(
+            $labelAdded->getItemId(),
+            $labelAdded
+        );
+
+        $expectedRelationType = $this->getRelationType($labelAdded);
+
+        $this->writeRepository->expects($this->once())
+            ->method('save')
+            ->with(
+                $this->uuid,
+                $expectedRelationType,
+                new StringLiteral(self::RELATION_ID)
+            );
+
+        $this->projector->handle($domainMessage);
+    }
+
+    /**
+     * @param AbstractLabelDeleted $labelDeleted
+     */
+    private function handleDeleting(AbstractLabelDeleted $labelDeleted)
+    {
+        $domainMessage = $this->createDomainMessage(
+            $labelDeleted->getItemId(),
+            $labelDeleted
+        );
+
+        $this->writeRepository->expects($this->once())
+            ->method('deleteByUuid')
+            ->with($this->uuid);
+
+        $this->projector->handle($domainMessage);
+    }
+
+    private function mockLabelEventHelper()
+    {
+        $this->mockGetUuid();
+        $this->mockGetRelationType();
+        $this->mockGetRelationId();
+    }
+
+    private function mockGetUuid()
+    {
+        $this->labelEventHelper->method('getUuid')
+            ->will($this->returnValue($this->uuid));
+    }
+
+    private function mockGetRelationType()
+    {
+        $this->labelEventHelper->method('getRelationType')
+            ->will($this->returnCallback(function ($labelEvent) {
+                return $this->getRelationType($labelEvent);
+            }));
+    }
+
+    private function mockGetRelationId()
+    {
+        $this->labelEventHelper->method('getRelationId')
+            ->will($this->returnValue(new StringLiteral(self::RELATION_ID)));
+    }
+
+    /**
+     * @param AbstractLabelEvent $labelEvent
+     * @return RelationType|null
+     */
+    private function getRelationType(AbstractLabelEvent $labelEvent)
+    {
+        $relationType = null;
+
+        if ($labelEvent instanceof LabelAddedToPlace ||
+            $labelEvent instanceof LabelDeletedFromPlace) {
+            $relationType = RelationType::PLACE();
+        } else if ($labelEvent instanceof LabelAddedToEvent ||
+            $labelEvent instanceof LabelDeletedFromEvent) {
+            $relationType = RelationType::EVENT();
+        }
+
+        return $relationType;
+    }
+}

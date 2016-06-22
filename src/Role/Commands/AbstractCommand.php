10760efc50926b6a8bc61f5ce781fa83529a383e
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-22 12:09:06 +0200

    III-940: Add commands

---
+++ src/Role/Commands/AbstractCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CultuurNet\UDB3\Role\Commands;
+
+use ValueObjects\Identity\UUID;
+
+abstract class AbstractCommand
+{
+    /**
+     * @var UUID
+     */
+    private $uuid;
+
+    /**
+     * AbstractCommand constructor.
+     * @param UUID $uuid
+     */
+    public function __construct(UUID $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    /**
+     * @return UUID
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+}

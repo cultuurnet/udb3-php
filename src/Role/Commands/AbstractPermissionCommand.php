f8024084f1b931e96d506e04e6505440285e0038
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-23 15:25:20 +0200

    III-940: Add AbstractPermissionCommand & RemovePermission command, update AddPermission

---
+++ src/Role/Commands/AbstractPermissionCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CultuurNet\UDB3\Role\Commands;
+
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use ValueObjects\Identity\UUID;
+
+class AbstractPermissionCommand extends AbstractCommand
+{
+    /**
+     * @var Permission
+     */
+    private $permission;
+
+    /**
+     * AddPermission constructor.
+     * @param UUID $uuid
+     * @param Permission $permission
+     */
+    public function __construct(
+        UUID $uuid,
+        Permission $permission
+    ) {
+        parent::__construct($uuid);
+
+        $this->permission = $permission;
+    }
+
+    /**
+     * @return Permission
+     */
+    public function getPermission()
+    {
+        return $this->permission;
+    }
+}
--- src/Role/Commands/AddPermission.php
+++ src/Role/Commands/AddPermission.php
@@ -5,32 +5,6 @@ namespace CultuurNet\UDB3\Role\Commands;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
 use ValueObjects\Identity\UUID;
 
-class AddPermission extends AbstractCommand
+class AddPermission extends AbstractPermissionCommand
 {
-    /**
-     * @var Permission
-     */
-    private $permission;
-
-    /**
-     * AddPermission constructor.
-     * @param UUID $uuid
-     * @param Permission $permission
-     */
-    public function __construct(
-        UUID $uuid,
-        Permission $permission
-    ) {
-        parent::__construct($uuid);
-
-        $this->permission = $permission;
-    }
-
-    /**
-     * @return Permission
-     */
-    public function getPermission()
-    {
-        return $this->permission;
-    }
 }

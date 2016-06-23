5485cdebd4b3649f7596f0e5e49aad290ec46541
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-23 15:27:11 +0200

    III-940: Add AbstractEvent

---
+++ src/Role/Events/AbstractEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CultuurNet\UDB3\Role\Events;
+
+use Broadway\Serializer\SerializableInterface;
+use ValueObjects\Identity\UUID;
+
+abstract class AbstractEvent implements SerializableInterface
+{
+    const UUID = 'uuid';
+
+    /**
+     * @var UUID
+     */
+    private $uuid;
+
+    /**
+     * AbstractEvent constructor.
+     * @param UUID $uuid
+     */
+    public function __construct(UUID $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    /**
+     * @return UUID
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function deserialize(array $data)
+    {
+        return new static(new UUID($data[self::UUID]));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serialize()
+    {
+        return [self::UUID => $this->getUuid()->toNative()];
+    }
+}

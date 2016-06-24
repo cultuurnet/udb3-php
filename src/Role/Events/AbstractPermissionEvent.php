38d05026fe9ecb2876540a5d8f3ff938014b33f1
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-24 14:39:57 +0200

    III-940: Add AbstractPermissionEvent

---
+++ src/Role/Events/AbstractPermissionEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CultuurNet\UDB3\Role\Events;
+
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use ValueObjects\Identity\UUID;
+
+class AbstractPermissionEvent extends AbstractEvent
+{
+    /**
+     * @var Permission
+     */
+    private $permission;
+
+    /**
+     * AbstractPermissionEvent constructor.
+     * @param UUID $uuid
+     * @param Permission $permission
+     */
+    public function __construct(UUID $uuid, Permission $permission)
+    {
+        parent::__construct($uuid);
+        $this->permission = $permission;
+    }
+
+    /**
+     * @return Permission
+     */
+    public function getPermission()
+    {
+        return $this->permission;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function deserialize(array $data)
+    {
+        return new static($data['uuid'], Permission::fromNative($data['permission']));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serialize()
+    {
+        return parent::serialize() + array(
+            'permission' => $this->permission->toNative(),
+        );
+    }
+}

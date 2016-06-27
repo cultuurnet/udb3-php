1e5a32aa5d9531ebb5c684d8bed563a6013a0c83
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-27 16:04:42 +0200

    III-940: Add AbstractEventTest

---
+++ test/Role/Events/AbstractEventTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CultuurNet\UDB3\Role\Events;
+
+use ValueObjects\Identity\UUID;
+
+class AbstractEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UUID
+     */
+    protected $uuid;
+
+    /**
+     * @var AbstractEvent
+     */
+    protected $event;
+
+    protected function setUp()
+    {
+        $this->uuid = new UUID();
+
+        $this->event = $this->getMockForAbstractClass(
+            AbstractEvent::class,
+            [$this->uuid]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_a_uuid()
+    {
+        $this->assertEquals($this->uuid, $this->event->getUuid());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_serialize()
+    {
+        $actualArray = $this->event->serialize();
+
+        $expectedArray = [AbstractEvent::UUID => $this->uuid->toNative()];
+
+        $this->assertEquals($expectedArray, $actualArray);
+    }
+
+    public function it_can_deserialize()
+    {
+        $data = ['uuid' => $this->uuid->toNative()];
+        $actualEvent = $this->event->deserialize($data);
+        $expectedEvent = $this->event;
+
+        $this->assertEquals($actualEvent, $expectedEvent);
+    }
+}

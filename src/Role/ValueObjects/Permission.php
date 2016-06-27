196147661b8788db776c8138381e4aa9430193c4
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-06-27 15:10:05 +0200

    III-940: Add the fixed list of permissions

--- src/Role/ValueObjects/Permission.php
+++ src/Role/ValueObjects/Permission.php
@@ -6,5 +6,11 @@ use ValueObjects\Enum\Enum;
 
 class Permission extends Enum
 {
-    
+    const AANBOD_INVOEREN = 'Aanbod invoeren';
+    const AANBOD_BEWERKEN = 'Aanbod bewerken';
+    const AANBOD_MODEREREN = 'Aanbod modereren';
+    const AANBOD_VERWIJDEREN = 'Aanbod verwijderen';
+    const ORGANISATIES_BEHEREN = 'Organisaties beheren';
+    const GEBRUIKERS_BEHEREN = 'Gebruikers beheren';
+    const LABELS_BEHEREN = 'Labels beheren';
 }

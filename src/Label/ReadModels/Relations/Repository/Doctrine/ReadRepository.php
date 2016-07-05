adb15bfbdf53431395d01dcd26f8fccfd76c6c54
Bert Ramakers <bertramakers@me.com>
2016-07-05 13:01:12 +0200

    III-1107: Remove unnecessary todo.

--- src/Label/ReadModels/Relations/Repository/Doctrine/ReadRepository.php
+++ src/Label/ReadModels/Relations/Repository/Doctrine/ReadRepository.php
@@ -15,15 +15,6 @@ class ReadRepository extends AbstractDBALRepository implements ReadRepositoryInt
      */
     public function getOfferLabelRelations(UUID $labelId)
     {
-//        TODO: This one gives a cryptic error message: " Illegal offset type"
-//        $queryBuilder = $this->createQueryBuilder()
-//            ->select(SchemaConfigurator::RELATION_ID_COLUMN)
-//            ->from($this->getTableName())
-//            ->where(SchemaConfigurator::UUID_COLUMN . ' = :label_id')
-//            ->setParameter(':label_id', (string) $labelId);
-//
-//        $results = $queryBuilder->execute();
-
         $query = $this
             ->getConnection()
             ->prepare(

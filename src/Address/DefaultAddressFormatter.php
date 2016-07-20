8e7a85e89c82d348541d3cbce25b169d3b60d94b
Bert Ramakers <bertramakers@me.com>
2016-07-20 12:05:03 +0200

    III-1260: AddressFormatterInterface with default implementation.

---
+++ src/Address/DefaultAddressFormatter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CultuurNet\UDB3\Address;
+
+use CultuurNet\UDB3\Address;
+
+class DefaultAddressFormatter implements AddressFormatterInterface
+{
+    /**
+     * @param Address $address
+     * @return string
+     */
+    public function format(Address $address)
+    {
+        return $address->getStreetAddress() . ', ' .
+            $address->getPostalCode() . ' ' .
+            $address->getLocality() . ', ' .
+            $address->getCountry();
+    }
+}

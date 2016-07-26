c7add1190ec50d05f1e4a2a05b308e37cd012261
Luc Wollants <luc.wollants@apptite.be>
2016-07-26 10:36:42 +0200

    III-1107 Only class that was marked as final which prevents the use of PHPUnit getMock, so the final keyword was removed.

--- src/Event/Events/LabelDeleted.php
+++ src/Event/Events/LabelDeleted.php
@@ -5,10 +5,8 @@
 
 namespace CultuurNet\UDB3\Event\Events;
 
-use CultuurNet\UDB3\Event\EventEvent;
-use CultuurNet\UDB3\Label;
 use CultuurNet\UDB3\Offer\Events\AbstractLabelDeleted;
 
-final class LabelDeleted extends AbstractLabelDeleted
+class LabelDeleted extends AbstractLabelDeleted
 {
 }

5641ee24c9abdbd3a560b41b12a05a9d4ecbb95d
Bert Ramakers <bertramakers@me.com>
2016-08-02 16:46:21 +0200

    III-1189: Test adding & removing users to/from the Role aggregate using commands.

--- test/Role/CommandHandlerTest.php
+++ test/Role/CommandHandlerTest.php
@@ -6,9 +6,11 @@ use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
 use Broadway\EventHandling\EventBusInterface;
 use Broadway\EventStore\EventStoreInterface;
 use CultuurNet\UDB3\Role\Commands\AddPermission;
+use CultuurNet\UDB3\Role\Commands\AddUser;
 use CultuurNet\UDB3\Role\Commands\CreateRole;
 use CultuurNet\UDB3\Role\Commands\DeleteRole;
 use CultuurNet\UDB3\Role\Commands\RemovePermission;
+use CultuurNet\UDB3\Role\Commands\RemoveUser;
 use CultuurNet\UDB3\Role\Commands\RenameRole;
 use CultuurNet\UDB3\Role\Commands\SetConstraint;
 use CultuurNet\UDB3\Role\Events\ConstraintCreated;
@@ -19,6 +21,8 @@ use CultuurNet\UDB3\Role\Events\PermissionRemoved;
 use CultuurNet\UDB3\Role\Events\RoleCreated;
 use CultuurNet\UDB3\Role\Events\RoleDeleted;
 use CultuurNet\UDB3\Role\Events\RoleRenamed;
+use CultuurNet\UDB3\Role\Events\UserAdded;
+use CultuurNet\UDB3\Role\Events\UserRemoved;
 use CultuurNet\UDB3\Role\ValueObjects\Permission;
 use ValueObjects\Identity\UUID;
 use ValueObjects\String\String as StringLiteral;
@@ -104,7 +108,7 @@ class CommandHandlerTest extends CommandHandlerScenarioTestCase
             $this->uuid,
             $this->name
         );
-        
+
         $this->roleRenamed = new RoleRenamed(
             $this->uuid,
             $this->name
@@ -114,7 +118,7 @@ class CommandHandlerTest extends CommandHandlerScenarioTestCase
             $this->uuid,
             $this->permission
         );
-        
+
         $this->permissionRemoved = new PermissionRemoved(
             $this->uuid,
             $this->permission
@@ -124,12 +128,12 @@ class CommandHandlerTest extends CommandHandlerScenarioTestCase
             $this->uuid,
             $this->query
         );
-        
+
         $this->constraintUpdated = new ConstraintUpdated(
             $this->uuid,
             $this->updatedQuery
         );
-        
+
         $this->constraintRemoved = new ConstraintRemoved(
             $this->uuid
         );
@@ -215,6 +219,111 @@ class CommandHandlerTest extends CommandHandlerScenarioTestCase
     /**
      * @test
      */
+    public function it_can_add_and_remove_users()
+    {
+        $userId = new StringLiteral('123456');
+
+        $this->scenario
+            ->withAggregateId($this->uuid)
+            ->given([$this->roleCreated])
+
+            // Add a user.
+            ->when(
+                new AddUser(
+                    $this->uuid,
+                    $userId
+                )
+            )
+            ->then(
+                [
+                    new UserAdded(
+                        $this->uuid,
+                        $userId
+                    ),
+                ]
+            )
+
+            // Adding the same user should not result in any new events.
+            ->when(
+                new AddUser(
+                    $this->uuid,
+                    $userId
+                )
+            )
+            ->then(
+                [
+                    new UserAdded(
+                        $this->uuid,
+                        $userId
+                    ),
+                ]
+            )
+
+            // Remove the user.
+            ->when(
+                new RemoveUser(
+                    $this->uuid,
+                    $userId
+                )
+            )
+            ->then(
+                [
+                    new UserAdded(
+                        $this->uuid,
+                        $userId
+                    ),
+                    new UserRemoved(
+                        $this->uuid,
+                        $userId
+                    ),
+                ]
+            )
+
+            // Removing the user again should not result in any new events.
+            ->when(
+                new RemoveUser(
+                    $this->uuid,
+                    $userId
+                )
+            )
+            ->then(
+                [
+                    new UserAdded(
+                        $this->uuid,
+                        $userId
+                    ),
+                    new UserRemoved(
+                        $this->uuid,
+                        $userId
+                    ),
+                ]
+            )
+
+            // Removing a user that was never added to the role should not
+            // result in any new events.
+            ->when(
+                new RemoveUser(
+                    $this->uuid,
+                    new StringLiteral('user-that-was-never-added')
+                )
+            )
+            ->then(
+                [
+                    new UserAdded(
+                        $this->uuid,
+                        $userId
+                    ),
+                    new UserRemoved(
+                        $this->uuid,
+                        $userId
+                    ),
+                ]
+            );
+    }
+
+    /**
+     * @test
+     */
     public function it_handles_setConstraint_by_creating_the_constraint()
     {
         $this->scenario
@@ -248,7 +357,7 @@ class CommandHandlerTest extends CommandHandlerScenarioTestCase
     public function it_handles_setConstraint_by_removing_the_constraint()
     {
         $query = new StringLiteral('');
-        
+
         $this->scenario
             ->withAggregateId($this->uuid)
             ->given([$this->roleCreated, $this->constraintCreated])

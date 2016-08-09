2a30a6424ffe315ae94c7a9042ba4aba75d9bc33
Luc Wollants <luc.wollants@apptite.be>
2016-08-09 11:59:55 +0200

    III-1280 DomainMessage is not needed for apply methods.

--- src/Role/ReadModel/Permissions/UserPermissionsProjector.php
+++ src/Role/ReadModel/Permissions/UserPermissionsProjector.php
@@ -2,12 +2,10 @@
 
 namespace CultuurNet\UDB3\Role\ReadModel\Permissions;
 
-use Broadway\Domain\DomainMessage;
 use Broadway\EventHandling\EventListenerInterface;
 use CultuurNet\UDB3\EventHandling\DelegateEventHandlingToSpecificMethodTrait;
 use CultuurNet\UDB3\Role\Events\PermissionAdded;
 use CultuurNet\UDB3\Role\Events\PermissionRemoved;
-use CultuurNet\UDB3\Role\Events\RoleCreated;
 use CultuurNet\UDB3\Role\Events\RoleDeleted;
 use CultuurNet\UDB3\Role\Events\UserAdded;
 use CultuurNet\UDB3\Role\Events\UserRemoved;
@@ -30,48 +28,42 @@ class UserPermissionsProjector implements EventListenerInterface
         $this->repository = $repository;
     }
 
-
     /**
      * @param RoleDeleted $roleDeleted
-     * @param DomainMessage $domainMessage
      */
-    public function applyRoleDeleted(RoleDeleted $roleDeleted, DomainMessage $domainMessage)
+    public function applyRoleDeleted(RoleDeleted $roleDeleted)
     {
         $this->repository->removeRole($roleDeleted->getUuid());
     }
 
     /**
      * @param UserAdded $userAdded
-     * @param DomainMessage $domainMessage
      */
-    public function applyUserAdded(UserAdded $userAdded, DomainMessage $domainMessage)
+    public function applyUserAdded(UserAdded $userAdded)
     {
         $this->repository->addUserRole($userAdded->getUserId(), $userAdded->getUuid());
     }
 
     /**
      * @param UserRemoved $userRemoved
-     * @param DomainMessage $domainMessage
      */
-    public function applyUserRemoved(UserRemoved $userRemoved, DomainMessage $domainMessage)
+    public function applyUserRemoved(UserRemoved $userRemoved)
     {
         $this->repository->removeUserRole($userRemoved->getUserId(), $userRemoved->getUuid());
     }
 
     /**
      * @param PermissionAdded $permissionAdded
-     * @param DomainMessage $domainMessage
      */
-    public function applyPermissionAdded(PermissionAdded $permissionAdded, DomainMessage $domainMessage)
+    public function applyPermissionAdded(PermissionAdded $permissionAdded)
     {
         $this->repository->addRolePermission($permissionAdded->getUuid(), $permissionAdded->getPermission());
     }
 
     /**
      * @param PermissionRemoved $permissionRemoved
-     * @param DomainMessage $domainMessage
      */
-    public function applyPermissionRemoved(PermissionRemoved $permissionRemoved, DomainMessage $domainMessage)
+    public function applyPermissionRemoved(PermissionRemoved $permissionRemoved)
     {
         $this->repository->removeRolePermission($permissionRemoved->getUuid(), $permissionRemoved->getPermission());
     }

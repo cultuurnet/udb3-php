ea476623c9dd9e8b7139fa1877992b0f64990315
Luc Wollants <luc.wollants@apptite.be>
2016-08-30 10:39:26 +0200

    III-1196 Cleanup header doc.

--- src/Offer/Security/SecurityInterface.php
+++ src/Offer/Security/SecurityInterface.php
@@ -3,22 +3,21 @@
 namespace CultuurNet\UDB3\Offer\Security;
 
 use CultuurNet\UDB3\Offer\Commands\AuthorizableCommandInterface;
-use ValueObjects\String\String;
+use ValueObjects\String\String as StringLiteral;
 
 interface SecurityInterface
 {
     /**
-     * @param String $offerId
+     * @param StringLiteral $offerId
      * @return boolean
      */
-    public function allowsUpdateWithCdbXml(String $offerId);
+    public function allowsUpdateWithCdbXml(StringLiteral $offerId);
 
     /**
      * Returns if the event allows updates through the UDB3 core APIs.
      *
      * @param AuthorizableCommandInterface $command
      * @return bool
-     * @internal param String $offerId
      */
     public function isAuthorized(AuthorizableCommandInterface $command);
 }

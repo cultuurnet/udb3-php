428008de101588e6b5d3746dafb68075adbad7b2
Luc Wollants <luc.wollants@apptite.be>
2016-09-15 14:07:33 +0200

    III-1230 Added simpel test to check if the moderation command extend from the abstract moderation command.

---
+++ test/Offer/Commands/Moderation/AbstractModerationCommandTestBase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CultuurNet\UDB3\Offer\Commands\Moderation;
+
+abstract class AbstractModerationCommandTestBase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_is_an_abstract_moderation_command()
+    {
+        $this->assertTrue(is_subclass_of(
+            $this->createModerationCommand(),
+            AbstractModerationCommand::class
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public abstract function getModerationCommandClass();
+
+    /**
+     * @return AbstractModerationCommand
+     */
+    private function createModerationCommand()
+    {
+        /** @var AbstractModerationCommand $abstractModerationCommand */
+        $abstractModerationCommand = $this->getMockForAbstractClass(
+            $this->getModerationCommandClass(),
+            [],
+            "",
+            false
+        );
+
+        return $abstractModerationCommand;
+    }
+}

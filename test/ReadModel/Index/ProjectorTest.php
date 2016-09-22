e4fcdc92a797503d422d2be2abfb3c64a8813e4c
Bram Cordie <bramcordie@gmail.com>
2016-09-22 23:22:06 +0200

    III-1325: Test if an index is projected when offers are created

--- test/ReadModel/Index/ProjectorTest.php
+++ test/ReadModel/Index/ProjectorTest.php
@@ -5,21 +5,33 @@ namespace CultuurNet\UDB3\ReadModel\Index;
 use Broadway\Domain\DateTime;
 use Broadway\Domain\DomainMessage;
 use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Address\Address;
+use CultuurNet\UDB3\Address\Locality;
+use CultuurNet\UDB3\Address\PostalCode;
+use CultuurNet\UDB3\Address\Street;
+use CultuurNet\UDB3\Calendar;
 use CultuurNet\UDB3\Cdb\CreatedByToUserIdResolverInterface;
+use CultuurNet\UDB3\Event\Events\EventCreated;
 use CultuurNet\UDB3\Event\Events\EventDeleted;
 use CultuurNet\UDB3\Event\Events\EventImportedFromUDB2;
 use CultuurNet\UDB3\Event\Events\EventProjectedToJSONLD;
+use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\Location\Location;
 use CultuurNet\UDB3\Offer\IriOfferIdentifier;
 use CultuurNet\UDB3\Offer\IriOfferIdentifierFactoryInterface;
 use CultuurNet\UDB3\Offer\OfferType;
 use CultuurNet\UDB3\Organizer\Events\OrganizerImportedFromUDB2;
+use CultuurNet\UDB3\Place\Events\PlaceCreated;
 use CultuurNet\UDB3\Place\Events\PlaceDeleted;
 use CultuurNet\UDB3\Place\Events\PlaceImportedFromUDB2;
 use CultuurNet\UDB3\Place\Events\PlaceImportedFromUDB2Event;
 use CultuurNet\UDB3\Place\Events\PlaceProjectedToJSONLD;
-use Guzzle\Common\Event;
+use CultuurNet\UDB3\Theme;
+use CultuurNet\UDB3\Title;
+use ValueObjects\Geography\Country;
 use ValueObjects\Web\Domain;
 use ValueObjects\Web\Url;
+use ValueObjects\String\String as StringLiteral;
 
 class ProjectorTest extends \PHPUnit_Framework_TestCase
 {
@@ -221,6 +233,86 @@ class ProjectorTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
+     */
+    public function it_should_add_an_indexed_item_when_an_event_is_created()
+    {
+        $this->repository->expects($this->once())
+            ->method('updateIndex')
+            ->with(
+                'f2b227c5-4756-49f6-a25d-8286b6a2351f',
+                EntityType::EVENT(),
+                '1adf21b4-711d-4e33-b9ef-c96843582a56',
+                'Algorave',
+                '9630',
+                Domain::specifyType('omd.be'),
+                $this->anything()
+            );
+
+        $this->projector->handle(
+            $this->domainMessage(
+                new EventCreated(
+                    'f2b227c5-4756-49f6-a25d-8286b6a2351f',
+                    new Title('Algorave'),
+                    new EventType('0.50.4.0.0', 'concert'),
+                    new Location(
+                        '335be568-aaf0-4147-80b6-9267daafe23b',
+                        new StringLiteral('Repeteerkot'),
+                        new Address(
+                            new Street('Kerkstraat 69'),
+                            new PostalCode('9630'),
+                            new Locality('Zottegem'),
+                            Country::fromNative('BE')
+                        )
+                    ),
+                    new Calendar(
+                        'permanent'
+                    ),
+                    new Theme('themeid', 'theme_label')
+                )
+            )
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_add_an_indexed_item_when_a_place_is_created()
+    {
+        $this->repository->expects($this->once())
+            ->method('updateIndex')
+            ->with(
+                'f2b227c5-4756-49f6-a25d-8286b6a2351f',
+                EntityType::PLACE(),
+                '1adf21b4-711d-4e33-b9ef-c96843582a56',
+                'Algorave',
+                '9630',
+                Domain::specifyType('omd.be'),
+                $this->anything()
+            );
+
+        $this->projector->handle(
+            $this->domainMessage(
+                new PlaceCreated(
+                    'f2b227c5-4756-49f6-a25d-8286b6a2351f',
+                    new Title('Algorave'),
+                    new EventType('0.50.4.0.0', 'concert'),
+                    new Address(
+                        new Street('Kerkstraat 69'),
+                        new PostalCode('9630'),
+                        new Locality('Zottegem'),
+                        Country::fromNative('BE')
+                    ),
+                    new Calendar(
+                        'permanent'
+                    ),
+                    new Theme('themeid', 'theme_label')
+                )
+            )
+        );
+    }
+
+    /**
+     * @test
      * @dataProvider indexUpdateEventsDataProvider
      */
     public function it_should_set_the_update_date_when_indexed_items_change(
@@ -305,7 +397,9 @@ class ProjectorTest extends \PHPUnit_Framework_TestCase
         return new DomainMessage(
             '123',
             1,
-            new Metadata(),
+            new Metadata([
+                'user_id' => '1adf21b4-711d-4e33-b9ef-c96843582a56'
+            ]),
             $payload,
             DateTime::fromString(self::DATETIME)
         );

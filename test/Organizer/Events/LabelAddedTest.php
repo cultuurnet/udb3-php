ee7926c7f3a2a3e25a55b28b7b7ab5d67750daab
Luc Wollants <luc.wollants@apptite.be>
2016-09-28 09:33:19 +0200

    III-1468 Added unit test for deserialize of a label added event.

--- test/Organizer/Events/LabelAddedTest.php
+++ test/Organizer/Events/LabelAddedTest.php
@@ -7,12 +7,52 @@ use ValueObjects\Identity\UUID;
 class LabelAddedTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var string
+     */
+    private $organizerId;
+
+    /**
+     * @var UUID
+     */
+    private $labelId;
+
+    /**
+     * @var LabelAdded
+     */
+    private $labelAdded;
+
+    protected function setUp()
+    {
+        $this->organizerId = 'organizerId';
+
+        $this->labelId = new UUID();
+
+        $this->labelAdded = new LabelAdded(
+            $this->organizerId,
+            $this->labelId
+        );
+    }
+
+    /**
      * @test
      */
     public function it_derives_from_abstract_label_event()
     {
-        $labelAdded = new LabelAdded('organizerId', new UUID());
+        $this->assertInstanceOf(AbstractLabelEvent::class, $this->labelAdded);
+    }
 
-        $this->assertInstanceOf(AbstractLabelEvent::class, $labelAdded);
+    /**
+     * @test
+     */
+    public function it_can_deserialize()
+    {
+        $labelAddedAsArray = [
+            'organizer_id' => $this->labelAdded->getOrganizerId(),
+            'labelId' => $this->labelAdded->getLabelId()
+        ];
+
+        $labelAdded = LabelAdded::deserialize($labelAddedAsArray);
+
+        $this->assertEquals($this->labelAdded, $labelAdded);
     }
 }

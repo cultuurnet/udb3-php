70d49f75414b89715607c348c3fa41d55358bbf1
Luc Wollants <luc.wollants@apptite.be>
2016-09-30 10:15:35 +0200

    III-1468 Added interface to check for label security based on uuid or label name.

---
+++ src/Security/LabelSecurityInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CultuurNet\UDB3\Security;
+
+use ValueObjects\Identity\UUID;
+use ValueObjects\String\String as StringLiteral;
+
+interface LabelSecurityInterface
+{
+    /**
+     * @return bool
+     */
+    public function isIdentifiedByUuid();
+
+    /**
+     * @return StringLiteral|null
+     */
+    public function getName();
+
+    /**
+     * @return UUID|null
+     */
+    public function getUuid();
+}

384021b3e4f253cd51d6b2041830575e08acaad4
Luc Wollants <luc.wollants@apptite.be>
2016-10-24 19:03:21 +0200

    III-1364 The abstract publish event now also has an optional embargo date.

--- src/Offer/Events/Moderation/AbstractPublished.php
+++ src/Offer/Events/Moderation/AbstractPublished.php
@@ -6,5 +6,42 @@ use CultuurNet\UDB3\Offer\Events\AbstractEvent;
 
 abstract class AbstractPublished extends AbstractEvent
 {
+    /** @var  \DateTimeInterface */
+    private $embargoDate;
 
+    /**
+     * AbstractPublish constructor.
+     * @param string $itemId
+     * @param \DateTimeInterface|null $embargoDate
+     */
+    public function __construct($itemId, \DateTimeInterface $embargoDate = null)
+    {
+        parent::__construct($itemId);
+
+        $this->embargoDate = $embargoDate;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getEmbargoDate()
+    {
+        return $this->embargoDate;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serialize()
+    {
+        return parent::serialize() + ['embargo_date' => $this->embargoDate];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function deserialize(array $data)
+    {
+        return new static($data['item_id'], $data['embargo_date']);
+    }
 }

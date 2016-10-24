99687dff6abbc3fef944b71259eb8e07460103b4
Luc Wollants <luc.wollants@apptite.be>
2016-10-24 15:51:24 +0200

    III-1364 The abstract publish command now has an optional embargo date parameter.

--- src/Offer/Commands/Moderation/AbstractPublish.php
+++ src/Offer/Commands/Moderation/AbstractPublish.php
@@ -4,5 +4,26 @@ namespace CultuurNet\UDB3\Offer\Commands\Moderation;
 
 abstract class AbstractPublish extends AbstractModerationCommand
 {
+    /** @var  \DateTimeInterface */
+    private $embargoDate;
 
+    /**
+     * AbstractPublish constructor.
+     * @param string $itemId
+     * @param \DateTimeInterface|null $embargoDate
+     */
+    public function __construct($itemId, \DateTimeInterface $embargoDate = null)
+    {
+        parent::__construct($itemId);
+
+        $this->embargoDate = $embargoDate;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getEmbargoDate()
+    {
+        return $this->embargoDate;
+    }
 }
--- test/Offer/Commands/Moderation/AbstractPublishTest.php
+++ test/Offer/Commands/Moderation/AbstractPublishTest.php
@@ -2,13 +2,65 @@
 
 namespace CultuurNet\UDB3\Offer\Commands\Moderation;
 
-class AbstractPublishTest extends AbstractModerationCommandTestBase
+use ValueObjects\Identity\UUID;
+
+class AbstractPublishTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @inheritdoc
+     * @var \DateTimeInterface
      */
-    public function getModerationCommandClass()
+    private $embargoDate;
+
+    /**
+     * @var AbstractPublish|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $abstractPublish;
+
+    public function setUp()
     {
-        return AbstractPublish::class;
+        $this->embargoDate = new \DateTime();
+
+        $this->abstractPublish = $this->getMockForAbstractClass(
+            AbstractPublish::class,
+            [new UUID(), $this->embargoDate]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_an_abstract_moderation_command()
+    {
+        $this->assertTrue(is_subclass_of(
+            $this->abstractPublish,
+            AbstractModerationCommand::class
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_store_an_embargo_date()
+    {
+        $this->assertEquals(
+            $this->embargoDate,
+            $this->abstractPublish->getEmbargoDate()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_default_embargo_date_of_null()
+    {
+        $abstractPublish = $this->getMockForAbstractClass(
+            AbstractPublish::class,
+            [new UUID()]
+        );
+
+        $this->assertEquals(
+            null,
+            $abstractPublish->getEmbargoDate()
+        );
     }
 }

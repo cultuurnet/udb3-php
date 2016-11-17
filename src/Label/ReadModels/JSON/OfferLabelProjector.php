2f16154a48b4c7cff2e24d69fbaece8a201f4976
Luc Wollants <luc.wollants@apptite.be>
2016-11-17 10:39:49 +0100

    III-1607 Removed the obsolote label name domain message enricher. The events for visibility and privacy now contain the label name that can be used by the projectors.

--- src/Label/ReadModels/JSON/OfferLabelProjector.php
+++ src/Label/ReadModels/JSON/OfferLabelProjector.php
@@ -3,19 +3,17 @@
 namespace CultuurNet\UDB3\Label\ReadModels\JSON;
 
 use Broadway\Domain\DomainMessage;
-use Broadway\Domain\Metadata;
 use Broadway\EventHandling\EventListenerInterface;
 use CultuurNet\UDB3\Event\ReadModel\DocumentGoneException;
 use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
-use CultuurNet\UDB3\EventHandling\DelegateEventHandlingToSpecificMethodTrait;
 use CultuurNet\UDB3\Label\Events\MadeInvisible;
 use CultuurNet\UDB3\Label\Events\MadeVisible;
-use CultuurNet\UDB3\Label\LabelDomainMessageEnricher;
 use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\ReadRepositoryInterface;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use ValueObjects\Identity\UUID;
+use ValueObjects\String\String as StringLiteral;
 
 class OfferLabelProjector implements EventListenerInterface, LoggerAwareInterface
 {
@@ -53,69 +51,60 @@ class OfferLabelProjector implements EventListenerInterface, LoggerAwareInterfac
         $event = $domainMessage->getPayload();
 
         if ($event instanceof MadeVisible) {
-            $this->applyMadeVisible($domainMessage->getPayload(), $domainMessage->getMetadata());
+            $this->applyMadeVisible($domainMessage->getPayload());
         } else if ($event instanceof MadeInvisible) {
-            $this->applyMadeInvisible($domainMessage->getPayload(), $domainMessage->getMetadata());
+            $this->applyMadeInvisible($domainMessage->getPayload());
         }
     }
 
     /**
      * @param MadeVisible $madeVisible
-     * @param Metadata $metaData
      */
-    public function applyMadeVisible(MadeVisible $madeVisible, Metadata $metaData)
+    public function applyMadeVisible(MadeVisible $madeVisible)
     {
-        $this->updateLabels($madeVisible->getUuid(), $metaData, true);
+        $this->updateLabels($madeVisible->getUuid(), $madeVisible->getName(), true);
     }
 
     /**
      * @param MadeInvisible $madeInvisible
-     * @param Metadata $metaData
      */
-    public function applyMadeInvisible(MadeInvisible $madeInvisible, Metadata $metaData)
+    public function applyMadeInvisible(MadeInvisible $madeInvisible)
     {
-        $this->updateLabels($madeInvisible->getUuid(), $metaData, false);
+        $this->updateLabels($madeInvisible->getUuid(), $madeInvisible->getName(), false);
     }
 
     /**
      * @param UUID $uuid
-     * @param Metadata $metaData
+     * @param StringLiteral $labelName
      * @param bool $madeVisible
      */
     private function updateLabels(
         UUID $uuid,
-        Metadata $metaData,
+        StringLiteral $labelName,
         $madeVisible
     ) {
-        $labelName = $this->getLabelName($metaData);
+        $offers = $this->getRelatedOffers($uuid);
 
-        if ($labelName) {
-            $offers = $this->getRelatedOffers($uuid);
+        $removeFrom = $madeVisible ? 'hiddenLabels' : 'labels';
+        $addTo = $madeVisible ? 'labels' : 'hiddenLabels';
 
-            $removeFrom = $madeVisible ? 'hiddenLabels' : 'labels';
-            $addTo = $madeVisible ? 'labels' : 'hiddenLabels';
+        foreach ($offers as $offer) {
+            $offerLd = $offer->getBody();
 
-            foreach ($offers as $offer) {
-                $offerLd = $offer->getBody();
+            $addToArray = isset($offerLd->{$addTo}) ? (array) $offerLd->{$addTo} : [];
 
-                $addToArray = isset($offerLd->{$addTo}) ? (array) $offerLd->{$addTo} : [];
+            $addToArray[] = $labelName->toNative();
+            $offerLd->{$addTo} = array_unique($addToArray);
 
-                $addToArray[] = $labelName;
-                $offerLd->{$addTo} = array_unique($addToArray);
+            if (isset($offerLd->{$removeFrom})) {
+                $offerLd->{$removeFrom} = array_diff((array) $offerLd->{$removeFrom}, [$labelName]);
 
-                if (isset($offerLd->{$removeFrom})) {
-                    $offerLd->{$removeFrom} = array_diff((array) $offerLd->{$removeFrom}, [$labelName]);
-
-                    if (count($offerLd->{$removeFrom}) === 0) {
-                        unset($offerLd->{$removeFrom});
-                    }
+                if (count($offerLd->{$removeFrom}) === 0) {
+                    unset($offerLd->{$removeFrom});
                 }
-
-                $this->offerRepository->save($offer->withBody($offerLd));
             }
-        } else {
-            $this->logger->alert('Could not apply visibility for label: ' .
-                $uuid . ' because label name not found in meta data!');
+
+            $this->offerRepository->save($offer->withBody($offerLd));
         }
     }
 
@@ -142,17 +131,5 @@ class OfferLabelProjector implements EventListenerInterface, LoggerAwareInterfac
                 );
             }
         }
-    }
-
-    /**
-     * @param Metadata $metadata
-     * @return string|null
-     */
-    private function getLabelName(Metadata $metadata)
-    {
-        $metadataAsArray = $metadata->serialize();
-
-        return isset($metadataAsArray[LabelDomainMessageEnricher::LABEL_NAME]) ?
-            $metadataAsArray[LabelDomainMessageEnricher::LABEL_NAME] : null;
     }
 }

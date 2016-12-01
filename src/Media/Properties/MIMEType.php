6ab1cee37cb8d3fcde9e9e89509208d73536c8de
Bram Cordie <bramcordie@gmail.com>
2016-12-01 08:21:18 +0100

    III-1642: Create MIMEType from subtype

--- src/Media/Properties/MIMEType.php
+++ src/Media/Properties/MIMEType.php
@@ -2,9 +2,28 @@
 
 namespace CultuurNet\UDB3\Media\Properties;
 
+use InvalidArgumentException;
 use ValueObjects\String\String;
 
 class MIMEType extends String
 {
+    protected static $supportedSubtypes = [
+        'jpeg' => 'image'
+    ];
 
+    /**
+     * @param string $subtypeString
+     *
+     * @return MIMEType
+     */
+    public static function fromSubtype($subtypeString)
+    {
+        $type = self::$supportedSubtypes[$subtypeString];
+
+        if (!$type) {
+            throw new InvalidArgumentException($subtypeString . ' is not supported!');
+        }
+
+        return new static($type . '/' . $subtypeString);
+    }
 }

9609921c4b7b4b035a544897bed4caec505e0cf4
Luc Wollants <luc.wollants@apptite.be>
2016-12-02 14:23:59 +0100

    III-165 Trailing space filter has now also a unit test for tabs.

--- test/StringFilter/StripTrailingSpaceStringFilterTest.php
+++ test/StringFilter/StripTrailingSpaceStringFilterTest.php
@@ -25,6 +25,16 @@ class StripTrailingSpaceStringFilterTest extends StringFilterTest
     /**
      * @test
      */
+    public function it_strips_trailing_tabs()
+    {
+        $original = "    \tHello!\t   \n Goodbye!  \n\n\nHello again!";
+        $expected = "    \tHello!\n Goodbye!\n\n\nHello again!";
+        $this->assertFilterValue($expected, $original);
+    }
+
+    /**
+     * @test
+     */
     public function it_only_filters_strings()
     {
         $this->setExpectedException(\InvalidArgumentException::class);

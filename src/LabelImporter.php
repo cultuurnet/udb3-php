6123581932067caa7b2c4f48287f83293e2737f7
Luc Wollants <luc.wollants@apptite.be>
2016-12-05 18:17:26 +0100

    III-1667 The label importer which gets all labels from an cdb item now also uses LabelCollection::fromKeywords.

--- src/LabelImporter.php
+++ src/LabelImporter.php
@@ -2,8 +2,6 @@
 
 namespace CultuurNet\UDB3;
 
-use CultureFeed_Cdb_Data_Keyword;
-
 class LabelImporter
 {
     /**
@@ -12,32 +10,22 @@ class LabelImporter
      */
     public function importLabels(\CultureFeed_Cdb_Item_Base $item, $jsonLD)
     {
-        /** @var CultureFeed_Cdb_Data_Keyword[] $keywords */
-        $keywords = array_values($item->getKeywords(true));
-
-        $validKeywords = array_filter(
-            $keywords,
-            function (CultureFeed_Cdb_Data_Keyword $keyword) {
-                return strlen(trim($keyword->getValue())) > 0;
-            }
+        $labelCollection = LabelCollection::fromKeywords(
+            $item->getKeywords(true)
         );
 
-        $visibleKeywords = array_filter(
-            $validKeywords,
-            function (CultureFeed_Cdb_Data_Keyword $keyword) {
-                return $keyword->isVisible();
+        $visibleLabelCollection = new LabelCollection();
+        $hiddenLabelCollection = new LabelCollection();
+        foreach ($labelCollection->asArray() as $label) {
+            if ($label->isVisible()) {
+                $visibleLabelCollection = $visibleLabelCollection->with($label);
+            } else {
+                $hiddenLabelCollection = $hiddenLabelCollection->with($label);
             }
-        );
+        }
 
-        $hiddenKeywords = array_filter(
-            $validKeywords,
-            function (CultureFeed_Cdb_Data_Keyword $keyword) {
-                return !$keyword->isVisible();
-            }
-        );
-
-        $visibleLabels = LabelCollection::fromKeywords($visibleKeywords)->toStrings();
-        $hiddenLabels = LabelCollection::fromKeywords($hiddenKeywords)->toStrings();
+        $visibleLabels = $visibleLabelCollection->toStrings();
+        $hiddenLabels = $hiddenLabelCollection->toStrings();
 
         empty($visibleLabels) ?: $jsonLD->labels = $visibleLabels;
         empty($hiddenLabels) ?: $jsonLD->hiddenLabels = $hiddenLabels;

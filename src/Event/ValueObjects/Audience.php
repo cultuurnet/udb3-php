8278e9cda88b651b44e5f9882dbd667552c71379
Luc Wollants <luc.wollants@apptite.be>
2017-01-02 15:00:51 +0100

    III-1406 Added Audience value object which for now only is a wrapper around the AudienceType.

---
+++ src/Event/ValueObjects/Audience.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace CultuurNet\UDB3\Event\ValueObjects;
+
+use Broadway\Serializer\SerializableInterface;
+
+class Audience implements SerializableInterface
+{
+    /**
+     * @var AudienceType
+     */
+    private $audienceType;
+
+    /**
+     * Audience constructor.
+     * @param AudienceType $audienceType
+     */
+    public function __construct(AudienceType $audienceType)
+    {
+        $this->audienceType = $audienceType;
+    }
+
+    /**
+     * @return AudienceType
+     */
+    public function getAudienceType()
+    {
+        return $this->audienceType;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function deserialize(array $data)
+    {
+        return new static(
+            AudienceType::fromNative($data['audience_type'])
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function serialize()
+    {
+        return [
+            'audience_type' => $this->audienceType->toNative()
+        ];
+    }
+
+    /**
+     * @param Audience $otherAudience
+     * @return bool
+     */
+    public function equals(Audience $otherAudience)
+    {
+        return $this->audienceType === $otherAudience->getAudienceType();
+    }
+}

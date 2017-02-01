f9ea41e68b3440a8f1a0cff8c619aa1027ae7b41
Bert Ramakers <bertramakers@me.com>
2017-02-01 10:35:04 +0100

    III-1777: Make GeoCoordinatesUpdated serializable.

--- src/Place/Events/GeoCoordinatesUpdated.php
+++ src/Place/Events/GeoCoordinatesUpdated.php
@@ -3,6 +3,8 @@
 namespace CultuurNet\UDB3\Place\Events;
 
 use CultuurNet\Geocoding\Coordinate\Coordinates;
+use CultuurNet\Geocoding\Coordinate\Latitude;
+use CultuurNet\Geocoding\Coordinate\Longitude;
 use CultuurNet\UDB3\Offer\Events\AbstractEvent;
 
 class GeoCoordinatesUpdated extends AbstractEvent
@@ -28,5 +30,33 @@ class GeoCoordinatesUpdated extends AbstractEvent
     public function getCoordinates()
     {
         return $this->coordinates;
+    }
+
+    /**
+     * @return array
+     */
+    public function serialize()
+    {
+        return parent::serialize() + [
+            'coordinates' => [
+                'lat' => $this->coordinates->getLatitude()->toDouble(),
+                'long' => $this->coordinates->getLongitude()->toDouble(),
+            ],
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @return GeoCoordinatesUpdated
+     */
+    public static function deserialize(array $data)
+    {
+        return new GeoCoordinatesUpdated(
+            $data['item_id'],
+            new Coordinates(
+                new Latitude($data['coordinates']['lat']),
+                new Longitude($data['coordinates']['long'])
+            )
+        );
     }
 }
---
+++ test/Place/Events/GeoCoordinatesUpdatedTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace CultuurNet\UDB3\Place\Events;
+
+
+use CultuurNet\Geocoding\Coordinate\Coordinates;
+use CultuurNet\Geocoding\Coordinate\Latitude;
+use CultuurNet\Geocoding\Coordinate\Longitude;
+
+class GeoCoordinatesUpdatedTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_be_serialized_and_deserialized()
+    {
+        $event = new GeoCoordinatesUpdated(
+            'f281bc85-3ee4-43a7-b42d-a8982ec9bbc4',
+            new Coordinates(
+                new Latitude(0.00456),
+                new Longitude(-1.24567)
+            )
+        );
+
+        $expectedArray = [
+            'item_id' => 'f281bc85-3ee4-43a7-b42d-a8982ec9bbc4',
+            'coordinates' => [
+                'lat' => 0.00456,
+                'long' => -1.24567,
+            ],
+        ];
+
+        $actualArray = $event->serialize();
+
+        $deserialized = GeoCoordinatesUpdated::deserialize($actualArray);
+
+        $this->assertEquals($expectedArray, $actualArray);
+        $this->assertEquals($event, $deserialized);
+    }
+}

f03670009ca5a525c18077b5bbfe727486f7bb2a
Bram Cordie <bramcordie@gmail.com>
2017-05-04 11:57:36 +0200

    III-2086: Timestamp conversion

--- src/Calendar/CalendarConverter.php
+++ src/Calendar/CalendarConverter.php
@@ -13,6 +13,8 @@ use CultureFeed_Cdb_Data_Calendar_Weekscheme;
 use CultuurNet\UDB3\CalendarInterface;
 use CultuurNet\UDB3\CalendarType;
 use DateTimeInterface;
+use InvalidArgumentException;
+use League\Period\Period;
 
 class CalendarConverter implements CalendarConverterInterface
 {
@@ -27,19 +29,22 @@ class CalendarConverter implements CalendarConverterInterface
         switch ($calendarType) {
             case CalendarType::MULTIPLE:
                 $cdbCalendar = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+                $index = 1;
                 foreach ($calendar->getTimestamps() as $timestamp) {
-                    $this->timestampCalendar(
+                    ($this->countTimestamps($cdbCalendar) - $this->countTimestamps($this->timestampCalendar(
                         $timestamp->getStartDate(),
                         $timestamp->getEndDate(),
-                        $cdbCalendar
-                    );
+                        $cdbCalendar,
+                        $index
+                    ))) === -1 ?: $index++ ;
                 }
                 break;
             case CalendarType::SINGLE:
                 $cdbCalendar = $this->timestampCalendar(
                     $calendar->getStartDate(),
                     $calendar->getEndDate(),
-                    new CultureFeed_Cdb_Data_Calendar_TimestampList()
+                    new CultureFeed_Cdb_Data_Calendar_TimestampList(),
+                    1
                 );
                 break;
             case CalendarType::PERIODIC:
@@ -64,6 +69,18 @@ class CalendarConverter implements CalendarConverterInterface
         }
 
         return $cdbCalendar;
+    }
+
+    /**
+     * @param CultureFeed_Cdb_Data_Calendar_TimestampList $timestamps
+     * @return int
+     */
+    private function countTimestamps(CultureFeed_Cdb_Data_Calendar_TimestampList $timestamps)
+    {
+        $numberOfTimestamps =  iterator_count($timestamps);
+        $timestamps->rewind();
+
+        return $numberOfTimestamps;
     }
 
     /**
@@ -133,30 +150,86 @@ class CalendarConverter implements CalendarConverterInterface
      * @param DateTimeInterface $startDate
      * @param DateTimeInterface $endDate
      * @param CultureFeed_Cdb_Data_Calendar_TimestampList $calendar
+     * @param Integer|null $index
      *
      * @return CultureFeed_Cdb_Data_Calendar_TimestampList
      */
     private function timestampCalendar(
         DateTimeInterface $startDate,
         DateTimeInterface $endDate,
-        CultureFeed_Cdb_Data_Calendar_TimestampList $calendar
+        CultureFeed_Cdb_Data_Calendar_TimestampList $calendar,
+        $index = null
     ) {
-        $startHour = $startDate->format('H:i:s');
-        if ($startHour == '00:00:00') {
-            $startHour = null;
-        }
-        $endHour = $endDate->format('H:i:s');
-        if ($endHour == '00:00:00') {
-            $endHour = null;
-        }
-        $calendar->add(
-            new CultureFeed_Cdb_Data_Calendar_Timestamp(
+        $startDay = Period::createFromDay($startDate);
+        $untilEndOfNextDay = $startDay
+            ->withDuration('2 DAYS')
+            ->moveEndDate('-1 SECOND');
+
+        if ($untilEndOfNextDay->contains($endDate)) {
+            $calendar->add(
+                new CultureFeed_Cdb_Data_Calendar_Timestamp(
+                    $startDate->format('Y-m-d'),
+                    $this->formatDateTimeAsCdbTime($startDate),
+                    $this->formatDateTimeAsCdbTime($endDate)
+                )
+            );
+        } else if (is_int($index)) {
+            $period = new Period($startDate, $endDate);
+
+            $startTimestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp(
                 $startDate->format('Y-m-d'),
-                $startHour,
-                $endHour
-            )
-        );
+                $this->formatDateTimeAsCdbTime($startDate, $index)
+            );
+
+            $endTimestamp = new CultureFeed_Cdb_Data_Calendar_Timestamp(
+                $endDate->format('Y-m-d'),
+                '00:00:' . str_pad((string) $index, 2, '0', STR_PAD_LEFT),
+                $this->formatDateTimeAsCdbTime($endDate)
+            );
+
+            $days = iterator_to_array($period->getDatePeriod('1 DAY'));
+            $fillerTimestamps = array_map(
+                function (DateTimeInterface $dateTime) use ($index) {
+                    return new CultureFeed_Cdb_Data_Calendar_Timestamp(
+                        $dateTime->format('Y-m-d'),
+                        '00:00:' . str_pad((string) $index, 2, '0', STR_PAD_LEFT)
+                    );
+                },
+                array_slice($days, 1, count($days) === 2 ? 2 : -1)
+            );
+
+            $calendar = array_reduce(
+                array_merge([$startTimestamp], $fillerTimestamps, [$endTimestamp]),
+                function (CultureFeed_Cdb_Data_Calendar_TimestampList $calendar, $timestamp) {
+                    $calendar->add($timestamp);
+                    return $calendar;
+                },
+                $calendar
+            );
+        }
 
         return $calendar;
+    }
+
+    /**
+     * @param DateTimeInterface $timestamp
+     * @param integer|null $index
+     * @return null|string
+     */
+    private function formatDateTimeAsCdbTime(DateTimeInterface $timestamp, $index = null)
+    {
+        if (is_int($index) && $index > 59) {
+            throw new InvalidArgumentException('The CDB time index should not be higher than 59!');
+        }
+
+        $time = is_int($index)
+            ? $timestamp->format('H:i') . ':' . str_pad((string) $index, 2, '0', STR_PAD_LEFT)
+            : $timestamp->format('H:i:s');
+
+        if ($time == '00:00:00') {
+            $time = null;
+        }
+
+        return $time;
     }
 }

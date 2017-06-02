fd2300101a215d923c0b29d1ec6a136b6cffbaa5
Luc Wollants <luc.wollants@apptite.be>
2017-06-02 13:11:06 +0200

    III-2163 Add missing test annotation.

--- test/Organizer/Commands/UpdateTitleTest.php
+++ test/Organizer/Commands/UpdateTitleTest.php
@@ -64,6 +64,9 @@ class UpdateTitleTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * @test
+     */
     public function it_stores_a_language()
     {
         $this->assertEquals(

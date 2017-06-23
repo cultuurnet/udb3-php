0f5986a12af3ed093ce03751a8e6066864679ec6
Bert Ramakers <bertramakers@me.com>
2017-06-23 13:47:11 +0200

    III-2167: Add interface and class to enrich JsonDocuments with language information

---
+++ src/ReadModel/JsonDocumentLanguageEnricher.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CultuurNet\UDB3\ReadModel;
+
+use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Language;
+
+class JsonDocumentLanguageEnricher implements JsonDocumentMetaDataEnricherInterface
+{
+    /**
+     * @var JsonDocumentLanguageAnalyzerInterface
+     */
+    private $languageAnalyzer;
+
+    /**
+     * @param JsonDocumentLanguageAnalyzerInterface $languageAnalyzer
+     */
+    public function __construct(
+        JsonDocumentLanguageAnalyzerInterface $languageAnalyzer
+    ) {
+        $this->languageAnalyzer = $languageAnalyzer;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function enrich(JsonDocument $jsonDocument, Metadata $metadata)
+    {
+        $body = $jsonDocument->getBody();
+
+        $castLanguageToString = function (Language $language) {
+            return $language->getCode();
+        };
+
+        $body->languages = array_map(
+            $castLanguageToString,
+            $this->languageAnalyzer->getAllLanguages($jsonDocument)
+        );
+
+        $body->completedLanguages = array_map(
+            $castLanguageToString,
+            $this->languageAnalyzer->getCompletedLanguages($jsonDocument)
+        );
+
+        return $jsonDocument->withBody($body);
+    }
+
+}

e8d8c55c3e8407ec48745cf5f965bdaea46d1080
Bram Cordie <bramcordie@gmail.com>
2017-08-30 16:55:20 +0200

    III-2285: Tighten delete-organizer command permissions

--- src/Organizer/Commands/DeleteOrganizer.php
+++ src/Organizer/Commands/DeleteOrganizer.php
@@ -2,6 +2,15 @@
 
 namespace CultuurNet\UDB3\Organizer\Commands;
 
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
+
 class DeleteOrganizer extends AbstractOrganizerCommand
 {
+    /**
+     * @inheritdoc
+     */
+    public function getPermission()
+    {
+        return Permission::ORGANISATIES_BEHEREN();
+    }
 }

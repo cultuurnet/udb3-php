fb405c0c57d5b1cfe4cc32db5c8950432839cb59
Luc Wollants <luc.wollants@apptite.be>
2017-09-07 10:26:30 +0200

    III-2308 Add AuthorizableCommandInterface to label commands.

--- src/Label/Commands/AbstractCommand.php
+++ src/Label/Commands/AbstractCommand.php
@@ -2,9 +2,11 @@
 
 namespace CultuurNet\UDB3\Label\Commands;
 
+use CultuurNet\UDB3\Offer\Commands\AuthorizableCommandInterface;
+use CultuurNet\UDB3\Role\ValueObjects\Permission;
 use ValueObjects\Identity\UUID;
 
-abstract class AbstractCommand
+abstract class AbstractCommand implements AuthorizableCommandInterface
 {
     /**
      * @var UUID
@@ -26,5 +28,21 @@ abstract class AbstractCommand
     public function getUuid()
     {
         return $this->uuid;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getItemId()
+    {
+        return $this->uuid->toNative();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getPermission()
+    {
+        return Permission::AANBOD_LABELEN();
     }
 }

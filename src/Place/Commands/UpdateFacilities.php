6059fde60689a2e1ba8af15f6d73b5a0a76875d0
Luc Wollants <luc.wollants@apptite.be>
2017-11-29 14:52:04 +0100

    III-2413 Create an abstract facility command.

--- src/Place/Commands/UpdateFacilities.php
+++ src/Place/Commands/UpdateFacilities.php
@@ -2,40 +2,9 @@
 
 namespace CultuurNet\UDB3\Place\Commands;
 
-use CultuurNet\UDB3\Offer\Commands\AbstractCommand;
-use CultuurNet\UDB3\Role\ValueObjects\Permission;
+use CultuurNet\UDB3\Offer\Commands\AbstractUpdateFacilities;
 
-class UpdateFacilities extends AbstractCommand
+class UpdateFacilities extends AbstractUpdateFacilities
 {
-    /**
-     * Facilities to be added.
-     * @var array
-     */
-    protected $facilities;
 
-    /**
-     * @param string $itemId
-     * @param array $facilities
-     */
-    public function __construct($itemId, array $facilities)
-    {
-        parent::__construct($itemId);
-        $this->facilities = $facilities;
-    }
-
-    /**
-     * @return array
-     */
-    public function getFacilities()
-    {
-        return $this->facilities;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getPermission()
-    {
-        return Permission::VOORZIENINGEN_BEWERKEN();
-    }
 }

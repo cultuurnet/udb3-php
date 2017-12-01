e419913ec7e91627242b0381a6e04e4a1265e52d
Luc Wollants <luc.wollants@apptite.be>
2017-12-01 14:59:45 +0100

    III-2413 Forgot import for FacilitiesUpdated in EventLDProjector.

--- src/Event/ReadModel/JSONLD/EventLDProjector.php
+++ src/Event/ReadModel/JSONLD/EventLDProjector.php
@@ -58,6 +58,7 @@ use CultuurNet\UDB3\Offer\ReadModel\JSONLD\OfferUpdate;
 use CultuurNet\UDB3\Offer\WorkflowStatus;
 use CultuurNet\UDB3\Organizer\OrganizerProjectedToJSONLD;
 use CultuurNet\UDB3\OrganizerService;
+use CultuurNet\UDB3\Place\Events\FacilitiesUpdated;
 use CultuurNet\UDB3\Place\Events\PlaceProjectedToJSONLD;
 use CultuurNet\UDB3\PlaceService;
 use CultuurNet\UDB3\ReadModel\JsonDocument;

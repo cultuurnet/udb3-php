d513bcd72a9a7f94178a5841843e3d304592ae25
Luc Wollants <luc.wollants@apptite.be>
2018-01-25 08:19:44 +0100

    III-2468 Create new common class AbstractUpdateGeoCoordinatesFromAddress to be used by place and event.

--- src/Place/Commands/UpdateGeoCoordinatesFromAddress.php
+++ src/Place/Commands/UpdateGeoCoordinatesFromAddress.php
@@ -2,31 +2,9 @@
 
 namespace CultuurNet\UDB3\Place\Commands;
 
-use CultuurNet\UDB3\Address\Address;
-use CultuurNet\UDB3\Offer\Commands\AbstractCommand;
+use CultuurNet\UDB3\Offer\Commands\AbstractUpdateGeoCoordinatesFromAddress;
 
-class UpdateGeoCoordinatesFromAddress extends AbstractCommand
+class UpdateGeoCoordinatesFromAddress extends AbstractUpdateGeoCoordinatesFromAddress
 {
-    /**
-     * @var Address
-     */
-    private $address;
 
-    /**
-     * @param string $itemId
-     * @param Address $address
-     */
-    public function __construct($itemId, Address $address)
-    {
-        parent::__construct($itemId);
-        $this->address = $address;
-    }
-
-    /**
-     * @return Address
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
 }

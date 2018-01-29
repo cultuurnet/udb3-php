d5697cfa08e2841a5ea5d31e2bde1ce6aec39bc2
Luc Wollants <luc.wollants@apptite.be>
2018-01-29 09:08:26 +0100

    III-2468 Create AbstractGeoCoordinatesCommandHandler to be used for Events and Places.

--- src/Event/GeoCoordinatesCommandHandler.php
+++ src/Event/GeoCoordinatesCommandHandler.php
@@ -2,74 +2,16 @@
 
 namespace CultuurNet\UDB3\Event;
 
-use Broadway\Repository\RepositoryInterface;
-use CultuurNet\Geocoding\GeocodingServiceInterface;
-use CultuurNet\UDB3\Address\AddressFormatterInterface;
-use CultuurNet\UDB3\CommandHandling\Udb3CommandHandler;
 use CultuurNet\UDB3\Event\Commands\UpdateGeoCoordinatesFromAddress;
+use CultuurNet\UDB3\Offer\AbstractGeoCoordinatesCommandHandler;
 
-class GeoCoordinatesCommandHandler extends Udb3CommandHandler
+class GeoCoordinatesCommandHandler extends AbstractGeoCoordinatesCommandHandler
 {
-    /**
-     * @var RepositoryInterface
-     */
-    private $eventRepository;
-
-    /**
-     * @var AddressFormatterInterface
-     */
-    private $defaultAddressFormatter;
-
-    /**
-     * @var AddressFormatterInterface
-     */
-    private $fallbackAddressFormatter;
-
-    /**
-     * @var GeocodingServiceInterface
-     */
-    private $geocodingService;
-
-    /**
-     * @param RepositoryInterface $eventRepository
-     * @param AddressFormatterInterface $defaultAddressFormatter
-     * @param AddressFormatterInterface $fallbackAddressFormatter
-     * @param GeocodingServiceInterface $geocodingService
-     */
-    public function __construct(
-        RepositoryInterface $eventRepository,
-        AddressFormatterInterface $defaultAddressFormatter,
-        AddressFormatterInterface $fallbackAddressFormatter,
-        GeocodingServiceInterface $geocodingService
-    ) {
-        $this->eventRepository = $eventRepository;
-        $this->defaultAddressFormatter = $defaultAddressFormatter;
-        $this->fallbackAddressFormatter = $fallbackAddressFormatter;
-        $this->geocodingService = $geocodingService;
-    }
-
     /**
      * @param UpdateGeoCoordinatesFromAddress $updateGeoCoordinates
      */
     public function handleUpdateGeoCoordinatesFromAddress(UpdateGeoCoordinatesFromAddress $updateGeoCoordinates)
     {
-        $coordinates = $this->geocodingService->getCoordinates(
-            $this->defaultAddressFormatter->format(
-                $updateGeoCoordinates->getAddress()
-            )
-        );
-
-        if ($coordinates === null) {
-            $coordinates = $this->geocodingService->getCoordinates(
-                $this->fallbackAddressFormatter->format(
-                    $updateGeoCoordinates->getAddress()
-                )
-            );
-        }
-
-        /** @var Event $event */
-        $event = $this->eventRepository->load($updateGeoCoordinates->getItemId());
-        $event->updateGeoCoordinates($coordinates);
-        $this->eventRepository->save($event);
+        $this->updateGeoCoordinatesFromAddress($updateGeoCoordinates);
     }
 }
--- src/Place/GeoCoordinatesCommandHandler.php
+++ src/Place/GeoCoordinatesCommandHandler.php
@@ -2,74 +2,16 @@
 
 namespace CultuurNet\UDB3\Place;
 
-use Broadway\Repository\RepositoryInterface;
-use CultuurNet\Geocoding\GeocodingServiceInterface;
-use CultuurNet\UDB3\Address\AddressFormatterInterface;
-use CultuurNet\UDB3\CommandHandling\Udb3CommandHandler;
+use CultuurNet\UDB3\Offer\AbstractGeoCoordinatesCommandHandler;
 use CultuurNet\UDB3\Place\Commands\UpdateGeoCoordinatesFromAddress;
 
-class GeoCoordinatesCommandHandler extends Udb3CommandHandler
+class GeoCoordinatesCommandHandler extends AbstractGeoCoordinatesCommandHandler
 {
-    /**
-     * @var RepositoryInterface
-     */
-    private $placeRepository;
-
-    /**
-     * @var AddressFormatterInterface
-     */
-    private $defaultAddressFormatter;
-
-    /**
-     * @var AddressFormatterInterface
-     */
-    private $fallbackAddressFormatter;
-
-    /**
-     * @var GeocodingServiceInterface
-     */
-    private $geocodingService;
-
-    /**
-     * @param RepositoryInterface $placeRepository
-     * @param AddressFormatterInterface $defaultAddressFormatter
-     * @param AddressFormatterInterface $fallbackAddressFormatter
-     * @param GeocodingServiceInterface $geocodingService
-     */
-    public function __construct(
-        RepositoryInterface $placeRepository,
-        AddressFormatterInterface $defaultAddressFormatter,
-        AddressFormatterInterface $fallbackAddressFormatter,
-        GeocodingServiceInterface $geocodingService
-    ) {
-        $this->placeRepository = $placeRepository;
-        $this->defaultAddressFormatter = $defaultAddressFormatter;
-        $this->fallbackAddressFormatter = $fallbackAddressFormatter;
-        $this->geocodingService = $geocodingService;
-    }
-
     /**
      * @param UpdateGeoCoordinatesFromAddress $updateGeoCoordinates
      */
     public function handleUpdateGeoCoordinatesFromAddress(UpdateGeoCoordinatesFromAddress $updateGeoCoordinates)
     {
-        $coordinates = $this->geocodingService->getCoordinates(
-            $this->defaultAddressFormatter->format(
-                $updateGeoCoordinates->getAddress()
-            )
-        );
-
-        if ($coordinates === null) {
-            $coordinates = $this->geocodingService->getCoordinates(
-                $this->fallbackAddressFormatter->format(
-                    $updateGeoCoordinates->getAddress()
-                )
-            );
-        }
-
-        /** @var Place $place */
-        $place = $this->placeRepository->load($updateGeoCoordinates->getItemId());
-        $place->updateGeoCoordinates($coordinates);
-        $this->placeRepository->save($place);
+        $this->updateGeoCoordinatesFromAddress($updateGeoCoordinates);
     }
 }

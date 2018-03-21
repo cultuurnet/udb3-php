561f540b00ee5f08f9a5f7231a06900cf8eee29a
Bert Ramakers <bertramakers@me.com>
2018-03-21 15:46:28 +0100

    III-2537: Add factory method to create description from udb3-model

--- src/Description.php
+++ src/Description.php
@@ -2,8 +2,17 @@
 
 namespace CultuurNet\UDB3;
 
+use CultuurNet\UDB3\Model\ValueObject\Text\Description as Udb3ModelDescription;
 use ValueObjects\StringLiteral\StringLiteral;
 
 class Description extends StringLiteral
 {
+    /**
+     * @param Udb3ModelDescription $udb3ModelDescription
+     * @return Description
+     */
+    public static function fromUdb3ModelDescription(Udb3ModelDescription $udb3ModelDescription)
+    {
+        return new Description($udb3ModelDescription->toString());
+    }
 }

5939f8f0b011a652d65c9b773cb2ffe4a716a5e4
Kristof Coomans <kristof@2dotstwice.be>
2019-05-17 11:22:50 +0200

    III-2885: Fix error when base price info is below zero

--- src/Offer/ReadModel/JSONLD/CdbXMLItemBaseImporter.php
+++ src/Offer/ReadModel/JSONLD/CdbXMLItemBaseImporter.php
@@ -192,7 +192,7 @@ class CdbXMLItemBaseImporter
             $basePrice = floatval($basePrice);
         }
 
-        if (!$basePrice) {
+        if (!$basePrice || $basePrice < 0) {
             return;
         }
 

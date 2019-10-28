c475f9668902ec17b759a70e5ec748eedbbd3c57
zvonimir <zspajich@gmail.com>
2019-10-28 11:34:46 +0100

    Fix inheritence structure on (Organizer) GeoCordinatesUpdated event

--- src/Organizer/Events/GeoCoordinatesUpdated.php
+++ src/Organizer/Events/GeoCoordinatesUpdated.php
@@ -2,9 +2,56 @@
 
 namespace CultuurNet\UDB3\Organizer\Events;
 
-use CultuurNet\UDB3\Offer\Events\AbstractGeoCoordinatesUpdated;
+use CultuurNet\Geocoding\Coordinate\Coordinates;
+use CultuurNet\Geocoding\Coordinate\Latitude;
+use CultuurNet\Geocoding\Coordinate\Longitude;
 
-class GeoCoordinatesUpdated extends AbstractGeoCoordinatesUpdated
+class GeoCoordinatesUpdated extends OrganizerEvent
 {
+    /**
+     * @var Coordinates
+     */
+    private $coordinates;
 
+    public function __construct(string $organizerId, Coordinates $coordinates)
+    {
+        parent::__construct($organizerId);
+        $this->coordinates = $coordinates;
+    }
+
+    /**
+     * @return Coordinates
+     */
+    public function coordinates(): Coordinates
+    {
+        return $this->coordinates;
+    }
+
+    /**
+     * @return array
+     */
+    public function serialize()
+    {
+        return parent::serialize() + [
+                'coordinates' => [
+                    'lat' => $this->coordinates->getLatitude()->toDouble(),
+                    'long' => $this->coordinates->getLongitude()->toDouble(),
+                ],
+            ];
+    }
+
+    /**
+     * @param array $data
+     * @return static
+     */
+    public static function deserialize(array $data)
+    {
+        return new static(
+            $data['item_id'],
+            new Coordinates(
+                new Latitude($data['coordinates']['lat']),
+                new Longitude($data['coordinates']['long'])
+            )
+        );
+    }
 }

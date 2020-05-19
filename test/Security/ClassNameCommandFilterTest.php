22753417613604cd13b9827d7c6ebe13e9737fba
Jonas Drieghe <jonasd@madewithlove.be>
2020-05-19 20:28:09 +0200

    Decouple ClassNameCommandFilterTest from real command

--- test/Security/ClassNameCommandFilterTest.php
+++ test/Security/ClassNameCommandFilterTest.php
@@ -2,7 +2,6 @@
 
 namespace CultuurNet\UDB3\Security;
 
-use CultuurNet\UDB3\Place\Commands\UpdateFacilities;
 use CultuurNet\UDB3\Role\Commands\AddLabel;
 use PHPUnit\Framework\TestCase;
 use ValueObjects\StringLiteral\StringLiteral;
@@ -10,32 +9,16 @@ use ValueObjects\StringLiteral\StringLiteral;
 class ClassNameCommandFilterTest extends TestCase
 {
     /**
-     * @var UpdateFacilities
-     */
-    private $updateFacilities;
-
-    protected function setUp()
-    {
-        $this->updateFacilities = new UpdateFacilities(
-            '26e36905-64d0-4cac-ba41-6d6dcd997ca0',
-            [
-                'facility1',
-                'facility2',
-            ]
-        );
-    }
-
-    /**
      * @test
      */
     public function it_returns_true_when_class_name_matches()
     {
         $classNameCommandFilter = new ClassNameCommandFilter(
-            new StringLiteral(UpdateFacilities::class),
+            new StringLiteral(DummyCommand::class),
             new StringLiteral(AddLabel::class)
         );
 
-        $this->assertTrue($classNameCommandFilter->matches($this->updateFacilities));
+        $this->assertTrue($classNameCommandFilter->matches(new DummyCommand()));
     }
 
     /**
@@ -47,6 +30,6 @@ class ClassNameCommandFilterTest extends TestCase
             new StringLiteral(AddLabel::class)
         );
 
-        $this->assertFalse($classNameCommandFilter->matches($this->updateFacilities));
+        $this->assertFalse($classNameCommandFilter->matches(new DummyCommand()));
     }
 }

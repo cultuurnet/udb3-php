78430510cd2e12cd715a72a6cd0baef796c6ada9
Jonas Drieghe <jonasd@madewithlove.be>
2020-05-19 20:53:11 +0200

    Remove all Place related Commands

--- src/Place/Commands/DeletePlace.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CultuurNet\UDB3\Place\Commands;
-
-use CultuurNet\UDB3\Offer\Commands\AbstractDeleteOffer;
-
-class DeletePlace extends AbstractDeleteOffer
-{
-}
--- src/Place/Commands/Moderation/Reject.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CultuurNet\UDB3\Place\Commands\Moderation;
-
-use CultuurNet\UDB3\Offer\Commands\Moderation\AbstractReject;
-
-class Reject extends AbstractReject
-{
-}

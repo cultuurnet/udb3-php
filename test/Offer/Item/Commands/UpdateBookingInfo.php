4f46c70bdaa566eee7c837674a55369d5edbdd4c
Jonas Drieghe <jonasd@madewithlove.be>
2020-05-20 13:44:55 +0200

    Remove Offer write model code

--- test/Offer/Item/Commands/UpdateBookingInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CultuurNet\UDB3\Offer\Item\Commands;
-
-use CultuurNet\UDB3\Offer\Commands\AbstractUpdateBookingInfo;
-
-class UpdateBookingInfo extends AbstractUpdateBookingInfo
-{
-}

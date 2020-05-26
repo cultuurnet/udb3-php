ce5b3b40f76aa7bfeff809dffeb4d34d630cd655
Jonas Drieghe <jonasd@madewithlove.be>
2020-05-26 14:39:00 +0200

    Move commands to udb3-silex

--- src/Role/Commands/AddLabel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace CultuurNet\UDB3\Role\Commands;
-
-class AddLabel extends AbstractLabelCommand
-{
-}
--- src/Role/Commands/AddUser.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace CultuurNet\UDB3\Role\Commands;
-
-class AddUser extends AbstractUserCommand
-{
-}

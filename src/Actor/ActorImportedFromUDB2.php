0173587f9c6f58fac32130d34e133eb59306f92a
Bert Ramakers <bertramakers@me.com>
2020-09-14 11:13:33 +0200

    Fix double constructor due to incorrect staging of changes

--- src/Actor/ActorImportedFromUDB2.php
+++ src/Actor/ActorImportedFromUDB2.php
@@ -24,7 +24,6 @@ class ActorImportedFromUDB2 extends ActorEvent
      * @param string $cdbXml
      * @param string $cdbXmlNamespaceUri
      */
-    public function __construct($actorId, $cdbXml, $cdbXmlNamespaceUri)
     final public function __construct(string $actorId, string $cdbXml, string $cdbXmlNamespaceUri)
     {
         parent::__construct($actorId);

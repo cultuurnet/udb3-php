7f50b830d8e47058499f2017dbc8339e15a78fbb
Bert Ramakers <bertramakers@me.com>
2020-09-14 14:20:25 +0200

    Fix tests broken by making some classes final

--- test/DomainMessage/CompositeDomainMessageEnricherTest.php
+++ test/DomainMessage/CompositeDomainMessageEnricherTest.php
@@ -2,18 +2,31 @@
 
 namespace CultuurNet\UDB3\DomainMessage;
 
+use Broadway\Domain\DateTime;
 use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use CultuurNet\UDB3\Address\Address;
+use CultuurNet\UDB3\Address\Locality;
+use CultuurNet\UDB3\Address\PostalCode;
+use CultuurNet\UDB3\Address\Street;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarType;
 use CultuurNet\UDB3\Event\Events\EventCreated;
+use CultuurNet\UDB3\Event\EventType;
+use CultuurNet\UDB3\Event\ValueObjects\LocationId;
+use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Organizer\Events\OrganizerCreated;
 use CultuurNet\UDB3\Organizer\Events\OrganizerCreatedWithUniqueWebsite;
 use CultuurNet\UDB3\Place\Events\PlaceCreated;
+use CultuurNet\UDB3\Title;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Rhumsaa\Uuid\Uuid;
+use ValueObjects\Geography\Country;
+use ValueObjects\Web\Url;
 
 class CompositeDomainMessageEnricherTest extends TestCase
 {
-    use DomainMessageTestDataTrait;
-
     /**
      * @var DomainMessageEnricherInterface|MockObject
      */
@@ -61,13 +74,10 @@ class CompositeDomainMessageEnricherTest extends TestCase
      */
     public function it_only_supports_domain_messages_supported_by_its_injected_enrichers()
     {
-        $eventCreatedDomainMessage = $this->createDomainMessage($this, EventCreated::class);
-        $placeCreatedDomainMessage = $this->createDomainMessage($this, PlaceCreated::class);
-        $organizerCreatedDomainMessage = $this->createDomainMessage($this, OrganizerCreated::class);
-        $organizerCreatedWithUniqueWebsiteDomainMessage = $this->createDomainMessage(
-            $this,
-            OrganizerCreatedWithUniqueWebsite::class
-        );
+        $eventCreatedDomainMessage = $this->createEventCreatedDomainMessage();
+        $placeCreatedDomainMessage = $this->createPlaceCreatedDomainMessage();
+        $organizerCreatedDomainMessage = $this->createOrganizerCreatedDomainMessage();
+        $organizerCreatedWithUniqueWebsiteDomainMessage = $this->createOrganizerCreatedWithUniqueWebsiteDomainMessage();
 
         $this->assertTrue($this->compositeEnricher->supports($eventCreatedDomainMessage));
         $this->assertTrue($this->compositeEnricher->supports($placeCreatedDomainMessage));
@@ -80,13 +90,10 @@ class CompositeDomainMessageEnricherTest extends TestCase
      */
     public function it_delegates_enrichment_of_supported_domain_messages()
     {
-        $eventCreatedDomainMessage = $this->createDomainMessage($this, EventCreated::class);
-        $placeCreatedDomainMessage = $this->createDomainMessage($this, PlaceCreated::class);
-        $organizerCreatedDomainMessage = $this->createDomainMessage($this, OrganizerCreated::class);
-        $organizerCreatedWithUniqueWebsiteDomainMessage = $this->createDomainMessage(
-            $this,
-            OrganizerCreatedWithUniqueWebsite::class
-        );
+        $eventCreatedDomainMessage = $this->createEventCreatedDomainMessage();
+        $placeCreatedDomainMessage = $this->createPlaceCreatedDomainMessage();
+        $organizerCreatedDomainMessage = $this->createOrganizerCreatedDomainMessage();
+        $organizerCreatedWithUniqueWebsiteDomainMessage = $this->createOrganizerCreatedWithUniqueWebsiteDomainMessage();
 
 
         $enrichedEventCreatedDomainMessage = clone $eventCreatedDomainMessage;
@@ -128,6 +135,81 @@ class CompositeDomainMessageEnricherTest extends TestCase
         $this->assertEquals(
             $organizerCreatedWithUniqueWebsiteDomainMessage,
             $this->compositeEnricher->enrich($organizerCreatedWithUniqueWebsiteDomainMessage)
+        );
+    }
+
+    private function createEventCreatedDomainMessage(): DomainMessage
+    {
+        return new DomainMessage(
+            Uuid::uuid4(),
+            0,
+            new Metadata(),
+            new EventCreated(
+                '97d50997-2f60-47f2-9861-05be747038fa',
+                new Language('nl'),
+                new Title('test title'),
+                new EventType('0.0.1', 'label'),
+                new LocationId('8bec7ce3-25d0-4677-926f-ac20df8898f1'),
+                new Calendar(CalendarType::PERMANENT())
+            ),
+            DateTime::now()
+        );
+    }
+
+    private function createPlaceCreatedDomainMessage(): DomainMessage
+    {
+        return new DomainMessage(
+            Uuid::uuid4(),
+            0,
+            new Metadata(),
+            new PlaceCreated(
+                'fd9e986d-6a23-470c-bf0c-4ad40aa4515e',
+                new Language('nl'),
+                new Title('test title'),
+                new EventType('0.0.1', 'label'),
+                new Address(
+                    new Street('street'),
+                    new PostalCode('3000'),
+                    new Locality('Leuven'),
+                    Country::fromNative('BE')
+                ),
+                new Calendar(CalendarType::PERMANENT())
+            ),
+            DateTime::now()
+        );
+    }
+
+    private function createOrganizerCreatedDomainMessage(): DomainMessage
+    {
+        return new DomainMessage(
+            Uuid::uuid4(),
+            0,
+            new Metadata(),
+            new OrganizerCreated(
+                'fd9e986d-6a23-470c-bf0c-4ad40aa4515e',
+                new Title('test title'),
+                [],
+                [],
+                [],
+                []
+            ),
+            DateTime::now()
+        );
+    }
+
+    private function createOrganizerCreatedWithUniqueWebsiteDomainMessage(): DomainMessage
+    {
+        return new DomainMessage(
+            Uuid::uuid4(),
+            0,
+            new Metadata(),
+            new OrganizerCreatedWithUniqueWebsite(
+                'fd9e986d-6a23-470c-bf0c-4ad40aa4515e',
+                new Language('nl'),
+                Url::fromNative('https://www.publiq.be'),
+                new Title('test title')
+            ),
+            DateTime::now()
         );
     }
 }

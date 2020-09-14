529fba6ab2ed13515423403aedde574f190b17be
Bert Ramakers <bertramakers@me.com>
2020-09-14 12:14:47 +0200

    Remove leftover deserialize() with unsafe usage of new static()

--- src/Organizer/Events/OrganizerEvent.php
+++ src/Organizer/Events/OrganizerEvent.php
@@ -27,12 +27,4 @@ abstract class OrganizerEvent implements SerializableInterface
           'organizer_id' => $this->organizerId,
         );
     }
-
-    /**
-     * @return mixed The object instance
-     */
-    public static function deserialize(array $data)
-    {
-        return new static($data['organizer_id']);
-    }
 }

2ebec59313e8accaa0c23ff133a759134d083406
Luc Wollants <lucwollants@gmail.com>
2020-11-23 13:47:40 +0100

    Add trailing commas

--- test/CalendarTest.php
+++ test/CalendarTest.php
@@ -189,7 +189,7 @@ class CalendarTest extends TestCase
                         'dayOfWeek' => [
                             'saturday',
                             'sunday',
-                        ]
+                        ],
                     ],
                 ],
             ],
@@ -249,7 +249,7 @@ class CalendarTest extends TestCase
                     'dayOfWeek' => [
                         'saturday',
                         'sunday',
-                    ]
+                    ],
                 ],
             ],
         ]);

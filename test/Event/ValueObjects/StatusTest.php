3d896f4c7cd7c6a4fcf7e79ab82148fd3c3b6ed2
Jonas Drieghe <jonasd@madewithlove.be>
2020-11-27 08:00:16 +0100

    Expect serialized properties to be different

--- test/Event/ValueObjects/StatusTest.php
+++ test/Event/ValueObjects/StatusTest.php
@@ -22,8 +22,8 @@ class StatusTest extends TestCase
 
         $this->assertEquals(
             [
-                'eventStatus' => 'EventCancelled',
-                'eventStatusReason' => [
+                'status' => 'EventCancelled',
+                'statusReason' => [
                     'nl' => 'Het concert van 10/11 is afgelast',
                     'fr' => 'Le concert de 10/11 a été annulé',
                 ],
@@ -39,8 +39,8 @@ class StatusTest extends TestCase
     {
         $actualStatus = Status::deserialize(
             [
-                'eventStatus' => 'EventCancelled',
-                'eventStatusReason' => [
+                'status' => 'EventCancelled',
+                'statusReason' => [
                     'nl' => 'Het concert van 10/11 is afgelast',
                     'fr' => 'Le concert de 10/11 a été annulé',
                 ],
